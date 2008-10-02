f4acd10c150a90ece08455dac3801d1a5cfd3341
Mitch Skinner <mitch_skinner@berkeley.edu>
2008-10-02 13:34:39 -0700

    multiple zooms in wig2png.cc

--- wig2png.cc
+++ wig2png.cc
@@ -4,6 +4,8 @@
 #include <string.h>
 #include <stdarg.h>
 #include <errno.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 
 #include <string>
 #include <vector>
@@ -31,16 +33,17 @@ void abort_(const char * s, ...)
 }
 
 void write_png_file(const char* file_name, int width, int height, png_bytep imgbuf[], png_color palette[], int num_colors, int bitdepth) {
-    /* create file */
     FILE *fp = fopen(file_name, "wb");
     if (!fp)
         abort_("[write_png_file] File %s could not be opened for writing", file_name);
 
+    //png_ptr holds overall libpng state
     png_structp png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
 	
     if (!png_ptr)
         abort_("[write_png_file] png_create_write_struct failed");
 
+    //info_ptr holds libpng state for this particular image
     png_infop info_ptr = png_create_info_struct(png_ptr);
     if (!info_ptr) {
         png_destroy_write_struct(&png_ptr, (png_infopp)NULL);
@@ -56,17 +59,24 @@ void write_png_file(const char* file_name, int width, int height, png_bytep imgb
 
     png_init_io(png_ptr, fp);
 
+    //set image header info
     png_set_IHDR(png_ptr, info_ptr, width, height, bitdepth,
                  PNG_COLOR_TYPE_PALETTE, PNG_INTERLACE_NONE,
                  PNG_COMPRESSION_TYPE_BASE, PNG_FILTER_TYPE_BASE);
 
+    //set palette info
     png_set_PLTE(png_ptr, info_ptr, palette, num_colors);
 
+    //set actual image data
     png_set_rows(png_ptr, info_ptr, imgbuf);
 
+    //the "up" filter encodes each pixel as the difference between it
+    //and the pixel above it (makes the most sense for wiggle images, I think)
     png_set_filter(png_ptr, 0, PNG_FILTER_VALUE_UP);
     png_set_compression_level(png_ptr, 5);
 
+    //actually write the file
+    //PNG_TRANSFORM_PACKING shrinks the bit depth down
     png_write_png(png_ptr, info_ptr, PNG_TRANSFORM_PACKING, NULL);
 
     png_destroy_write_struct(&png_ptr, &info_ptr);
@@ -91,81 +101,18 @@ class WiggleTileRenderer {
 
 public:
 
-    WiggleTileRenderer(int tileWidthBases, int tileWidthPixels, int tileHeight,
+    WiggleTileRenderer(int pixelBases, int tileWidthPixels, int tileHeight,
                        string baseDir)
-        : curTile_(0),
-          tileWidthBases_(tileWidthBases),
+        : curTile_(numeric_limits<int>::min()),
+          pixelBases_(pixelBases),
+          tileWidthBases_(pixelBases * tileWidthPixels),
           tileWidthPixels_(tileWidthPixels),
           tileHeight_(tileHeight),
-          curBase_(numeric_limits<int>::min()),
-          step_(1),
-          span_(1),
-          chrom_(""),
           baseDir_(baseDir) {
     }
 
-    void newSection(string chrom, string start, string step, string span) {
-        //cerr << "chrom: " << chrom << ", start: " << start << ", step: " << step << ", span: " << span << endl;
-        chrom_ = chrom;
-        if(!from_string<int>(curBase_, start, dec)) {
-            cerr << "wig parsing failed on start \"" << start << "\"" << endl;
-            exit(1);
-        }
-        if(!from_string<int>(step_, step, dec)) {
-            cerr << "wig parsing failed on step \"" << step << "\"" << endl;
-            exit(1);
-        }
-        if(!from_string<int>(span_, span, dec)) {
-            cerr << "wig parsing failed on span \"" << span << "\"" << endl;
-            exit(1);
-        }
-        //TODO: handle chromosome switching, generate blank tiles?
-    }
-
-    void handleParams(stringstream& ss, map<string,string>& params) {
-        string setting;
-        size_t found;
-        while (ss >> setting) {
-            found = setting.find("=");
-            if (string::npos == found) continue;
-            params[setting.substr(0, found)] = setting.substr(found + 1);
-        }
-    }
-
-    void processLine(string line) {
-        string word;
-        float sample;
-        stringstream ss(line);
-        map<string,string> params;
-        //defaults
-        params["span"] = "1";
-        ss >> word;
-        if ("fixedStep" == word) {
-            handleParams(ss, params);
-            newSection(params["chrom"],
-                       params["start"],
-                       params["step"],
-                       params["span"]);
-        } else if ("variableStep" == word) {
-            cerr << "variableStep not yet implemented";
-            exit(1);
-        } else {
-            if(from_string<float>(sample, word, dec)) {
-                if (1 == span_) {
-                    addValue(curBase_, sample);
-                } else {
-                    for (int i = 0; i < span_; i++)
-                        addValue(curBase_ + i, sample);
-                }
-                curBase_ += step_;
-            } else {
-                cerr << "wig parsing failed on: \"" << line << "\"" << endl;
-                exit(1);
-            }
-        }
-    }
-
     void addValue(int base, float value) {
+        //cerr << "zoom " << pixelBases_ << " addValue: base: " << base << ", value: " << value <<endl;
         if (base > ((curTile_ + 1) * tileWidthBases_)) {
             renderTile();
             curTile_ = base / tileWidthBases_;
@@ -176,26 +123,59 @@ public:
     }
   
     void renderTile() {
+        //cerr << "rendering" << endl;
         stringstream s;
-        s << baseDir_ << "/" << curTile_ << ".png";
+        s << baseDir_ << "/" 
+          << chrom_ << "/" 
+          << pixelBases_ << "/"
+          << curTile_ << ".png";
         drawTile(s.str());
     }
-    
+
+    void newSection(const string chrom, const int base) {
+        //cerr << "newSection: chrom: " << chrom << ", base: " << base << endl;
+        //we have to finish off the current tile if:
+        // a) there is a current tile (i.e., we're not at the beginning), and
+        // b) we're on a new chromosome, or
+        // c) we've skipped past the end of this tile
+        if ((curTile_ != numeric_limits<int>::min())
+            && ((chrom != chrom_)
+                || (curTile_ != base / tileWidthBases_))) {
+            renderTile();
+            newTile();
+        } 
+        chrom_ = chrom;
+        curTile_ = base / tileWidthBases_;
+
+        stringstream s;
+        s << baseDir_ << "/" 
+          << chrom_ << "/" 
+          << pixelBases_ << "/";
+
+        if (-1 == mkdir(s.str().c_str(), 0777)) {
+            if (EEXIST != errno) {
+                cerr << "failed to make directory" << s.str() << endl;
+                exit(1);
+            }
+        }
+    }    
+
+    //render the current tile to file
     virtual void drawTile(string pngFile) = 0;
+    //initialize data (e.g., set counts/sums to zero)
     virtual void newTile() = 0;
+    //handle sample
     virtual void processValue(int base, float value) = 0;
 
 protected:
 
+    int pixelBases_;
     int tileWidthBases_;
     int tileWidthPixels_;
     int tileHeight_;
-    int curBase_;
-    int step_;
-    int span_;
-    string chrom_;
     int curTile_;
     string baseDir_;
+    string chrom_;
 };
 
 class MeanRenderer : public WiggleTileRenderer {
@@ -286,6 +266,111 @@ private:
     png_bytep * buf_;
 };
 
+class WiggleParser {
+public:
+
+    WiggleParser() :
+          curBase_(numeric_limits<int>::min()),
+          step_(1),
+          span_(1),
+          chrom_("") {
+    }
+
+    void addRenderer(WiggleTileRenderer* r) {
+        renderers_.push_back(r);
+    }
+
+    void newSection(string chrom, string start, string step, string span) {
+        chrom_ = chrom;
+        if(!from_string<int>(curBase_, start, dec)) {
+            cerr << "wig parsing failed on start \"" << start << "\"" << endl;
+            exit(1);
+        }
+        if(!from_string<int>(step_, step, dec)) {
+            cerr << "wig parsing failed on step \"" << step << "\"" << endl;
+            exit(1);
+        }
+        if(!from_string<int>(span_, span, dec)) {
+            cerr << "wig parsing failed on span \"" << span << "\"" << endl;
+            exit(1);
+        }
+
+        for (int i = 0; i < renderers_.size(); i++)
+            renderers_[i]->newSection(chrom_, curBase_);
+    }
+
+    void handleParams(stringstream& ss, map<string,string>& params) {
+        string setting;
+        size_t found;
+        while (ss >> setting) {
+            found = setting.find("=");
+            if (string::npos == found) continue;
+            params[setting.substr(0, found)] = setting.substr(found + 1);
+        }
+    }
+
+    void processLine(string line) {
+        string word;
+        float sample;
+        stringstream ss(line);
+        map<string,string> params;
+        ss >> word;
+        if ("fixedStep" == word) {
+            //defaults
+            params["span"] = "1";
+            handleParams(ss, params);
+
+            newSection(params["chrom"],
+                       params["start"],
+                       params["step"],
+                       params["span"]);
+        } else if ("variableStep" == word) {
+            cerr << "variableStep not yet implemented";
+            exit(1);
+        } else {
+            if(from_string<float>(sample, word, dec)) {
+                if (1 == span_) {
+                    addValue(curBase_, sample);
+                } else {
+                    for (int i = 0; i < span_; i++)
+                        addValue(curBase_ + i, sample);
+                }
+                curBase_ += step_;
+            } else {
+                cerr << "wig parsing failed on: \"" << line << "\"" << endl;
+                exit(1);
+            }
+        }
+    }
+
+    void addValue(int base, float value) {
+        for (int i = 0; i < renderers_.size(); i++)
+            renderers_[i]->addValue(base, value);
+    }
+
+    void processWiggle(const char* filename) {
+        string line;
+        ifstream wig(filename);
+        if (wig.is_open()) {
+            while (! wig.eof() ) {
+                getline (wig,line);
+                if (line.length() > 0) processLine(line);
+            }
+            wig.close();
+        }  else {
+            cerr << "Unable to open file " << filename << endl;
+            exit(1);
+        }
+    }
+
+private:
+    vector<WiggleTileRenderer*> renderers_;
+
+    int curBase_;
+    int step_;
+    int span_;
+    string chrom_;
+};
 
 
 int main(int argc, char **argv){
@@ -311,29 +396,19 @@ int main(int argc, char **argv){
     float max = 1.0f;
     float min = 0.0f;
 
-/*
-  //bases per pixel
-  int[] zooms = {1, 2, 5, 10, 20, 50, 100, 200, 500, 1000,
-                 2000, 5000, 10000, 20000, 50000, 100000,
-                 200000, 500000, 1000000, 2000000, 5000000};
-  
-*/
+    //bases per pixel
+    int zooms[] = {1, 2, 5, 10, 20, 50, 100, 200, 500, 1000,
+                   2000, 5000, 10000, 20000, 50000, 100000};
+    int num_zooms=16;
 
-    MeanRenderer r(2000, width, height, baseDir,
-                   bg, fg, max, min);
+    WiggleParser p;
 
-    string line;
-    ifstream wig(argv[1]);
-    if (wig.is_open()) {
-        while (! wig.eof() ) {
-            getline (wig,line);
-            if (line.length() > 0) r.processLine(line);
-        }
-        wig.close();
-    }  else {
-        cerr << "Unable to open file " << argv[1];
-        return 1;
+    for (int i = 0; i < num_zooms; i++) {
+        p.addRenderer(new MeanRenderer(zooms[i], width, height, baseDir,
+                                       bg, fg, max, min));
     }
+
+    p.processWiggle(argv[1]);
 
     return 0;
 }
