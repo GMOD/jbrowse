c59f75213e50b27874943fcfe18b66522324f55a
Mitch Skinner <mitch_skinner@berkeley.edu>
2009-01-22 18:47:52 -0800

    make prepare-refseqs.pl generate chunked sequence [#3 state:resolved]

--- bin/prepare-refseqs.pl
+++ bin/prepare-refseqs.pl
@@ -6,13 +6,18 @@ use warnings;
 use FindBin qw($Bin);
 use lib "$Bin/../lib";
 
+use POSIX;
 use Getopt::Long;
 use JsonGenerator;
 
-my ($confFile, $gff, $refs, $refids);
+my $chunkSize = 20000;
+my ($confFile, $noSeq, $gff, $refs, $refids);
 my $outDir = "data";
+my $seqDir = "seq";
 GetOptions("out=s" => \$outDir,
            "conf=s" => \$confFile,
+           "noseq" => \$noSeq,
+           "seqdir" => \$seqDir,
            "gff=s" => \$gff,
 	   "refs=s" => \$refs,
            "refids=s" => \$refids);
@@ -22,15 +27,25 @@ if (!(defined($gff) || defined($confFile))) {
 USAGE:
        $0 [--out <output directory>] --gff <gff file describing refseqs>
    OR:
-       $0 [--out <output directory>] --conf <JBrowse config file> --refs <list of refseq names> --refids <list of refseq IDs>
+       $0 [--out <output directory>] [--noseq] [--seqdir <sequence data directory>] --conf <JBrowse config file> --refs <list of refseq names> --refids <list of refseq IDs>
     <output directory>: defaults to "data"
+    <sequence data directory>: chunks of sequence go here; defaults to "seq"
 
-    you can use a GFF file to describe the reference sequences, or you can use a JBrowse config file and a list of refseq names or a list of refseq IDs.  If you use a GFF file, it should contain ##sequence-region lines as described in the GFF specs.  If you use a JBrowse config file, you can either provide a (comma-separated) list of refseq names, or (if the names aren't globally unique) a list of refseq IDs.
+    --noseq: do not prepare sequence data for the client.
+
+    You can use a GFF file to describe the reference sequences, or
+    you can use a JBrowse config file and a list of refseq names
+    or a list of refseq IDs.  If you use a GFF file, it should
+    contain ##sequence-region lines as described in the GFF specs.
+    If you use a JBrowse config file, you can either provide a
+    (comma-separated) list of refseq names, or (if the names
+    aren't globally unique) a list of refseq IDs.
 HELP
 exit;
 }
 
 mkdir($outDir) unless (-d $outDir);
+mkdir($seqDir) unless $noSeq || (-d $seqDir);
 
 my @refSeqs;
 
@@ -71,31 +86,80 @@ if (defined($gff)) {
         eval {$db->default_class($refclass)};
     }
     $db->strict_bounds_checking(1) if $db->can('strict_bounds_checking');
-    $db->absolute(1)               if $db->can('absolute');
 
     if (defined($refids)) {
         foreach my $refid (split ",", $refids) {
             my $seg = $db->segment(-db_id => $refid);
-            push @refSeqs, {
+            my $refInfo = {
                 name => refName($seg),
                 id => $refid, #keep ID for later querying
                 start => $seg->start - 1,
                 end => $seg->end,
                 length => $seg->length
             };
+
+            unless ($noSeq) {
+                my $refDir = $seqDir . "/" . $refInfo->{"name"};
+                exportSeqChunks($refDir, $chunkSize, $db,
+                                [-db_id => $refid],
+                                $seg->start, $seg->end);
+                $refInfo->{"seqDir"} = $refDir;
+                $refInfo->{"seqChunkSize"} = $chunkSize;
+            }
+
+            push @refSeqs, $refInfo;
         }
     }
 
     if (defined($refs)) {
         foreach my $ref (split ",", $refs) {
             my $seg = $db->segment(-name => $ref);
-            push @refSeqs, {
+            my $refInfo =  {
                 name => refName($seg),
                 start => $seg->start - 1,
                 end => $seg->end,
                 length => $seg->length
             };
+
+            unless ($noSeq) {
+                my $refDir = $seqDir . "/" . $refInfo->{"name"};
+                exportSeqChunks($refDir, $chunkSize, $db,
+                                [-name => $ref],
+                                $seg->start, $seg->end);
+                $refInfo->{"seqDir"} = $refDir;
+                $refInfo->{"seqChunkSize"} = $chunkSize;
+            }
+
+            push @refSeqs, $refInfo;
         }
+    }
+}
+
+sub exportSeqChunks {
+    my ($dir, $len, $db, $segDef, $start, $end) = @_;
+
+    mkdir($dir) unless (-d $dir);
+    $start = 1 if ($start < 1);
+
+    my $chunkStart = $start;
+    while ($chunkStart <= $end) {
+        my $chunkEnd = $chunkStart + $len - 1;
+        my $chunkNum = floor(($chunkStart - 1) / $chunkSize);
+        my $path = "$dir/$chunkNum.txt";
+        my $seg = $db->segment(@$segDef,
+                               -start => $chunkStart,
+                               -end => $chunkEnd,
+                               -absolute => 1);
+        die "requested $chunkStart .. $chunkEnd; got " . $seg->start . " .. " . $seg->end if ($seg->start != $chunkStart);
+        $chunkStart = $chunkEnd + 1;
+        next unless ($seg && $seg->seq && $seg->seq->seq);
+
+
+        open CHUNK, ">$path"
+          or die "couldn't open $path: $!";
+        print CHUNK $seg->seq->seq;
+        close CHUNK
+          or die "couldn't open $path.txt: $!";
     }
 }
 
