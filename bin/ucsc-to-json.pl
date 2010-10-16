ec6a74b3b29cdfd0fc74546b3e84c062509542f7
Mitch Skinner <mitch_skinner@berkeley.edu>
2010-10-15 21:58:42 -0700

    convert from direct database access to parsing the DB dump, also speed things up a little

--- bin/ucsc-to-json.pl
+++ bin/ucsc-to-json.pl
@@ -6,18 +6,19 @@ use warnings;
 use FindBin qw($Bin);
 use lib "$Bin/../lib";
 
-use DBI;
+use IO::Uncompress::Gunzip qw($GunzipError);
 use Getopt::Long;
 use List::Util qw(min max);
 use JSON 2;
 use JsonGenerator;
 
-my ($tracks, $arrowheadClass, $subfeatureClasses, $clientConfig, $db);
+my $trackdb = "trackDb";
+my ($indir, $tracks, $arrowheadClass, $subfeatureClasses, $clientConfig, $db);
 my $outdir = "data";
 my $cssClass = "basic";
 my $nclChunk = 500;
-GetOptions("out=s" => \$outdir,
-           "db=s" => \$db,
+GetOptions("in=s" => \$indir,
+           "out=s" => \$outdir,
            "track=s@" => \$tracks,
            "cssClass=s", \$cssClass,
            "arrowheadClass=s", \$arrowheadClass,
@@ -25,22 +26,14 @@ GetOptions("out=s" => \$outdir,
            "clientConfig=s", \$clientConfig,
            "nclChunk=i" => \$nclChunk);
 
-die "please specify a database (e.g., hg19) with the --db parameter"
-    unless defined($db);
+die "please specify the directory with the database dumps using the --in parameter"
+    unless defined($indir);
 
 my $trackDir = "$outdir/tracks";
 mkdir($outdir) unless (-d $outdir);
 mkdir($trackDir) unless (-d $trackDir);
 
 my @refSeqs = @{JsonGenerator::readJSON("$outdir/refSeqs.js", [], 1)};
-
-my $dbh = DBI->connect("DBI:mysql:database=$db;host=genome-mysql.cse.ucsc.edu",
-                       "genome");
-
-#$sth = $dbh->prepare("select * from trackDb where ((type like 'bed %') or (type like 'genePred %')) and not (settings like '%compositeTrack on%')");
-#my $trackDbFields = $sth->{NAME_hash};
-#my $trackList = $sth->fetchall_arrayref();
-
 
 # the jbrowse NCList code requires that "start" and "end" be
 # the first and second fields in the array; @defaultHeaders and @srcMap
@@ -72,23 +65,17 @@ my %skipFields = ("bin" => 1,
 my %defaultStyle = ("class" => $cssClass);
 
 foreach my $shortLabel (@$tracks) {
-    my $track = $dbh->selectrow_hashref(
-        sprintf(
-            "select * from trackDb where shortLabel = %s",
-            $dbh->quote($shortLabel)
-        ))
-        or die "couldn't get track metadata";
-
+    my %trackdbCols = name2column_map($indir . "/" . $trackdb);
+    my $shortLabelCol = $trackdbCols{shortLabel};
+    my $trackRows = selectall($indir . "/" . $trackdb,
+                              sub { $_[0]->[$shortLabelCol] eq $shortLabel });
+    my $track = arrayref2hash($trackRows->[0], \%trackdbCols);
     my %trackSettings = (map {split(" ", $_, 2)} split("\n", $track->{settings}));
     $defaultStyle{subfeature_classes} = JSON::from_json($subfeatureClasses)
         if defined($subfeatureClasses);
     $defaultStyle{arrowheadClass} = $arrowheadClass if defined($arrowheadClass);
     $defaultStyle{clientConfig} = JSON::from_json($clientConfig)
         if defined($clientConfig);
-
-    my $featQuery = $dbh->prepare("select * from "
-                                      . $dbh->quote_identifier($track->{tableName})
-                                          . " where chrom=?");
 
     my @types = split(" ", $track->{type});
     my $type = $types[0];
@@ -99,26 +86,19 @@ foreach my $shortLabel (@$tracks) {
         "key" => $trackSettings{shortLabel}
     );
 
-    foreach my $ref (@refSeqs) {
-        mkdir("$trackDir/" . $ref->{name})
-            unless (-d "$trackDir/" . $ref->{name});
-        unless ($featQuery->execute($ref->{name})) {
-            warn "query failed for table " . $track->{tableName};
-            sleep 10;
-            next;
-        }
-        my $fields = $featQuery->{NAME_hash};
-        my ($converter, $headers, $subfeatures) = makeConverter($fields, $type);
+    my %fields = name2column_map($indir . "/" . $track->{tableName});
 
-        my $color = sprintf("#%02x%02x%02x",
-                            $track->{colorR},
-                            $track->{colorG},
-                            $track->{colorB});
+    my ($converter, $headers, $subfeatures) = makeConverter(\%fields, $type);
 
-        if ($subfeatures) {
-            $style{subfeatureHeaders} = \@subfeatHeaders;
-            $style{class} = "generic_parent";
-            $style{clientConfig}->{featureCallback} = <<ENDJS;
+    my $color = sprintf("#%02x%02x%02x",
+                        $track->{colorR},
+                        $track->{colorG},
+                        $track->{colorB});
+
+    if ($subfeatures) {
+        $style{subfeatureHeaders} = \@subfeatHeaders;
+        $style{class} = "generic_parent";
+        $style{clientConfig}->{featureCallback} = <<ENDJS;
 function(feat, fields, div) {
     if (fields.type) {
         div.className = "basic";
@@ -138,30 +118,46 @@ function(feat, fields, div) {
     }
 }
 ENDJS
-        } else {
-            $style{clientConfig} = {
-                "featureCss" => "background-color: $color; height: 8px;",
-                "histCss" => "background-color: $color;"
-            };
-        }
+    } else {
+        $style{clientConfig} = {
+            "featureCss" => "background-color: $color; height: 8px;",
+            "histCss" => "background-color: $color;"
+        };
+    }
 
-        my @nameList;
-        my $jsonGen = JsonGenerator->new($trackSettings{track},
-                                         $ref->{name},
-                                         \%style, $headers);
-        while (my $row = $featQuery->fetchrow_arrayref()) {
-            my $jsonRow = $converter->($row, $fields, $type);
-            #print STDERR join(" ", @$jsonRow) . "\n";
-            $jsonGen->addFeature($jsonRow);
-            if ($fields->{name}) {
-                $jsonGen->addName([$trackSettings{track},
-                                   $row->[$fields->{name}],
-                                   $ref->{name},
-                                   $jsonRow->[0],
-                                   $jsonRow->[1],
-                                   $row->[$fields->{name}]]);
-            }
-        }
+    my %perChromGens;
+    foreach my $ref (@refSeqs) {
+        mkdir("$trackDir/" . $ref->{name})
+            unless (-d "$trackDir/" . $ref->{name});
+        $perChromGens{$ref->{name}} = JsonGenerator->new($trackSettings{track},
+                                                         $ref->{name},
+                                                         \%style, $headers);
+    }
+
+    my @nameList;
+    my $chromCol = $fields{chrom};
+    my $nameCol = $fields{name};
+    for_columns("$indir/" . $track->{tableName},
+                sub {
+                    my $jsonRow = $converter->($_[0], \%fields, $type);
+                    my $jsonGen = $perChromGens{$_[0]->[$chromCol]};
+                    if (defined $jsonGen) {
+                        $jsonGen->addFeature($jsonRow);
+                        if (defined $nameCol) {
+                            $jsonGen->addName([$trackSettings{track},
+                                               $_[0]->[$nameCol],
+                                               $_[0]->[$chromCol],
+                                               $jsonRow->[0],
+                                               $jsonRow->[1],
+                                               $_[0]->[$nameCol]]);
+                        }
+                    }
+                });
+
+    foreach my $ref (@refSeqs) {
+        mkdir("$trackDir/" . $ref->{name})
+            unless (-d "$trackDir/" . $ref->{name});
+        my $jsonGen = $perChromGens{$ref->{name}};
         if ($jsonGen->hasFeatures) {
             $jsonGen->generateTrack("$trackDir/"
                                         . $ref->{name} . "/" 
@@ -191,19 +187,15 @@ ENDJS
         });
 }
 
-$dbh->disconnect();
-
 sub calcSizes {
     my ($starts, $ends) = @_;
     return undef unless (defined($starts) && defined($ends));
-    my @exonStarts = split (/,/, $starts);
-    my @exonEnds = split (/,/, $ends);
-    return join ("", map (($exonEnds[$_] - $exonStarts[$_]) . ",", 0..$#exonStarts));
+    return [map($ends->[$_] - $starts->[$_], 0..$#$starts)];
 }
 
 sub abs2rel {
     my ($start, $starts) = @_;
-    return join("", map {($_ - $start) . ","} split(",", $starts));
+    return [map($_ - $start, @$starts)];
 }
 
 sub indexHash {
@@ -220,6 +212,12 @@ sub maybeIndex {
     return (defined $index) ? $ary->[$index] : undef;
 }
 
+sub splitNums {
+    my ($ary, $index) = @_;
+    return [] unless defined $index;
+    return [map(int, split(",", $ary->[$index]))];
+}
+
 sub makeConverter {
     # $orig_fields should be a reference to a hash where
     # the keys are names of columns in the source, and the values
@@ -231,7 +229,6 @@ sub makeConverter {
     # describe the arrays returned by the sub
     my ($orig_fields, $type) = @_;
     my %fields = (%$orig_fields);
-    #print STDERR "fields: " . join(" ", %fields) . "\n";
     my @headers;
     my $srcMap = $typeMaps{$type};
     my @indexMap;
@@ -239,7 +236,6 @@ sub makeConverter {
     for (my $i = 0; $i <= $#defaultHeaders; $i++) {
         last if $i > $#{$srcMap};
         my $srcName = $srcMap->[$i];
-        #print STDERR "srcName: $srcName\n";
         if (exists($fields{$srcName})) {
             push @headers, $defaultHeaders[$i];
             push @indexMap, $fields{$srcName};
@@ -268,8 +264,8 @@ sub makeConverter {
                 makeSubfeatures(maybeIndex($dest, $strandIndex),
                                 $dest->[0], $dest->[1],
                                 maybeIndex($src, $fields{blockCount}),
-                                maybeIndex($src, $fields{chromStarts}),
-                                maybeIndex($src, $fields{blockSizes}),
+                                splitNums($src, $fields{chromStarts}),
+                                splitNums($src, $fields{blockSizes}),
                                 maybeIndex($src, $fields{thickStart}),
                                 maybeIndex($src, $fields{thickEnd}),
                                 "thin", "thick");
@@ -281,16 +277,14 @@ sub makeConverter {
         $extraProcessing = sub {
             my ($dest, $src) = @_;
             $dest->[$subIndex] =
-                makeSubfeatures(defined($strandIndex)
-                                ? $dest->[$strandIndex]
-                                : undef,
+                makeSubfeatures(maybeIndex($dest, $strandIndex),
                                 $dest->[0], $dest->[1],
-                                $src->[$fields{exonCount}],
-                                abs2rel($dest->[0], $src->[$fields{exonStarts}]),
-                                calcSizes($src->[$fields{exonStarts}],
-                                          $src->[$fields{exonEnds}]),
-                                $src->[$fields{cdsStart}],
-                                $src->[$fields{cdsEnd}],
+                                maybeIndex($src, $fields{exonCount}),
+                                abs2rel($dest->[0], splitNums($src, $fields{exonStarts})),
+                                calcSizes(splitNums($src, $fields{exonStarts}),
+                                          splitNums($src, $fields{exonEnds})),
+                                maybeIndex($src, $fields{cdsStart}),
+                                maybeIndex($src, $fields{cdsEnd}),
                                 "UTR", "CDS");
         }
     } else {
@@ -319,7 +313,7 @@ sub makeConverter {
 
 sub makeSubfeatures {
     my ($strand, $start, $end,
-        $block_count, $block_starts, $block_sizes,
+        $block_count, $offset_list, $length_list,
         $thick_start, $thick_end,
         $thin_type, $thick_type) = @_;
 
@@ -331,18 +325,15 @@ sub makeSubfeatures {
     my $parent_strand = $strand ? ($strand eq '+' ? 1 : -1) : 0;
 
     if ($block_count > 0) {
-        my @length_list = split(",", $block_sizes);
-        my @offset_list = split(",", $block_starts);
-
-        if (($block_count != ($#length_list + 1))
-                || ($block_count != ($#offset_list + 1)) ) {
-            warn "expected $block_count blocks, got " . ($#length_list + 1) . " lengths and " . ($#offset_list + 1) . " offsets for feature at $start .. $end";
+        if (($block_count != ($#$length_list + 1))
+                || ($block_count != ($#$offset_list + 1)) ) {
+            warn "expected $block_count blocks, got " . ($#$length_list + 1) . " lengths and " . ($#$offset_list + 1) . " offsets for feature at $start .. $end";
         } else {
             for (my $i = 0; $i < $block_count; $i++) {
                 #block start and end, in absolute (sequence rather than feature)
                 #coords.  These are still in interbase.
-                my $abs_block_start = int($start) + int($offset_list[$i]);
-                my $abs_block_end = $abs_block_start + int($length_list[$i]);
+                my $abs_block_start = int($start) + int($offset_list->[$i]);
+                my $abs_block_end = $abs_block_start + int($length_list->[$i]);
 
                 #add a thin subfeature if this block extends
                 # left of the thick zone
@@ -379,4 +370,72 @@ sub makeSubfeatures {
                             $thick_type];
     }
     return \@subfeatures;
+}
+
+# processes a table to find all the rows for which $filter returns true.
+# returns a list of arrayrefs, where each arrayref represents a row.
+sub selectall {
+    my ($table, $filter) = @_;
+    my @result;
+    for_columns($table, sub { push @result, $_[0] if ($filter->($_[0])) });
+    return \@result;
+}
+
+# converts an array ref of values and a hash ref with field name->index mappings
+# into a hash of name->value mappings
+sub arrayref2hash {
+    my ($aref, $fields) = @_;
+    my %result;
+    foreach my $key (keys %$fields) {
+        $result{$key} = $aref->[$fields->{$key}];
+    }
+    return \%result;
+}
+
+# subroutine to crudely parse a .sql table description file and return a map from column names to column indices
+sub name2column_map {
+    my ($table) = @_;
+    my $sqlfile = "$table.sql";
+
+    my @cols;
+    local *SQL;
+    local $_;
+    open SQL, "<$sqlfile" or die "$sqlfile: $!";
+    while (<SQL>) { last if /CREATE TABLE/ }
+    while (<SQL>) {
+	last if /^\)/;
+	if (/^\s*\`(\S+)\`/) { push @cols, $1 }
+    }
+    close SQL;
+
+    return map (($cols[$_] => $_), 0..$#cols);
+}
+
+# subroutine to crudely parse a .txt.gz table dump, and, for each row,
+# apply a given subroutine to a array ref that holds the values for the
+# columns of that row
+sub for_columns {
+    my ($table, $func) = @_;
+
+    my $gzip = new IO::Uncompress::Gunzip "$table.txt.gz"
+        or die "gunzip failed: $GunzipError\n";
+
+    my $lines = 0;
+    my $row = "";
+    while (my $line = $gzip->getline()) {
+	chomp $line;
+	if ($line =~ /\\$/) {
+	    chop $line;
+	    $row .= "$line\n";
+	} else {
+            chomp $line;
+	    $row .= $line;
+	    my @data = split /\t/, $row; # deal with escaped tabs in data?  what are the escaping rules?
+	    &$func (\@data);
+	    $row = "";
+	}
+	if (++$lines % 50000 == 0) { warn "(processed $lines lines)\n" }
+    }
+    $gzip->close()
+        or die "couldn't close gunzip: $GunzipError\n";
 }
