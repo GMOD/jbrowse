03bd20b96fde03688d11bf35709d8a44277bf178
Mitch Skinner <mitch_skinner@berkeley.edu>
2011-02-14 11:41:49 -0800

    clean up commented-out code from before PerlIO was used

--- lib/ExternalSorter.pm
+++ lib/ExternalSorter.pm
@@ -4,9 +4,6 @@ use strict;
 use warnings;
 
 use Carp;
-#use IO::File;
-#use IO::Compress::Gzip qw(:level $GzipError);
-#use IO::Uncompress::Gunzip qw($GunzipError);
 use PerlIO::gzip;
 use Storable qw(store_fd fd_retrieve);
 use Devel::Size qw(size total_size);
@@ -48,12 +45,6 @@ sub finish {
     my $compare = $self->{compare};
     if ($#{$self->{segments}} >= 0) {
         $self->flush();
-        # my @unzipSegments =
-        #     map {
-        #         my $zip = IO::Uncompress::Gunzip->new($_)
-        #             or croak "couldn't create gunzip: $GunzipError\n";
-        #         $zip;
-        #     } @{$self->{segments}};
         my @unzipFiles =
             map {
                 my $zip;
@@ -97,28 +88,12 @@ sub flush {
         or croak "couldn't close temp file: $!\n";
     open $fh, ">:gzip", $fn
         or croak "couldn't reopen $fn: $!\n";
-    #print STDERR "opened temp file $fn\n";
-    # my $zip = IO::Compress::Gzip->new($fh, Level => Z_BEST_SPEED)
-    #     or croak "IO::Compress::Gzip failed: $GzipError\n";
     foreach my $item (@sorted) {
-        # my $frozen = freeze($item);
-        # # the format of records in the file is a length, written in pack("w")
-        # # format, followed by the Storable frozen version of the item itself.
-        # # see the readOne method
-        # $zip->write(pack("w", length($frozen)))
-        #     or croak "couldn't write length: $GzipError\n"; #should this be $! ?
-        # $zip->write($frozen)
         store_fd($item, $fh)
             or croak "couldn't write item: $!\n";
     }
-    # $zip->flush()
-    #     or croak "couldn't flush gzip stream: $GzipError $!\n";
-    # $zip->close()
-    #     or croak "couldn't close gzip stream: $GzipError $!\n";
     $fh->flush()
         or croak "couldn't flush segment file: $!\n";
-    # $fh->seek(0, 0)
-    #     or croak "couldn't seek to beginning of segment file: $!\n";
     $fh->close()
         or croak "couldn't close $fn: $!\n";
     push @{$self->{segments}}, $fn;
@@ -149,25 +124,10 @@ sub get {
 # read one item from a file handle
 sub readOne {
     my ($fh) = @_;
-    #my @lenBytes;
-    #my $byte;
     if ($fh->eof()) {
         $fh->close();
         return undef;
     }
-    # # the format of records in the file is a length, written in pack("w")
-    # # format, followed by the Storable frozen version of the item itself.
-    # # see the flush method
-    # do {
-    #     $byte = $fh->getc()
-    #         or croak "couldn't read item length from file; current length takes up " . ($#lenBytes + 1) . " bytes; at line " . $fh->input_line_number() . " in file " . $fh->fileno() . "\n";
-    #     push @lenBytes, $byte;
-    # } until (ord($byte) < 128);
-    # my $len = unpack("w", join("", @lenBytes));
-    # my $frozen;
-    # $fh->read($frozen, $len)
-    #     or croak "couldn't read item from file";
-    # return thaw($frozen);
     my $item = fd_retrieve($fh)
         or croak "couldn't retrieve item: $!\n";
     return $item;
