7760f5437d8a8e718b85382feeb32acd5aacf3e6
Mitch Skinner <git@arctur.us>
2011-05-10 17:26:48 -0700

    support mysql in db_importer

--- python/db_importer.py
+++ python/db_importer.py
@@ -2,20 +2,18 @@ import os
 from json_generator import JsonGenerator
 
 # example call:
-# snpQuery = 'select chromStart as Start, chromEnd as End, name as Name, transcript, frame, alleleCount, funcCodes, alleles, codons, peptides'
-# db_importer.dbImport(conn, "snp132CodingDbSnp", snpQuery, "chromEnd", "chrom", "../data/", "snp132CodingDbSnp")
-def dbImport(conn, table, query, endCol, chromCol, dataDir,
+# snpQuery = 'select chromStart as Start, chromEnd as End, name as Name, transcript, frame, alleleCount, funcCodes, alleles, codons, peptides from snp132CodingDbSnp where chrom=%s'
+# db_importer.dbImport(cur, "snp132CodingDbSnp", snpQuery, "chromEnd", "chrom", "../data/", "snp132CodingDbSnp")
+def dbImport(cur, table, query, endCol, chromCol, dataDir,
              trackLabel, key = None, chunkBytes = 200000,
              compress = True,
              config = {'style': {'className': 'feature2'}}):
-    query += " from %s where %s=? order by Start asc, End desc" \
-             % (table, chromCol)
-    cur = conn.execute("""
+    cur.execute("""
     select %(chrom)s, max(%(end)s), count(*) from %(table)s group by %(chrom)s
     """ % {'chrom': chromCol, 'end': endCol, 'table': table})
     chromList = cur.fetchall()
     for (chrom, refEnd, count) in chromList:
-        cur = conn.execute(query, (chrom,))
+        cur.execute(query, (chrom,))
         classes = [{
             'attributes': [f[0] for f in cur.description],
             'proto': {'Chrom': chrom}
