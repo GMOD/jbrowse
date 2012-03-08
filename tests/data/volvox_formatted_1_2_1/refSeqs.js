ceeb09d81350b55b86968f213a2a899eaa53675f
Robert Buels <rmb32@cornell.edu>
2012-03-08 16:43:35 -0500

    added test data consisting of volvox formatted with the 1.2.1 release

---
+++ tests/data/volvox_formatted_1_2_1/refSeqs.js
@@ -0,0 +1,11 @@
+refSeqs = 
+[
+   {
+      "length" : 50001,
+      "name" : "ctgA",
+      "seqDir" : "seq/ctgA",
+      "seqChunkSize" : 20000,
+      "end" : 50001,
+      "start" : 0
+   }
+]
---
+++ tests/data/volvox_formatted_1_2_1/trackInfo.js
@@ -0,0 +1,78 @@
+trackInfo = 
+[
+   {
+      "url" : "seq/{refseq}/",
+      "args" : {
+         "chunkSize" : 20000
+      },
+      "label" : "DNA",
+      "type" : "SequenceTrack",
+      "key" : "DNA"
+   },
+   {
+      "url" : "tracks/{refseq}/ExampleFeatures/trackData.json",
+      "type" : "FeatureTrack",
+      "label" : "ExampleFeatures",
+      "key" : "Example Features"
+   },
+   {
+      "url" : "tracks/{refseq}/NameTest/trackData.json",
+      "label" : "NameTest",
+      "type" : "FeatureTrack",
+      "key" : "Name test track"
+   },
+   {
+      "url" : "tracks/{refseq}/Motifs/trackData.json",
+      "type" : "FeatureTrack",
+      "label" : "Motifs",
+      "key" : "Example motifs"
+   },
+   {
+      "url" : "tracks/{refseq}/Alignments/trackData.json",
+      "label" : "Alignments",
+      "type" : "FeatureTrack",
+      "key" : "Example alignments"
+   },
+   {
+      "url" : "tracks/{refseq}/Genes/trackData.json",
+      "type" : "FeatureTrack",
+      "label" : "Genes",
+      "key" : "Protein-coding genes"
+   },
+   {
+      "url" : "tracks/{refseq}/ReadingFrame/trackData.json",
+      "label" : "ReadingFrame",
+      "type" : "FeatureTrack",
+      "key" : "Frame usage"
+   },
+   {
+      "url" : "tracks/{refseq}/CDS/trackData.json",
+      "type" : "FeatureTrack",
+      "label" : "CDS",
+      "key" : "Predicted genes"
+   },
+   {
+      "url" : "tracks/{refseq}/Transcript/trackData.json",
+      "label" : "Transcript",
+      "type" : "FeatureTrack",
+      "key" : "Exonerate predictions"
+   },
+   {
+      "url" : "tracks/{refseq}/Clones/trackData.json",
+      "type" : "FeatureTrack",
+      "label" : "Clones",
+      "key" : "Fingerprinted BACs"
+   },
+   {
+      "url" : "tracks/{refseq}/EST/trackData.json",
+      "label" : "EST",
+      "type" : "FeatureTrack",
+      "key" : "ESTs"
+   },
+   {
+      "url" : "tracks/{refseq}/volvox_microarray.wig.json",
+      "type" : "ImageTrack",
+      "label" : "volvox_microarray.wig",
+      "key" : "volvox_microarray.wig"
+   }
+]
