6654bc680961a6c0cdb1274848ccf5d620e75141
Robert Buels <rmb32@cornell.edu>
2012-03-14 08:23:14 -0400

    refactor JB_json_v1 conf adaptor to be extendible

--- js/ConfigAdaptor/JB_json_v1.js
+++ js/ConfigAdaptor/JB_json_v1.js
@@ -1,18 +1,21 @@
 var ConfigAdaptor; if( !ConfigAdaptor ) ConfigAdaptor = {};
+
+/**
+ * Configuration adaptor for JBrowse JSON version 1 configuration
+ * files (formerly known as trackList.json files).
+ * @class
+ */
 ConfigAdaptor.JB_json_v1 = function() {
 };
 
 ConfigAdaptor.JB_json_v1.prototype.load = function( args ) {
-    var config_def = args.config;
+    var that = this;
     dojo.xhrGet({
-        url: config_def.url,
-        handleAs: 'json',
+        url: args.config.url,
+        handleAs: 'text',
         load: function( o ) {
-            o.sourceUrl = o.sourceUrl || config_def.url;
-            o.baseUrl   = o.baseUrl || Util.resolveUrl( o.sourceUrl, '.' );
-            if( ! /\/$/.test( o.baseUrl ) )
-                o.baseUrl += "/";
-
+            o = that.parse_conf( o, args );
+            o = that.regularize_conf( o, args );
             args.onSuccess.call( args.context || this, o );
         },
         error: function( i ) {
@@ -21,4 +24,33 @@ ConfigAdaptor.JB_json_v1.prototype.load = function( args ) {
                 args.onFailure.call( args.context || this, i);
         }
     });
+};
+
+/**
+ * In this adaptor, just evals the conf text to parse the JSON, but
+ * other conf adaptors might want to inherit and override this.
+ * @param {String} conf_text the configuration text
+ * @param {Object} load_args the arguments that were passed to <code>load()</code>
+ * @returns {Object} the parsed JSON
+ */
+ConfigAdaptor.JB_json_v1.prototype.parse_conf = function( conf_text, load_args ) {
+    var conf;
+    return eval( 'conf = ' + conf_text );
+};
+
+/**
+ * Applies defaults and any other necessary tweaks to the loaded JSON
+ * configuration.  Called by <code>load()</code> on the JSON
+ * configuration before it calls the <code>onSuccess</code> callback.
+ * @param {Object} o the object containing the configuration, which it
+ *                   modifies in-place
+ * @param {Object} load_args the arguments that were passed to <code>load()</code>
+ * @returns the same object it was passed
+ */
+ConfigAdaptor.JB_json_v1.prototype.regularize_conf = function( o, load_args ) {
+    o.sourceUrl = o.sourceUrl || load_args.config.url;
+    o.baseUrl   = o.baseUrl || Util.resolveUrl( o.sourceUrl, '.' );
+    if( ! /\/$/.test( o.baseUrl ) )
+        o.baseUrl += "/";
+    return o;
 };
