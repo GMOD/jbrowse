59ce85dd5ef4c62a34046a03eb3b267d86e3b8bd
Robert Buels <rmb32@cornell.edu>
2012-03-22 11:55:44 -0700

    split Animation, Slider, and Zoomer out of GenomeView.js into their own js files

---
+++ js/Slider.js
@@ -0,0 +1,21 @@
+/**
+ * @class
+ */
+function Slider(view, callback, time, distance) {
+    Animation.call(this, view, callback, time);
+    this.slideStart = view.getX();
+    this.slideDistance = distance;
+}
+
+Slider.prototype = new Animation();
+
+Slider.prototype.step = function(pos) {
+    var newX = (this.slideStart -
+                (this.slideDistance *
+                 //cos will go from 1 to -1, we want to go from 0 to 1
+                 ((-0.5 * Math.cos(pos * Math.PI)) + 0.5))) | 0;
+
+    newX = Math.max(Math.min(this.subject.maxLeft - this.subject.offset, newX),
+                         this.subject.minLeft - this.subject.offset);
+    this.subject.setX(newX);
+};
