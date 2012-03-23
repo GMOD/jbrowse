b6e5b9d9e60eecabd05bad087d18edd856d53b3c
Robert Buels <rmb32@cornell.edu>
2012-03-22 17:53:17 -0700

    simplify viewElem creation in Browser.js

--- js/Browser.js
+++ js/Browser.js
@@ -97,10 +97,9 @@ Browser.prototype.initView = function() {
 
     this.navbox = this.createNavBox( topPane, 25 );
 
-    var viewElem = document.createElement("div");
-    this.viewElem = viewElem;
-    this.container.appendChild(viewElem);
-    viewElem.className = "dragWindow";
+    this.viewElem = document.createElement("div");
+    this.viewElem.className = "dragWindow";
+    this.container.appendChild( this.viewElem);
 
     this.containerWidget = new dijit.layout.BorderContainer({
         liveSplitters: false,
@@ -110,7 +109,7 @@ Browser.prototype.initView = function() {
     var contentWidget =
         new dijit.layout.ContentPane({region: "top"}, topPane);
     this.browserWidget =
-        new dijit.layout.ContentPane({region: "center"}, viewElem);
+        new dijit.layout.ContentPane({region: "center"}, this.viewElem);
 
     //create location trapezoid
     this.locationTrap = document.createElement("div");
