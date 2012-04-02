d8dd7efa3ba58746a01f5efb9ab85ca949e27352
Robert Buels <rmb32@cornell.edu>
2012-04-02 14:43:39 -0400

    tweak Ruler display to look better in both IE and other browsers

--- js/Ruler.js
+++ js/Ruler.js
@@ -32,7 +32,7 @@ Ruler.prototype.render_to = function( target_div ) {
 
     dojo.style(container,{
                    position: 'absolute',
-                   left: "-12px",
+                   left: "-9px",
                    bottom: "-13px",
                    width: (40+4*label_digits)+"px",
                    height: (target_dims.h+26)+"px"
@@ -51,8 +51,15 @@ Ruler.prototype.render_to = function( target_div ) {
                             // majorTickStep: 1
                             //labels: [{value: 1, text: "One"}, {value: 3, text: "Ten"}]
                         });
-        chart1.addPlot("default", {type: "Default", fill: 'transparent'});
+        chart1.addPlot("default", {type: "Bubble", fill: 'transparent'});
         chart1.render();
+
+        // hack to remove a undesirable opaque white rectangle i can't
+        // coax dojox.charting to leave out
+        var undesirable_rect = container.childNodes[0].childNodes[1];
+        if( undesirable_rect )
+            undesirable_rect.setAttribute('fill-opacity',0);
+
     } catch (x) {
         console.error(x+'');
         console.error("Failed to draw Ruler with SVG, your browser may not support the necessary technology.");
