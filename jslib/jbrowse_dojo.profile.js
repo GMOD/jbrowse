478af24486ee78d8d6e8a56659942fe0cf6726d7
Robert Buels <rbuels@gmail.com>
2012-04-20 09:16:52 -0400

    Revert "remove some dojo related files".  These are for building JBrowse's copy of dojo.
    This reverts commit b1c0d91d4550f43085c2ea2a8a3f849c13cc93c6.

---
+++ jslib/jbrowse_dojo.profile.js
@@ -0,0 +1,23 @@
+dependencies = (
+    {
+        prefixes: [
+            ["dijit", "../dijit"]
+        ],
+        layers: [
+            {
+                dependencies: [
+                    "dojo.dnd.Source",
+                    "dojo.dnd.Moveable",
+                    "dojo.dnd.Mover",
+                    "dojo.dnd.move",
+                    "dijit.layout.ContentPane",
+                    "dijit.layout.BorderContainer"
+//                    "dijit.form.ComboBox"
+                ],
+                layerDependencies: [],
+                name: "jbrowse_dojo.js",
+                resourceName: "jbrowse_dojo"
+            }
+        ]
+    }
+);
