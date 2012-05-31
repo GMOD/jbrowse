277312f71ce7d713fd891109d61ea2def24c86e9
Robert Buels <rbuels@gmail.com>
2012-05-31 18:17:57 -0400

    update dojo layers build

--- jslib/dojo/cache.js
+++ jslib/dojo/cache.js
@@ -1,54 +1,8 @@
 /*
-	Copyright (c) 2004-2009, The Dojo Foundation All Rights Reserved.
+	Copyright (c) 2004-2011, The Dojo Foundation All Rights Reserved.
 	Available via Academic Free License >= 2.1 OR the modified BSD license.
 	see: http://dojotoolkit.org/license for details
 */
 
 
-if(!dojo._hasResource["dojo.cache"]){
-dojo._hasResource["dojo.cache"]=true;
-dojo.provide("dojo.cache");
-(function(){
-var _1={};
-dojo.cache=function(_2,_3,_4){
-if(typeof _2=="string"){
-var _5=dojo.moduleUrl(_2,_3);
-}else{
-_5=_2;
-_4=_3;
-}
-var _6=_5.toString();
-var _7=_4;
-if(_4!==undefined&&!dojo.isString(_4)){
-_7=("value" in _4?_4.value:undefined);
-}
-var _8=_4&&_4.sanitize?true:false;
-if(_7||_7===null){
-if(_7==null){
-delete _1[_6];
-}else{
-_7=_1[_6]=_8?dojo.cache._sanitize(_7):_7;
-}
-}else{
-if(!(_6 in _1)){
-_7=dojo._getText(_6);
-_1[_6]=_8?dojo.cache._sanitize(_7):_7;
-}
-_7=_1[_6];
-}
-return _7;
-};
-dojo.cache._sanitize=function(_9){
-if(_9){
-_9=_9.replace(/^\s*<\?xml(\s)+version=[\'\"](\d)*.(\d)*[\'\"](\s)*\?>/im,"");
-var _a=_9.match(/<body[^>]*>\s*([\s\S]+)\s*<\/body>/im);
-if(_a){
-_9=_a[1];
-}
-}else{
-_9="";
-}
-return _9;
-};
-})();
-}
+if(!dojo._hasResource["dojo.cache"]){dojo._hasResource["dojo.cache"]=true;dojo.provide("dojo.cache");var cache={};dojo.cache=function(_1,_2,_3){if(typeof _1=="string"){var _4=dojo.moduleUrl(_1,_2);}else{_4=_1;_3=_2;}var _5=_4.toString();var _6=_3;if(_3!=undefined&&!dojo.isString(_3)){_6=("value" in _3?_3.value:undefined);}var _7=_3&&_3.sanitize?true:false;if(typeof _6=="string"){_6=cache[_5]=_7?dojo.cache._sanitize(_6):_6;}else{if(_6===null){delete cache[_5];}else{if(!(_5 in cache)){_6=dojo._getText(_5);cache[_5]=_7?dojo.cache._sanitize(_6):_6;}_6=cache[_5];}}return _6;};dojo.cache._sanitize=function(_8){if(_8){_8=_8.replace(/^\s*<\?xml(\s)+version=[\'\"](\d)*.(\d)*[\'\"](\s)*\?>/im,"");var _9=_8.match(/<body[^>]*>\s*([\s\S]+)\s*<\/body>/im);if(_9){_8=_9[1];}}else{_8="";}return _8;};}
