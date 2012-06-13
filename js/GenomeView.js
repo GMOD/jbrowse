dd88fdc2983b326bb0a5f91cf5b88220a3458643
Robert Buels <rbuels@gmail.com>
2012-06-13 15:24:40 -0400

    fix bug where GenomeView was not publishing /jbrowse/v1/v/tracks/show messages

--- js/GenomeView.js
+++ js/GenomeView.js
@@ -183,7 +183,19 @@ function GenomeView( browser, elem, stripeWidth, refseq, zoomLevel, browserRoot)
             });
 
     // subscribe to showTracks commands
-    this.browser.subscribe( '/dnd/drop',dojo.hitch(this, 'updateTrackList'));
+    this.browser.subscribe( '/dnd/drop', this, function( source, nodes, copy, target ) {
+        this.updateTrackList();
+        if( target.node === this.trackContainer ) {
+            // if dragging into the trackcontainer, we are showing some tracks
+            // get the configs from the tracks being dragged in
+            var confs = dojo.filter( dojo.map( nodes, function(n) {
+                                         return n.track && n.track.config;
+                                     }),
+                                     function(c) {return c;}
+                                   );
+            this.browser.publish( '/jbrowse/v1/v/tracks/show', [confs] );
+        }
+    });
     this.browser.subscribe( '/jbrowse/v1/c/tracks/show', this, 'showTracks' );
     this.browser.subscribe( '/jbrowse/v1/c/tracks/hide', this, 'hideTracks' );
 
