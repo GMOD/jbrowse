9a5ca40eb9f4a929e554bc3d159dc49972449726
Robert Buels <rbuels@gmail.com>
2012-07-05 11:06:20 -0400

    remove node-tap unit test infrastructure

--- tests/js_tests/tap_common.js
+++
@@ -1,9 +0,0 @@
-var requirejs = require('requirejs');
-requirejs.config({
-    nodeRequire: require,
-    baseUrl: 'src'
-});
-exports.require = requirejs;
-exports.tap = require('tap');
-exports.test = exports.tap.test;
-
