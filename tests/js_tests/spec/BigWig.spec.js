77f9cecbb90fafaca42fa581df8e50db91e44ca2
Robert Buels <rbuels@gmail.com>
2012-07-13 17:52:48 -0400

    add failing test for bigwig at low zoom level

--- tests/js_tests/spec/BigWig.spec.js
+++ tests/js_tests/spec/BigWig.spec.js
@@ -53,5 +53,23 @@ require(['JBrowse/Store/BigWig','JBrowse/Model/XHRBlob'], function( BigWig, XHRB
             });
         });
 
+        it('reads good data when zoomed very little', function() {
+            var v = b.getZoomedView( 17.34 );
+            var wigData;
+            v.readWigData( 'SL2.40ch01', 19999, 24999, function(features) {
+                wigData = features;
+            });
+            waitsFor(function() { return wigData; },1000);
+            runs(function() {
+                expect(wigData.length).toBeGreaterThan(19);
+                expect(wigData.length).toBeLessThan(100);
+                dojo.forEach( wigData, function(feature) {
+                    expect(feature.get('start')).toBeGreaterThan(80000);
+                    expect(feature.get('end')).toBeLessThan(2050000);
+                });
+                     //console.log(wigData);
+            });
+        });
+
     });
 });
