cad9d765a10486b36a48d55dff4ec243896b8444
Robert Buels <rbuels@gmail.com>
2012-07-17 10:20:31 -0400

    port JBrowse/Store to dojo declare

--- src/JBrowse/Store.js
+++ src/JBrowse/Store.js
@@ -1,45 +1,47 @@
-define( [],
-        function() {
+define( [ 'dojo/_base/declare'],
+        function( declare ) {
+
+return declare(null,
 
 /**
- * Base class for all JBrowse data stores.
- * @class
+ * @lends JBrowse.Store.prototype
  */
+{
+    /**
+     * Base class for all JBrowse data stores.
+     * @constructs
+     */
+    constructor: function( args ) {
+    },
 
-function Store( args ) {
-};
+    loadSuccess: function( data, url ) {
+    },
 
-Store.prototype.loadSuccess = function( data, url ) {
-};
+    loadFail: function(error) {
+        this.empty = true;
+        this.setLoaded();
+    },
 
-Store.prototype.loadFail = function(error) {
-    this.empty = true;
-    this.setLoaded();
-};
+    load: function(url) {
+        dojo.xhrGet({ url: url || this.url,
+                      handleAs: "json",
+                      failOk: true,
+                      load:  dojo.hitch( this, function(o) { this.loadSuccess(o, url); }),
+                      error: dojo.hitch( this, function(o) { this.loadFail(o, url);    })
+	            });
+    },
 
-Store.prototype.load = function(url) {
-    dojo.xhrGet({ url: url || this.url,
-                  handleAs: "json",
-                  failOk: true,
-                  load:  dojo.hitch( this, function(o) { this.loadSuccess(o, url); }),
-                  error: dojo.hitch( this, function(o) { this.loadFail(o, url);    })
-	        });
-};
+    setLoaded: function() {
+        this.loaded = true;
+        this.hideAll();
+        this.changed();
+    },
 
-Store.prototype.setLoaded = function() {
-    this.loaded = true;
-    this.hideAll();
-    this.changed();
-};
+    hideAll: function() {
+    },
 
+    changed: function() {
+    }
 
-Store.prototype.hideAll = function() {
-};
-
-
-Store.prototype.changed = function() {
-};
-
-return Store;
-
+});
 });
