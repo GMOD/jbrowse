48965ab57a0b957eb42f5196138be6d8b9c4f1fc
Robert Buels <rbuels@gmail.com>
2012-07-19 16:37:39 -0400

    add most commonly-used modules to main.js

--- src/JBrowse/main.js
+++ src/JBrowse/main.js
@@ -1,1 +1,19 @@
-require(['JBrowse/Browser']);
+// saves some loading time by loading most of the commonly-used
+// JBrowse modules at the outset
+require([
+            'JBrowse/Browser',
+            'JBrowse/ConfigAdaptor/JB_json_v1',
+            'JBrowse/View/TrackList/Simple',
+
+            'JBrowse/View/Track/Sequence',
+            'JBrowse/Store/Sequence/StaticChunked',
+
+            'JBrowse/View/Track/HTMLFeatures',
+            'JBrowse/Store/SeqFeature/NCList',
+
+            'JBrowse/View/TrackList/Simple',
+            'JBrowse/Store/TrackMetaData',
+
+            'dojox/gfx/svg',
+            'dojox/gfx/vml'
+        ]);
