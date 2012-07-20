bbf6f842314cf7fb138c84cf18c1b055329c829c
Robert Buels <rbuels@gmail.com>
2012-07-20 14:11:48 -0400

    add more commonly-used stuff to JBrowse/main

--- src/JBrowse/main.js
+++ src/JBrowse/main.js
@@ -5,11 +5,15 @@ require([
             'JBrowse/ConfigAdaptor/JB_json_v1',
             'JBrowse/View/TrackList/Simple',
 
-            'JBrowse/View/Track/Sequence',
+            // common stores
             'JBrowse/Store/Sequence/StaticChunked',
-
-            'JBrowse/View/Track/HTMLFeatures',
             'JBrowse/Store/SeqFeature/NCList',
+            'JBrowse/Store/TiledImage/Fixed',
+
+            // common track views
+            'JBrowse/View/Track/Sequence',
+            'JBrowse/View/Track/HTMLFeatures',
+            'JBrowse/View/Track/FixedImage/Wiggle',
 
             'JBrowse/View/TrackList/Simple',
             'JBrowse/Store/TrackMetaData',
