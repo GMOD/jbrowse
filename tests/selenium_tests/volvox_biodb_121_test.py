1ce7297e9d209cc9296ae4eb2ae2b5ba1fb5ea7d
Robert Buels <rbuels@gmail.com>
2012-08-01 11:50:53 -0400

    make 1.2.1 backcompat selenium test not test the context menus

--- tests/selenium_tests/volvox_biodb_121_test.py
+++ tests/selenium_tests/volvox_biodb_121_test.py
@@ -12,6 +12,29 @@ class VolvoxBiodbTest121 ( AbstractVolvoxBiodbTest, unittest.TestCase ):
         # actually running any formatting
         super( AbstractVolvoxBiodbTest, self ).setUp()
 
+    def test_volvox( self ):
+        # select "ctgA from the dropdown
+        self.select_refseq( 'ctgA' )
+
+        # check a good browser title
+        assert "ctgA" in self.browser.title, "browser title is actually %s" % self.browser.title
+
+        # do a test where we search for a certain gene using the search box
+        self.search_f15()
+
+        self.assert_no_js_errors()
+
+        # test scrolling, make sure we get no js errors
+        self.scroll()
+
+        # test dragging in and displaying the wiggle track
+        self.wiggle()
+
+        # test sequence track display
+        self.sequence()
+
+        self.browser.close()
+
     def baseURL( self ):
         if not self.base_url:
             self.base_url = re.sub('[^/]+$','compat_121.html',super( AbstractVolvoxBiodbTest, self ).baseURL() )
