d2188c9494ee3bcc8ffc4b9de9389e563a97da9c
Robert Buels <rbuels@gmail.com>
2012-08-25 14:31:19 -0400

    In simple tracklist, found a workaround that both prevents multi-selection and closes #89.
    Also reverts "put styles back in for tracklist multi-select.  might be confusing,", commit ccda0e2d319cbdeeb675c6c769fbc10154ff3f80.

--- src/JBrowse/View/TrackList/Simple.js
+++ src/JBrowse/View/TrackList/Simple.js
@@ -80,7 +80,8 @@ return declare( 'JBrowse.View.TrackList.Simple', null,
             { id: 'tracksAvail',
               className: 'container handles',
               style: { width: '100%', height: '100%', overflowX: 'hidden', overflowY: 'auto' },
-              innerHTML: '<h2>Available Tracks</h2>'
+              innerHTML: '<h2>Available Tracks</h2>',
+              onclick: dojo.hitch( this, function() { this.trackListWidget.selectNone(); } )
             },
             leftPane
         );
