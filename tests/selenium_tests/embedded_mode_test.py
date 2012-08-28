be5e5031b5856e98412a9e0fc50f6a8aeb2b952f
Robert Buels <rbuels@gmail.com>
2012-08-28 13:40:42 -0400

    improved embedded mode test to display an actual region

--- tests/selenium_tests/embedded_mode_test.py
+++ tests/selenium_tests/embedded_mode_test.py
@@ -1,11 +1,14 @@
 import unittest
+import time
 from volvox_biodb_test import AbstractVolvoxBiodbTest
 
 class EmbeddedModeTest( AbstractVolvoxBiodbTest, unittest.TestCase ):
     def baseURL( self ):
         if not self.base_url:
-            self.base_url = super( AbstractVolvoxBiodbTest, self ).baseURL() + '?nav=0&tracklist=0&overview=0&tracks=volvox_microarray.bw'
+            self.base_url = super( AbstractVolvoxBiodbTest, self ).baseURL() + '?nav=0&tracklist=0&overview=0&tracks=volvox_microarray.bw&loc=ctgA'
         return self.base_url
 
     def test_volvox( self ):
+        time.sleep(0.5)
         self.assert_no_js_errors()
+        self.browser.close()
