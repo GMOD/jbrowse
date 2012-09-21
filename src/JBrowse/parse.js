900d3a896ee57c72c0b9d19b841ab8bf88b562bf
Erik Derohanian <erik.derohanian@gmail.com>
2012-09-21 14:45:10 -0400

    Begun work on issue #140
    need to figure out how to read in files through JS

---
+++ src/JBrowse/parse.js
@@ -0,0 +1,62 @@
+//  script for parsing GBrowse config files
+
+/*  Based (now quite loosely) on
+ *  https://github.com/shockie/node-iniparser -> released under the MIT license
+ *  Copyright (c) 2009-2010 Jordy van Gelder <jordyvangelder@gmail.com>, see
+ *  https://github.com/shockie/node-iniparser/blob/master/README.md#license
+ *  for license details
+ */
+ 
+function parseGBConfig(file){
+
+    // var data = read in 'file' somhow I dunno how and google has failed me.
+
+    var regex = {
+        section: /^\s*\[\s*([^\]]*)\s*\]\s*$/,
+        param: /^\s*([\w\.\-\_\:\s]+)\s*=\s*(.*?)\s*$/,
+        halfParam: /^\s*([\w\.\-\_\:\s]+)\s*=\s*$/,
+        comment: /^\s*#*.*$/,
+        emptyLine: /^\s*$/
+    };
+    var value = {};
+    var lines = data.split(/\r\n|\r|\n/);
+    var section = null;
+
+    for (i = 0; i < lines.length; i++){
+        if(regex.comment.test(lines[i])){
+            return;
+        }else if(regex.param.test(lines[i])){
+            if (regex.halfParam.test(lines[i])){
+                var multiLineNum = i+1;
+                var tmpStrVal = '';
+                while  (! ( regex.param.test(lines[multiLineNum])   || regex.section.test(lines[multiLineNum])   )){
+                    if (! ( regex.comment.test(lines[multiLineNum]) || regex.emptyLine.test(lines[multiLineNum]) )){
+                        tmpStrVal += lines[multiLineNum];
+                    }
+                    multiLineNum++;
+                }
+                var match = line[i].match(regex.param);
+                if(section){
+                    value[section][match[1]] = tmpStrVal;
+                }else{
+                    value[match[1]] = tmpStrVal;
+                }
+                i = multiLineNum - 1;
+            }else{
+                var match = lines[i].match(regex.param);
+                if(section){
+                    value[section][match[1]] = match[2];
+                }else{
+                    value[match[1]] = match[2];
+                }
+            }
+        }else if(regex.section.test(line[i])){
+            var match = lines[i].match(regex.section);
+            value[match[1]] = {};
+            section = match[1];
+        }else if(lines[i].length == 0 && section){
+            section = null;
+        };
+    };
+    return value;
+}
---
+++ tests/js_tests/spec/GBrowseParser.spec.js
@@ -0,0 +1,34 @@
+describe( 'GMOD config file parser', function() {
+
+    // run the script with shortened ITAG2.3_genomic.conf.mas as input
+    // to test different kinds of input
+    // config file => GBrowseParseTestBasic.conf
+
+    var gbConfig = parseGBConfig('spec/GBrowseParseTestBasic.conf');
+
+    it ("Check the gbConfig values", function(){
+
+        expect(gbConfig).toBeTruthy();
+
+        expect(gbConfig['TRACK DEFAULTS']).toBeTruthy();
+        expect(gbConfig['TRACK DEFAULTS']['glyph'           ]).toBe('generic');
+        expect(gbConfig['TRACK DEFAULTS']['height'          ]).toBe(8);
+        expect(gbConfig['TRACK DEFAULTS']['bgcolor'         ]).toBe('cyan');
+        expect(gbConfig['TRACK DEFAULTS']['fgcolor'         ]).toBe('black');
+        expect(gbConfig['TRACK DEFAULTS']['label density'   ]).toBe(25);
+        expect(gbConfig['TRACK DEFAULTS']['bump density'    ]).toBe(25);
+
+        expect(gbConfig['Markers:region']).toBeTruthy();
+        expect(gbConfig['Markers:region']['feature'         ]).toBe('match:ITAG_sgn_markers');
+        expect(gbConfig['Markers:region']['ignore_sub_part' ]).toBe('match_part');
+        expect(gbConfig['Markers:region']['key'             ]).toBe('Markers');
+        expect(gbConfig['Markers:region']['fgcolor'         ]).toBe('black');
+        expect(gbConfig['Markers:region']['bgcolor'         ]).toBe('yellow');
+        expect(gbConfig['Markers:region']['glyph'           ]).toBe('generic');
+        expect(gbConfig['Markers:region']['label density'   ]).toBe(100);
+        expect(gbConfig['Markers:region']['bump density'    ]).toBe(100);
+        expect(gbConfig['Markers:region']['link'            ]).toBe('/search/quick?term=$name');
+        expect(gbConfig['Markers:region']['citation'        ]).toBe('GenomeThreader alignments of SGN marker sequences.');
+
+    });
+});
