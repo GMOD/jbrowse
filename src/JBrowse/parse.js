4da1a54e26e68dcf9dba6d77e5e19144475541a8
Erik Derohanian <erik.derohanian@gmail.com>
2012-10-10 14:11:42 -0400

    Rearranged code into pseudo-state machine. Almost clear to read now.

--- src/JBrowse/parse.js
+++ src/JBrowse/parse.js
@@ -1,12 +1,13 @@
-//  script for parsing GBrowse config files
+// Function for parsing GBrowse config files
+// Input : The text in a file (with newlines)
+// Output : a JS object
 
-/*  Based (now quite loosely) on
- *  https://github.com/shockie/node-iniparser -> released under the MIT license
- */
- 
+// Based (now extremely loosely) on
+// https://github.com/shockie/node-iniparser -> released under the MIT license
+
 function parseGBConfig(data){
 
-    var regex = {
+    this.regex = {
         section:   /^\s*\[\s*([^\]]*)\s*\]\s*$/,
         param:     /^(\w[\w\.\-\_\:\s]+)=\s*(.*?)\s*$/,
         halfParam: /^(\w[\w\.\-\_\:\s]+)=\s*$/,
@@ -14,55 +15,72 @@ function parseGBConfig(data){
         emptyLine: /^\s*$/,
         newLine:   /\r\n|\r|\n/
     };
-    var value = {};
-    var lines = data.split(regex.newLine);
-    var section = null;
-    var n = lines.length;
+    this.value = {};
+    this.lines = data.split(this.regex.newLine);
+    this.section = null;
+    var n = this.lines.length;
+
+
     for (var i = 0; i < n; i++){
-        if(regex.comment.test(lines[i])){
+        if(this.regex.comment.test(this.lines[i])){
             // do nothing, since it's a comment.
-        }else if(regex.param.test(lines[i])){
-            if (regex.halfParam.test(lines[i])){
-                var multiLineNum = i+1;
-                var tmpStrVal = '';
-                while (typeof lines[multiLineNum] !== 'undefined' && (! ( regex.param.test(lines[multiLineNum])   || regex.section.test(lines[multiLineNum])  ))){
-                    if (! ( regex.comment.test(lines[multiLineNum]) || regex.emptyLine.test(lines[multiLineNum]) )){
-                        if (tmpStrVal === ''){
-                            tmpStrVal += lines[multiLineNum].trim();
-                        } else {
-                            tmpStrVal = tmpStrVal + ' ' + lines[multiLineNum].trim();
-                        }
-                    }
-                    multiLineNum++;
-                }
-                var match = lines[i].match(regex.param);
-                if(section){
-                    value[section][match[1].trim()] = tmpStrVal;
-                }else{
-                    value[match[1].trim()] = tmpStrVal;
-                }
-                i = multiLineNum - 1;
-            }else{
-                var match = lines[i].match(regex.param);
-                if(section){
-                    if (!isNaN(match[2])){
-                        match[2] = parseInt(match[2]);
-                    }
-                    value[section][match[1].trim()] = match[2];
-                }else{
-                    if (!isNaN(match[2].trim())){
-                        match[2] = parseInt(match[2].trim());
-                    }
-                    value[match[1].trim()] = match[2].trim();
-                }
-            }
-        }else if(regex.section.test(lines[i])){
-            var match = lines[i].match(regex.section);
-            value[match[1].trim()] = {};
-            section = match[1].trim();
-        }else if(lines[i].length == 0 && section){
-            section = null;
+        }else if(this.regex.param.test(this.lines[i])){
+            i = itIsParam(i);
+        }else if(this.regex.section.test(this.lines[i])){
+            isSection(i);
+        }else if(this.lines[i].length == 0 && this.section){
+            this.section = null;
         };
     };
-    return value;
+
+
+    return this.value; //Returns the JS object
+}
+
+function itIsParam(i){
+    if (this.regex.halfParam.test(this.lines[i])){
+        return multiLine (i);
+    }else{
+        var match = this.lines[i].match(this.regex.param);
+        if(this.section){
+            if (!isNaN(match[2])){
+                match[2] = parseInt(match[2]);
+            }
+            this.value[this.section][match[1].trim()] = match[2];
+        }else{
+            if (!isNaN(match[2].trim())){
+                match[2] = parseInt(match[2].trim());
+            }
+            this.value[match[1].trim()] = match[2].trim();
+        }
+        return i; //don't change
+    }
+}
+
+function multiLine (i){
+    var multiLineNum = i+1;
+    var tmpStrVal = '';
+    while (typeof this.lines[multiLineNum] !== 'undefined' && (! ( this.regex.param.test(this.lines[multiLineNum])   || this.regex.section.test(this.lines[multiLineNum])  ))){
+        if (! ( this.regex.comment.test(this.lines[multiLineNum]) || this.regex.emptyLine.test(this.lines[multiLineNum]) )){
+            if (tmpStrVal === ''){
+                tmpStrVal += this.lines[multiLineNum].trim();
+            } else {
+                tmpStrVal = tmpStrVal + ' ' + this.lines[multiLineNum].trim();
+            }
+        }
+        multiLineNum++;
+    }
+    var match = this.lines[i].match(this.regex.param);
+    if(this.section){
+        this.value[this.section][match[1].trim()] = tmpStrVal;
+    }else{
+        this.value[match[1].trim()] = tmpStrVal;
+    }
+    return multiLineNum - 1; //The next line it should read
+}
+
+function isSection (i){
+    var match = this.lines[i].match(this.regex.section);
+    this.value[match[1].trim()] = {};
+    this.section = match[1].trim();
 }
