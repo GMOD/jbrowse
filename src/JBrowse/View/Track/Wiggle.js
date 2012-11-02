2895ebc505ed739ccb9f0feb38c691c33da97e4e
Robert Buels <rbuels@gmail.com>
2012-11-02 13:30:51 -0400

    refactor Wiggle track  some moreto make it easier  to extend

--- src/JBrowse/View/Track/Wiggle.js
+++ src/JBrowse/View/Track/Wiggle.js
@@ -1,11 +1,13 @@
-define( ['dojo/_base/declare',
-         'dojo/on',
-         'JBrowse/View/Track/Canvas',
-         'JBrowse/View/Track/YScaleMixin',
-         'JBrowse/View/Track/ExportMixin',
-         'JBrowse/Util'
+define( [
+            'dojo/_base/declare',
+            'dojo/_base/array',
+            'dojo/on',
+            'JBrowse/View/Track/Canvas',
+            'JBrowse/View/Track/YScaleMixin',
+            'JBrowse/View/Track/ExportMixin',
+            'JBrowse/Util'
         ],
-        function( declare, on, CanvasTrack, YScaleMixin, ExportMixin, Util ) {
+        function( declare, array, on, CanvasTrack, YScaleMixin, ExportMixin, Util ) {
 var Wiggle = declare( CanvasTrack, {
     constructor: function( args ) {
         this.store = args.store;
@@ -184,11 +186,6 @@ Wiggle.extend({
         var canvasHeight = parseInt(( this.config.style || {}).height) || 100;
         this.height = canvasHeight;
 
-        var posColor  = this.config.style.pos_color || '#00f';
-        var negColor  = this.config.style.neg_color || '#f00';
-        var clipColor = this.config.style.clip_marker_color;
-        var disableClipMarkers = this.config.disable_clip_markers;
-
         this.readWigData( scale, this.refSeq.name, leftBase, rightBase+1, dojo.hitch(this,function( features ) {
                 if(! this.yscale )
                     this.makeWiggleYScale();
@@ -203,135 +200,17 @@ Wiggle.extend({
                     block
                 );
                 c.startBase = leftBase;
-                var context = c && c.getContext && c.getContext('2d');
-                if( context && this.scale && this.scale.toY && features ) {
-                        var toY = dojo.hitch( this, this.scale.toY, c.height );
-                        var originY = toY( this.scale.origin );
+                if( c && c.getContext && c.getContext('2d') && this.scale && this.scale.toY && features ) {
 
-                        //context.fillText(features.length+' spans', 10,10);
-                        //console.log( 'filling '+leftBase+'-'+rightBase);
-                        var pixelValues = new Array( c.width );
-                        dojo.forEach(features, function(f) {
-                            //console.log( f.get('start') +'-'+f.get('end')+':'+f.get('score') );
-                            var score = f.get('score');
-                            var rTop = toY( score );
-                            if( rTop <= canvasHeight ) {
-                                var rWidth = Math.ceil( ( f.get('end')   - f.get('start') ) * scale );
-                                var rLeft  = Math.floor(( f.get('start') - leftBase       ) * scale );
+                    var featureRects = array.map( features, function(f) {
+                        return this._featureRect( scale, leftBase, c, f );
+                    }, this );
 
-                                // if rLeft is negative (off the left
-                                // side of the canvas), clip off the
-                                // (possibly large!) non-visible
-                                // portion
-                                if( rLeft < 0 ) {
-                                    rWidth += rLeft;
-                                    rLeft  = 0;
-                                }
-                                // also don't let rWidth get overly big
-                                if( rWidth > canvasWidth ) {
-                                    rWidth = canvasWidth;
-                                }
+                    this._preDraw(      scale, leftBase, rightBase, block, c, features, featureRects );
+                    this._drawFeatures( scale, leftBase, rightBase, block, c, features, featureRects );
+                    this._postDraw(     scale, leftBase, rightBase, block, c, features, featureRects );
 
-                                this._updatePixelValues( pixelValues, rLeft, rWidth, f );
-                                if( rTop <= originY ) {
-                                    // bar goes upward
-                                    context.fillStyle = posColor;
-                                    context.fillRect( rLeft, rTop, rWidth, originY-rTop);
-                                    if( !disableClipMarkers && rTop < 0 ) { // draw clip marker if necessary
-                                        context.fillStyle = clipColor || negColor;
-                                        context.fillRect( rLeft, 0, rWidth, 2 );
-                                    }
-                                }
-                                else {
-                                    // bar goes downward
-                                    context.fillStyle = negColor;
-                                    context.fillRect( rLeft, originY, rWidth, canvasHeight-rTop );
-                                    if( !disableClipMarkers && rTop >= canvasHeight ) { // draw clip marker if necessary
-                                        context.fillStyle = clipColor || posColor;
-                                        context.fillRect( rLeft, canvasHeight-3, rWidth, 2 );
-                                    }
-                                }
-                            }
-                        }, this );
-
-                        // draw the variance_band if requested
-                        if( this.config.variance_band ) {
-                            var stats = this.getGlobalStats();
-                            if( stats && ('scoreMean' in stats) && ('scoreStdDev' in stats) ) {
-                                var drawVarianceBand = function( plusminus, fill, label ) {
-                                    context.fillStyle = fill;
-                                    var varTop = toY( stats.scoreMean + plusminus );
-                                    var varHeight = toY( stats.scoreMean - plusminus ) - varTop;
-                                    varHeight = Math.max( 1, varHeight );
-                                    context.fillRect( 0, varTop, c.width, varHeight );
-                                    context.font = '12px sans-serif';
-                                    if( plusminus > 0 ) {
-                                        context.fillText( '+'+label, 2, varTop );
-                                        context.fillText( '-'+label, 2, varTop+varHeight );
-                                    }
-                                    else {
-                                        context.fillText( label, 2, varTop );
-                                    }
-                                };
-                                drawVarianceBand( 2*stats.scoreStdDev, 'rgba(0,0,0,0.12)', '2σ' );
-                                drawVarianceBand( stats.scoreStdDev, 'rgba(0,0,0,0.25)', '1σ' );
-                                drawVarianceBand( 0, 'rgba(255,255,0,0.7)', 'mean' );
-                            }
-                        }
-
-                        var scoreDisplay = dojo.create(
-                            'div', {
-                                className: 'wiggleValueDisplay',
-                                style: {
-                                    position: 'absolute',
-                                    display: 'none',
-                                    top: 0,
-                                    left: 0,
-                                    zIndex: 15
-                                }
-                            }, block );
-                        var verticalLine = dojo.create( 'div', {
-                                className: 'trackVerticalPositionIndicator',
-                                style: {
-                                    position: 'absolute',
-                                    top: 0,
-                                    display: 'none',
-                                    cursor: 'default',
-                                    left: '-2px',
-                                    height: c.height+'px',
-                                    width: '1px',
-                                    borderWidth: '0',
-                                    zIndex: 15
-                                }
-                        }, block);
-                        var outTO;
-                        on( c, 'mousemove', dojo.hitch(this,function(evt) {
-                                if( outTO ) {
-                                    window.clearTimeout( outTO );
-                                    outTO = null;
-                                }
-                                var x = ( evt.offsetX || evt.layerX )-3;
-                                verticalLine.style.display = 'block';
-                                verticalLine.style.left = x+'px';
-
-                                if( this._showPixelValue( scoreDisplay, pixelValues[x] ) ) {
-                                    scoreDisplay.style.left = x+'px';
-                                    scoreDisplay.style.display = 'block';
-                                } else {
-                                    scoreDisplay.style.display = 'none';
-                                }
-                        }));
-                        on( c, 'mouseout', function(evt) {
-                                outTO = window.setTimeout( function() {
-                                    scoreDisplay.style.display = 'none';
-                                    verticalLine.style.display = 'none';
-                                }, 50 );
-                        });
-                        on( c, 'mousein', function(evt) {
-                                    scoreDisplay.style.display = 'block';
-                                    verticalLine.style.display = 'block';
-                        });
-
+                    this._makeScoreDisplay( scale, leftBase, rightBase, block, c, features, featureRects );
 
                     this.heightUpdate( c.height, blockIndex );
                     c.className = 'canvas-track';
@@ -363,13 +242,184 @@ Wiggle.extend({
             }));
     },
 
-    _updatePixelValues: function( pixelValues, rLeft, rWidth, f ) {
-        var iend = rLeft+rWidth;
-        var score = f.get('score');
-        for( var i = rLeft; i < iend; i++ ) {
-            pixelValues[i] = i in pixelValues ? Math.max( pixelValues[i], score ) : score;
+    /**
+     * Calculate the left and width, in pixels, of where this feature
+     * will be drawn on the canvas.
+     * @private
+     * @returns {Object} with l, r, and w
+     */
+    _featureRect: function( scale, leftBase, canvas, feature ) {
+        var fRect = {
+            w: Math.ceil( ( feature.get('end')   - feature.get('start') ) * scale ),
+            l: Math.floor(( feature.get('start') - leftBase       ) * scale )
+        };
+
+        // if fRect.l is negative (off the left
+        // side of the canvas), clip off the
+        // (possibly large!) non-visible
+        // portion
+        if( fRect.l < 0 ) {
+            fRect.w += fRect.l;
+            fRect.l  = 0;
+        }
+
+        // also don't let fRect.w get overly big
+        fRect.w = Math.min( canvas.width-fRect.l, fRect.w );
+        fRect.r = fRect.w + fRect.l;
+
+        return fRect;
+    },
+
+    _preDraw: function( canvas ) {
+    },
+
+    /**
+     * Draw a set of features on the canvas.
+     * @private
+     */
+    _drawFeatures: function( scale, leftBase, rightBase, block, canvas, features, featureRects ) {
+        var context = canvas.getContext('2d');
+        var toY = dojo.hitch( this, this.scale.toY, canvas.height );
+        var originY = toY( this.scale.origin );
+        var canvasHeight = canvas.height;
+
+        var posColor  = this.config.style.pos_color || '#00f';
+        var negColor  = this.config.style.neg_color || '#f00';
+        var clipColor = this.config.style.clip_marker_color;
+        var disableClipMarkers = this.config.disable_clip_markers;
+
+        dojo.forEach( features, function(f,i) {
+
+            var fRect = featureRects[i];
+
+            //console.log( f.get('start') +'-'+f.get('end')+':'+f.get('score') );
+            var score = f.get('score');
+            fRect.t = toY( score );
+
+            if( fRect.t <= canvasHeight ) { // if the rectangle is visible at all
+
+                if( fRect.t <= originY ) {
+                    // bar goes upward
+                    context.fillStyle = posColor;
+                    context.fillRect( fRect.l, fRect.t, fRect.w, originY-fRect.t);
+                    if( !disableClipMarkers && fRect.t < 0 ) { // draw clip marker if necessary
+                        context.fillStyle = clipColor || negColor;
+                        context.fillRect( fRect.l, 0, fRect.w, 2 );
+                    }
+                }
+                else {
+                    // bar goes downward
+                    context.fillStyle = negColor;
+                    context.fillRect( fRect.l, originY, fRect.w, canvasHeight-fRect.t );
+                    if( !disableClipMarkers && fRect.t >= canvasHeight ) { // draw clip marker if necessary
+                        context.fillStyle = clipColor || posColor;
+                        context.fillRect( fRect.l, canvasHeight-3, fRect.w, 2 );
+                    }
+                }
+            }
+        }, this );
+    },
+
+    /**
+     * Draw anything needed after the features are drawn.
+     */
+    _postDraw: function( scale, leftBase, rightBase, block, canvas, features, featureRects ) {
+        var context = canvas.getContext('2d');
+
+        // draw the variance_band if requested
+        if( this.config.variance_band ) {
+            var stats = this.getGlobalStats();
+            if( stats && ('scoreMean' in stats) && ('scoreStdDev' in stats) ) {
+                var drawVarianceBand = function( plusminus, fill, label ) {
+                    context.fillStyle = fill;
+                    var varTop = toY( stats.scoreMean + plusminus );
+                    var varHeight = toY( stats.scoreMean - plusminus ) - varTop;
+                    varHeight = Math.max( 1, varHeight );
+                    context.fillRect( 0, varTop, canvas.width, varHeight );
+                    context.font = '12px sans-serif';
+                    if( plusminus > 0 ) {
+                        context.fillText( '+'+label, 2, varTop );
+                        context.fillText( '-'+label, 2, varTop+varHeight );
+                    }
+                    else {
+                        context.fillText( label, 2, varTop );
+                    }
+                };
+                drawVarianceBand( 2*stats.scoreStdDev, 'rgba(0,0,0,0.12)', '2σ' );
+                drawVarianceBand( stats.scoreStdDev, 'rgba(0,0,0,0.25)', '1σ' );
+                drawVarianceBand( 0, 'rgba(255,255,0,0.7)', 'mean' );
+            }
         }
     },
+
+    _makeScoreDisplay: function( scale, leftBase, rightBase, block, canvas, features, featureRects ) {
+
+        // make an array of the max score at each pixel on the canvas
+        var pixelValues = new Array( canvas.width );
+        dojo.forEach( features, function( f, i ) {
+            var fRect = featureRects[i];
+            var jEnd = fRect.r;
+            var score = f.get('score');
+            for( var j = fRect.l; j < jEnd; j++ ) {
+                pixelValues[j] = j in pixelValues ? Math.max( pixelValues[j], score ) : score;
+            }
+        },this);
+
+        // make elements and events to display it
+        var scoreDisplay = dojo.create(
+            'div', {
+                className: 'wiggleValueDisplay',
+                style: {
+                    position: 'absolute',
+                    display: 'none',
+                    top: 0,
+                    left: 0,
+                    zIndex: 15
+                }
+            }, block );
+        var verticalLine = dojo.create( 'div', {
+                className: 'trackVerticalPositionIndicator',
+                style: {
+                    position: 'absolute',
+                    top: 0,
+                    display: 'none',
+                    cursor: 'default',
+                    left: '-2px',
+                    height: canvas.height+'px',
+                    width: '1px',
+                    borderWidth: '0',
+                    zIndex: 15
+                }
+        }, block);
+        var outTimeout;
+        on( canvas, 'mousemove', dojo.hitch(this,function(evt) {
+                if( outTimeout ) {
+                    window.clearTimeout( outTimeout );
+                    outTimeout = null;
+                }
+                var x = ( evt.offsetX || evt.layerX )-3;
+                verticalLine.style.display = 'block';
+                verticalLine.style.left = x+'px';
+
+                if( this._showPixelValue( scoreDisplay, pixelValues[x] ) ) {
+                    scoreDisplay.style.left = x+'px';
+                    scoreDisplay.style.display = 'block';
+                } else {
+                    scoreDisplay.style.display = 'none';
+                }
+        }));
+        on( canvas, 'mouseout', function(evt) {
+                outTimeout = window.setTimeout( function() {
+                    scoreDisplay.style.display = 'none';
+                    verticalLine.style.display = 'none';
+                }, 50 );
+        });
+        on( canvas, 'mousein', function(evt) {
+                    scoreDisplay.style.display = 'block';
+                    verticalLine.style.display = 'block';
+        });
+    },
+
     _showPixelValue: function( scoreDisplay, score ) {
         if( typeof score == 'number' ) {
             // display the score with only 6
