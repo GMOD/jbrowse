b88c9c5c0360c205bd9697c67176d25379aa3691
Robert Buels <rbuels@gmail.com>
2012-11-12 14:56:45 -0800

    import CodonTable.js

---
+++ src/JBrowse/CodonTable.js
@@ -0,0 +1,100 @@
+define( [],
+        function() {
+
+var CodonTable = {
+    "TCA" : "S",
+    "TCC" : "S",
+    "TCG" : "S",
+    "TCT" : "S",
+    "TTC" : "F",
+    "TTT" : "F",
+    "TTA" : "L",
+    "TTG" : "L",
+    "TAC" : "Y",
+    "TAT" : "Y",
+    "TAA" : "*",
+    "TAG" : "*",
+    "TGC" : "C",
+    "TGT" : "C",
+    "TGA" : "*",
+    "TGG" : "W",
+    "CTA" : "L",
+    "CTC" : "L",
+    "CTG" : "L",
+    "CTT" : "L",
+    "CCA" : "P",
+    "CCC" : "P",
+    "CCG" : "P",
+    "CCT" : "P",
+    "CAC" : "H",
+    "CAT" : "H",
+    "CAA" : "Q",
+    "CAG" : "Q",
+    "CGA" : "R",
+    "CGC" : "R",
+    "CGG" : "R",
+    "CGT" : "R",
+    "ATA" : "I",
+    "ATC" : "I",
+    "ATT" : "I",
+    "ATG" : "M",
+    "ACA" : "T",
+    "ACC" : "T",
+    "ACG" : "T",
+    "ACT" : "T",
+    "AAC" : "N",
+    "AAT" : "N",
+    "AAA" : "K",
+    "AAG" : "K",
+    "AGC" : "S",
+    "AGT" : "S",
+    "AGA" : "R",
+    "AGG" : "R",
+    "GTA" : "V",
+    "GTC" : "V",
+    "GTG" : "V",
+    "GTT" : "V",
+    "GCA" : "A",
+    "GCC" : "A",
+    "GCG" : "A",
+    "GCT" : "A",
+    "GAC" : "D",
+    "GAT" : "D",
+    "GAA" : "E",
+    "GAG" : "E",
+    "GGA" : "G",
+    "GGC" : "G",
+    "GGG" : "G",
+    "GGT" : "G"
+};
+
+var tempCodonTable = { };
+for (var codon in CodonTable) {
+    // looping through codon table, make sure no hitting generic properties...
+    if (CodonTable.hasOwnProperty(codon)) {
+	var aa = CodonTable[codon];
+	// console.log("Codon: ", codon, ", aa: ", aa);
+	var nucs = [];
+	for (var i=0; i<3; i++) {
+	    var nuc = codon.charAt(i);
+	    nucs[i] = [];
+	    nucs[i][0] = nuc.toUpperCase();
+	    nucs[i][1] = nuc.toLowerCase();
+	}
+	for (var i=0; i<2; i++) {
+	    var n0 = nucs[0][i];
+	    for (var j=0; j<2; j++) {
+		var n1 = nucs[1][j];
+		for (var k=0; k<2; k++) {
+		    var n2 = nucs[2][k];
+		    var triplet = n0 + n1 + n2;
+		    tempCodonTable[triplet] = aa;
+		    // console.log("triplet: ", triplet, ", aa: ", aa );
+		}
+	    }
+	}
+    }
+}
+
+return tempCodonTable;
+});
