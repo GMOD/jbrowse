14b7960b41ff286c0c0fec36be9a67e9daa5b049
Robert Buels <rbuels@gmail.com>
2012-11-14 17:06:30 -0800

    add an experimental WebApollo/Store/SeqFeature//ScratchPad for use in WA user-annotated tracks

---
+++ plugins/WebApollo/js/Store/SeqFeature/ScratchPad.js
@@ -0,0 +1,57 @@
+define( ['dojo/_base/declare',
+         'JBrowse/Store/SeqFeature'
+        ],
+        function( declare, SeqFeatureStore ) {
+
+return declare( SeqFeatureStore,
+{
+    constructor: function( args ) {
+        this.features = {};
+        this._calculateStats();
+    },
+
+    insert: function( feature ) {
+        this.features[feature._uniqueID] = feature;
+        this._calculateStats();
+    },
+
+    delete: function( feature ) {
+        delete this.features[ feature._uniqueID ];
+        this._calculateStats();
+    },
+
+    _calculateStats: function() {
+        var minStart = Infinity;
+        var maxEnd = -Infinity;
+        var featureCount = 0;
+        for( var id in this.features ) {
+            var f = this.features[id];
+            var s = f.get('start');
+            var e = f.get('end');
+            if( s < minStart )
+                minStart = s;
+
+            if( e > maxEnd )
+                maxEnd = e;
+
+            featureCount++;
+        }
+
+        this.globalStats = {
+            featureDensity: featureCount/(maxEnd-minStart+1)
+        };
+    },
+
+    getFeatures: function( query, featCallback, endCallback, errorCallback ) {
+        var start = query.start;
+        var end = query.end;
+        for( var id in this.features ) {
+            var f = this.features[id];
+            if(! ( f.get('end') < start  || f.get('start') > end ) ) {
+                featCallback( f );
+            }
+        }
+        endCallback();
+    }
+});
+});
