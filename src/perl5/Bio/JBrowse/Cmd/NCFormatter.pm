GMOD/jbrowse

commit 44a931025ad79e3b21997fd8a28503ff0e421bb3

make perl NCFormatter base class support a transform_feature method that subclasses can use to munge features before writing them out

src/perl5/Bio/JBrowse/Cmd/NCFormatter.pm
@@ -37,6 +37,8 @@ sub _format {
             my $chrom = $feat->{seq_id};
             $featureCounts{$chrom} += 1;
 
+            $feat = $self->transform_feature( $feat );
+
             my $row = [ $chrom,
                         $feature_stream->flatten_to_feature( $feat ),
                         $feature_stream->flatten_to_name( $feat ),
@@ -84,6 +86,11 @@ sub _format {
     if( !$totalMatches && @$types ) {
         warn "WARNING: No matching features found for @$types\n";
     }
+}
+
+# stub
+sub transform_feature {
+    return $_[0];
 }
 
 1;

commit 450a7a2d95ba46395e4ba5f6c7c010be8577131b

fix my broken transform_feature stub from 44a931025ad79e3b21997fd8a28503ff0e421bb3

src/perl5/Bio/JBrowse/Cmd/NCFormatter.pm
@@ -90,7 +90,7 @@ sub _format {
 
 # stub
 sub transform_feature {
-    return $_[0];
+    return $_[1];
 }
 
 1;