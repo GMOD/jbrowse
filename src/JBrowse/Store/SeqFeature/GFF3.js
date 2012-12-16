GMOD/jbrowse

commit 9b0f041f746c598e5c6e47c04f9b165a871c6d01

GFF3 inloading stuff - added test and code to make sure GFF3toJBrowseJson returns both mRNAs when given a gene with two mRNA children

src/JBrowse/Store/SeqFeature/GFF3.js
@@ -89,7 +89,6 @@ return declare([ NCListStore ],
         console.log("   content start: " + content.substr(0, 50));
         var gparser = new GFF3Parser();
         var gff3_json = gparser.parse(content);
-        console.log(gff3_json);
         var results = this._gff3toJbrowseJson(gff3_json, store.args);
         var trackInfo = results.trackInfo;
         var featArray = results.featArray;
@@ -208,58 +207,61 @@ return declare([ NCListStore ],
             return null;
         }
 
-        //
-        // set parent info
-        //
-        var rawdata = theseParents[0].data[0].rawdata;
-        featureArray[1] = parseInt(rawdata[3])-1; // set start (-1 for converting from 1-based to 0-based)
-        featureArray[2] = parseInt(rawdata[4]); // set end
-        featureArray[3] = rawdata[6]; // set strand
-        featureArray[4] = rawdata[1]; // set source
-        featureArray[5] = rawdata[7]; // set phase
-        featureArray[6] = rawdata[2]; // set type
-        featureArray[7] = rawdata[5]; // set score
-        featureArray[8] = theseParents[0].ID; // set id
-
-        var parsedNinthField = this._parsedNinthGff3Field(rawdata[8]);
-        if ( !!parsedNinthField["Name"] ){
-            featureArray[9] = parsedNinthField["Name"];
-        }
-        else  { featureArray[9] = null; }
-
-        //
-        // now set children info
-        //
-        var children = theseParents[0].children;
-        var subfeats = null; // make array for all child features
-        if ( theseParents[0].children && (theseParents[0].children.length > 0))  {
-            subfeats = [];
-            for (var i = 0; i < theseParents[0].children.length; i++ ){
-                var childData = theseParents[0].children[i].data[0].rawdata;
-                var subfeat = [];
-
-                subfeat[0] = 1; // ?
-                subfeat[1] = parseInt(childData[3])-1; // start  (-1 for converting from 1-based to 0-based)
-                subfeat[2] = parseInt(childData[4]); // end
-                subfeat[3] = childData[6]; // strand
-                subfeat[4] = childData[1]; // source
-                subfeat[5] = childData[7]; // phase
-                subfeat[6] = childData[2]; // type
-                subfeat[7] = childData[5]; // score
-
-                var childNinthField = this._parsedNinthGff3Field( childData[8] );
-                if ( !!childNinthField["ID"] ){
-                    subfeat[8] = childNinthField["ID"];
-                }
-                else  { subfeat[8] = null; }
-                if ( !!childNinthField["Name"] ){
-                    subfeat[9] = childNinthField["Name"];
-                }
+	for ( j = 0; j < theseParents.length; j++ ){
+	    console.log("j: " + j);
+	    //
+	    // set parent info
+	    //
+	    var rawdata = theseParents[j].data[0].rawdata;
+	    featureArray[1] = parseInt(rawdata[3])-1; // set start (-1 for converting from 1-based to 0-based)
+	    featureArray[2] = parseInt(rawdata[4]); // set end
+	    featureArray[3] = rawdata[6]; // set strand
+	    featureArray[4] = rawdata[1]; // set source
+	    featureArray[5] = rawdata[7]; // set phase
+	    featureArray[6] = rawdata[2]; // set type
+	    featureArray[7] = rawdata[5]; // set score
+	    featureArray[8] = theseParents[j].ID; // set id
+	    
+	    var parsedNinthField = this._parsedNinthGff3Field(rawdata[8]);
+	    if ( !!parsedNinthField["Name"] ){
+		featureArray[9] = parsedNinthField["Name"];
+	    }
+	    else  { featureArray[9] = null; }
+	    
+	    //
+	    // now set children info
+	    //
+	    var children = theseParents[j].children;
+	    var subfeats = null; // make array for all child features
+	    if ( theseParents[j].children && (theseParents[j].children.length > 0))  {
+		subfeats = [];
+		for (var i = 0; i < theseParents[j].children.length; i++ ){
+		    var childData = theseParents[j].children[i].data[0].rawdata;
+		    var subfeat = [];
+		    
+		    subfeat[0] = 1; // ?
+		    subfeat[1] = parseInt(childData[3])-1; // start  (-1 for converting from 1-based to 0-based)
+		    subfeat[2] = parseInt(childData[4]); // end
+		    subfeat[3] = childData[6]; // strand
+		    subfeat[4] = childData[1]; // source
+		    subfeat[5] = childData[7]; // phase
+		    subfeat[6] = childData[2]; // type
+		    subfeat[7] = childData[5]; // score
+		    
+		    var childNinthField = this._parsedNinthGff3Field( childData[8] );
+		    if ( !!childNinthField["ID"] ){
+			subfeat[8] = childNinthField["ID"];
+		    }
+		    else  { subfeat[8] = null; }
+		    if ( !!childNinthField["Name"] ){
+			subfeat[9] = childNinthField["Name"];
+		    }
                 else  { subfeat[9] = null; }
-                subfeats[i] = subfeat;
-            }
-        }
-        featureArray[10] = subfeats; // load up children
+		    subfeats[i] = subfeat;
+		}
+	    }
+	    featureArray[10] = subfeats; // load up children
+	}
 
         return featureArray;
     },
@@ -296,17 +298,17 @@ return declare([ NCListStore ],
     // that depth)
     _getFeaturesAtGivenDepth: function(gffFeature, depth) {
         var recursion_level = 0;
-        var maximum_recursion_level = 10; // paranoid about infinite recursion
+	var maximum_recursion_level = 20; // paranoid about infinite recursion
         var getFeature = function(thisJsonFeature, thisDepth) {
-            recursion_level++;
             if ( recursion_level > maximum_recursion_level ){
                 return null;
             }
             // are we at the right depth?
-            if ( recursion_level == thisDepth ){
+            if ( recursion_level + 1 == thisDepth ){
                 return thisJsonFeature;
             }
             else if ( thisJsonFeature.children != null && thisJsonFeature.children.length > 0 ){
+		recursion_level++;
                 var returnedFeatures = new Array;
 		for (var m = 0; m < thisJsonFeature.children.length; m++){
 		    if ( thisFeature = getFeature(thisJsonFeature.children[m], depth) ){
@@ -317,7 +319,6 @@ return declare([ NCListStore ],
             }
             return null;
         };
-        //    return getFeature( parsedGff3.parsedData[0], depth );
         return getFeature( gffFeature, depth );
     },
 

tests/js_tests/spec/GFF3toJbrowseJsonSpec.js
@@ -23,9 +23,6 @@ require(['JBrowse/Store/SeqFeature/GFF3/GFF3Parser','JBrowse/Store/SeqFeature/GF
 			gparser = new GFF3Parser();
 			parsedGFF3toJbrowseJsonInput = gparser.parse( makerGff3String );
 			actualJbrowseJsonOutput = store._gff3toJbrowseJson( parsedGFF3toJbrowseJsonInput );
-
-			parsedGFF3toJbrowseJsonInput2 = gparser.parse( makerGff3String2 );
-			actualJbrowseJsonOutput2 = store._gff3toJbrowseJson( parsedGFF3toJbrowseJsonInput2 );
 		    });
 		
 		it("should respond to _gff3toJbrowseJson", function() {
@@ -73,20 +70,26 @@ require(['JBrowse/Store/SeqFeature/GFF3/GFF3Parser','JBrowse/Store/SeqFeature/GF
 		it("should correctly set ['trackInfo']['intervals']['urlTemplate'] in jbrowse json", function() {
 			expect(actualJbrowseJsonOutput['trackInfo']["intervals"]["urlTemplate"]).toEqual("lf-{Chunk}.json");
 		    });
-		
-		it("should parse all features with multiple siblings (not just the first)", function() {
-			expect(actualJbrowseJsonOutput2['featArray'][0]).toEqual(
-						 [ 0, 245453, 247006, '+', 'maker', '.', 'mRNA', '.', 'mRNA_1', 'mRNA_1', 
-						   [ 
-						    [ 1, 245759, 245879, '+', 'maker', '0', 'CDS', '.', 'cds_1.1', null ], 
-						    [ 1, 246045, 246278, '+', 'maker', '0', 'CDS', '.', 'cds_1.2', null ], 
-						    [ 1, 246388, 246815, '+', 'maker', '1', 'CDS', '.', 'cds_1.3', null ] 
-						     ] 
-						   ]
-										 );
-			// expect(actualJbrowseJsonOutput2['featArray'][1]).toEqual(["foo"]);
-			expect(actualJbrowseJsonOutput2['featArray'].length).toEqual(2);
-		    });
+
+		it("should correctly retrieve two level features from parsed gene GFF3", function() {
+			var twoLevelFeat = store._getFeaturesAtGivenDepth( parsedGFF3toJbrowseJsonInput["parsedData"][0], 2 );
+			expect(twoLevelFeat).toNotEqual(null);
+			expect(twoLevelFeat).toEqual(
+						     [ { ID : '1:gnomon_566853_mRNA', data : [ { rawdata : [ 'Group1.33', 'maker', 'mRNA', '245454', '247006', '.', '+', '.', 'ID=1:gnomon_566853_mRNA;Parent=this_parent_id_12345;Name=gnomon_566853_mRNA;_AED=0.45;_eAED=0.45;_QI=138|1|1|1|1|1|4|191|259;' ], attributes : { ID : [ '1:gnomon_566853_mRNA' ], Parent : [ 'this_parent_id_12345' ], Name : [ 'gnomon_566853_mRNA' ], _AED : [ '0.45' ], _eAED : [ '0.45' ], _QI : [ '138|1|1|1|1|1|4|191|259' ] } } ], children : [ { ID : '1:gnomon_566853_mRNA:exon:5976', data : [ { rawdata : [ 'Group1.33', 'maker', 'exon', '245454', '245533', '.', '+', '.', 'ID=1:gnomon_566853_mRNA:exon:5976;Parent=1:gnomon_566853_mRNA;' ], attributes : { ID : [ '1:gnomon_566853_mRNA:exon:5976' ], Parent : [ '1:gnomon_566853_mRNA' ] } } ], children : [ ] }, { ID : '1:gnomon_566853_mRNA:exon:5977', data : [ { rawdata : [ 'Group1.33', 'maker', 'exon', '245702', '245879', '.', '+', '.', 'ID=1:gnomon_566853_mRNA:exon:5977;Parent=1:gnomon_566853_mRNA;' ], attributes : { ID : [ '1:gnomon_566853_mRNA:exon:5977' ], Parent : [ '1:gnomon_566853_mRNA' ] } } ], children : [ ] }, { ID : '1:gnomon_566853_mRNA:exon:5978', data : [ { rawdata : [ 'Group1.33', 'maker', 'exon', '246046', '246278', '.', '+', '.', 'ID=1:gnomon_566853_mRNA:exon:5978;Parent=1:gnomon_566853_mRNA;' ], attributes : { ID : [ '1:gnomon_566853_mRNA:exon:5978' ], Parent : [ '1:gnomon_566853_mRNA' ] } } ], children : [ ] }, { ID : '1:gnomon_566853_mRNA:exon:5979', data : [ { rawdata : [ 'Group1.33', 'maker', 'exon', '246389', '247006', '.', '+', '.', 'ID=1:gnomon_566853_mRNA:exon:5979;Parent=1:gnomon_566853_mRNA;' ], attributes : { ID : [ '1:gnomon_566853_mRNA:exon:5979' ], Parent : [ '1:gnomon_566853_mRNA' ] } } ], children : [ ] }, { ID : '1:gnomon_566853_mRNA:five_prime_utr', data : [ { rawdata : [ 'Group1.33', 'maker', 'five_prime_UTR', '245702', '245759', '.', '+', '.', 'ID=1:gnomon_566853_mRNA:five_prime_utr;Parent=1:gnomon_566853_mRNA;' ], attributes : { ID : [ '1:gnomon_566853_mRNA:five_prime_utr' ], Parent : [ '1:gnomon_566853_mRNA' ] } }, { rawdata : [ 'Group1.33', 'maker', 'five_prime_UTR', '245702', '245759', '.', '+', '.', 'ID=1:gnomon_566853_mRNA:five_prime_utr;Parent=1:gnomon_566853_mRNA;' ], attributes : { ID : [ '1:gnomon_566853_mRNA:five_prime_utr' ], Parent : [ '1:gnomon_566853_mRNA' ] } } ], children : [ ] }, { ID : '1:gnomon_566853_mRNA:cds', data : [ { rawdata : [ 'Group1.33', 'maker', 'CDS', '246389', '246815', '.', '+', '1', 'ID=1:gnomon_566853_mRNA:cds;Parent=1:gnomon_566853_mRNA;' ], attributes : { ID : [ '1:gnomon_566853_mRNA:cds' ], Parent : [ '1:gnomon_566853_mRNA' ] } }, { rawdata : [ 'Group1.33', 'maker', 'CDS', '246389', '246815', '.', '+', '1', 'ID=1:gnomon_566853_mRNA:cds;Parent=1:gnomon_566853_mRNA;' ], attributes : { ID : [ '1:gnomon_566853_mRNA:cds' ], Parent : [ '1:gnomon_566853_mRNA' ] } }, { rawdata : [ 'Group1.33', 'maker', 'CDS', '246389', '246815', '.', '+', '1', 'ID=1:gnomon_566853_mRNA:cds;Parent=1:gnomon_566853_mRNA;' ], attributes : { ID : [ '1:gnomon_566853_mRNA:cds' ], Parent : [ '1:gnomon_566853_mRNA' ] } }, { rawdata : [ 'Group1.33', 'maker', 'CDS', '246389', '246815', '.', '+', '1', 'ID=1:gnomon_566853_mRNA:cds;Parent=1:gnomon_566853_mRNA;' ], attributes : { ID : [ '1:gnomon_566853_mRNA:cds' ], Parent : [ '1:gnomon_566853_mRNA' ] } } ], children : [ ] }, { ID : '1:gnomon_566853_mRNA:three_prime_utr', data : [ { rawdata : [ 'Group1.33', 'maker', 'three_prime_UTR', '246816', '247006', '.', '+', '.', 'ID=1:gnomon_566853_mRNA:three_prime_utr;Parent=1:gnomon_566853_mRNA;' ], attributes : { ID : [ '1:gnomon_566853_mRNA:three_prime_utr' ], Parent : [ '1:gnomon_566853_mRNA' ] } } ], children : [ ] } ] } ]
+						     )
+			    });
+
+		it("should correctly retrieve two level features from parsed gene GFF3 with two mRNAs", function() {
+			parsedGFF3toJbrowseJsonInput2 = gparser.parse( makerGff3String2 );
+			var twoLevelFeat = store._getFeaturesAtGivenDepth( parsedGFF3toJbrowseJsonInput2["parsedData"][0], 2 );
+			expect(twoLevelFeat).toNotEqual(null);
+			expect(twoLevelFeat).toEqual(
+						     [ 
+						      { ID : 'mRNA_1', data : [ { rawdata : [ 'Group1.33', 'maker', 'mRNA', '245454', '247006', '.', '+', '.', 'ID=mRNA_1;Parent=this_parent_id_12345;Name=mRNA_1;' ], attributes : { ID : [ 'mRNA_1' ], Parent : [ 'this_parent_id_12345' ], Name : [ 'mRNA_1' ] } } ], children : [ { ID : 'cds_1.1', data : [ { rawdata : [ 'Group1.33', 'maker', 'CDS', '245760', '245879', '.', '+', '0', 'ID=cds_1.1;Parent=mRNA_1;' ], attributes : { ID : [ 'cds_1.1' ], Parent : [ 'mRNA_1' ] } } ], children : [ ] }, { ID : 'cds_1.2', data : [ { rawdata : [ 'Group1.33', 'maker', 'CDS', '246046', '246278', '.', '+', '0', 'ID=cds_1.2;Parent=mRNA_1;' ], attributes : { ID : [ 'cds_1.2' ], Parent : [ 'mRNA_1' ] } } ], children : [ ] }, { ID : 'cds_1.3', data : [ { rawdata : [ 'Group1.33', 'maker', 'CDS', '246389', '246815', '.', '+', '1', 'ID=cds_1.3;Parent=mRNA_1;' ], attributes : { ID : [ 'cds_1.3' ], Parent : [ 'mRNA_1' ] } } ], children : [ ] } ] }, 
+						      { ID : 'mRNA_2', data : [ { rawdata : [ 'Group1.33', 'maker', 'mRNA', '245454', '247006', '.', '+', '.', 'ID=mRNA_2;Parent=this_parent_id_12345;Name=mRNA_2;' ], attributes : { ID : [ 'mRNA_2' ], Parent : [ 'this_parent_id_12345' ], Name : [ 'mRNA_2' ] } } ], children : [ { ID : 'cds_2.1', data : [ { rawdata : [ 'Group1.33', 'maker', 'CDS', '245760', '245879', '.', '+', '0', 'ID=cds_2.1;Parent=mRNA_2;' ], attributes : { ID : [ 'cds_2.1' ], Parent : [ 'mRNA_2' ] } } ], children : [ ] }, { ID : 'cds_2.2', data : [ { rawdata : [ 'Group1.33', 'maker', 'CDS', '246046', '246278', '.', '+', '0', 'ID=cds_2.2;Parent=mRNA_2;' ], attributes : { ID : [ 'cds_2.2' ], Parent : [ 'mRNA_2' ] } } ], children : [ ] }, { ID : 'cds_2.3', data : [ { rawdata : [ 'Group1.33', 'maker', 'CDS', '246389', '246815', '.', '+', '1', 'ID=cds_2.3;Parent=mRNA_2;' ], attributes : { ID : [ 'cds_2.3' ], Parent : [ 'mRNA_2' ] } } ], children : [ ] } ] }, 
+						       ] 
+						     )
+			    });
 		
 	    });
 	

commit 6afbafe0c1de00a56ae101af5fb028b2eeb6999d

Fixed bug introduced by commit 9b0f041, that caused GFF3 loading to silently fail for two-level features.

src/JBrowse/Store/SeqFeature/GFF3.js
@@ -89,7 +89,11 @@ return declare([ NCListStore ],
         console.log("   content start: " + content.substr(0, 50));
         var gparser = new GFF3Parser();
         var gff3_json = gparser.parse(content);
+        console.log("parsed GFF3:");
+        console.log(gff3_json);
         var results = this._gff3toJbrowseJson(gff3_json, store.args);
+        console.log("converted GFF3:");
+        console.log(results);
         var trackInfo = results.trackInfo;
         var featArray = results.featArray;
         store.attrs = new ArrayRepr(trackInfo.intervals.classes);
@@ -293,33 +297,28 @@ return declare([ NCListStore ],
     },
 
     // helper feature for _convertParsedGFF3JsonToFeatureArray
-    // that returns the feature at a given depth
-    // (it will return the first feature in the arrayref at
-    // that depth)
+    // For a given top-level feature, returns descendant features at a given depth
     _getFeaturesAtGivenDepth: function(gffFeature, depth) {
         var recursion_level = 0;
 	var maximum_recursion_level = 20; // paranoid about infinite recursion
-        var getFeature = function(thisJsonFeature, thisDepth) {
+        var getFeatures = function(thisJsonFeature, thisDepth, returnedFeatures) {
             if ( recursion_level > maximum_recursion_level ){
                 return null;
             }
             // are we at the right depth?
             if ( recursion_level + 1 == thisDepth ){
-                return thisJsonFeature;
+                returnedFeatures.push( thisJsonFeature );
             }
             else if ( thisJsonFeature.children != null && thisJsonFeature.children.length > 0 ){
 		recursion_level++;
-                var returnedFeatures = new Array;
 		for (var m = 0; m < thisJsonFeature.children.length; m++){
-		    if ( thisFeature = getFeature(thisJsonFeature.children[m], depth) ){
-			returnedFeatures.push( thisFeature );
-		    }
+                    getFeatures( thisJsonFeature.children[m], depth, returnedFeatures );
 		}
-		return returnedFeatures;
             }
-            return null;
         };
-        return getFeature( gffFeature, depth );
+        var results = [];
+        getFeatures( gffFeature, depth, results );
+        return results;
     },
 
     // helper feature for _convertParsedGFF3JsonToFeatureArray