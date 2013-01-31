b2bf1409d9be904d72d8d573f0e831a3ca08fae3
Robert Buels <rbuels@gmail.com>
2013-01-30 20:38:55 -0500

    change binning algorithm of FeatureCoverage track to take average coverage of each bin, instead of sum of how many features overlap the bin

--- src/JBrowse/View/Track/FeatureCoverage.js
+++ src/JBrowse/View/Track/FeatureCoverage.js
@@ -46,23 +46,42 @@ return declare( Wiggle,
         var binWidth = Math.ceil( query.basesPerSpan ); // in bp
 
         var coverageBins = new Array( Math.ceil( widthBp/binWidth ) );
-        var bpToBin = function( bp ) {
-            return Math.floor( (bp-leftBase) / binWidth );
+        var binOverlap = function( bp, isRightEnd ) {
+            var binCoord  = (bp-leftBase) / binWidth;
+            var binNumber = Math.floor( binCoord );
+            var overlap   = isRightEnd ? 1-(binCoord-binNumber) : binCoord - binNumber;
+            return {
+                bin: binNumber,
+                overlap: overlap // between 0 and 1: proportion of this bin that the feature overlaps
+            };
         };
 
         this.store.getFeatures(
             query,
             dojo.hitch( this, function( feature ) {
-                            var startBin = bpToBin( feature.get('start') );
-                            var endBin   = bpToBin( feature.get('end')-1 );
-                            for( var i = startBin; i <= endBin; i++ ) {
+                            var start = feature.get('start')-1;
+                            var end = feature.get('end')-1;
+                            var startBO = binOverlap( start, false );
+                            var endBO   = binOverlap( end,   true  );
+
+                            // increment start and end partial-overlap bins by proportion of overlap
+                            if( startBO.bin == endBO.bin ) {
+                                coverageBins[startBO.bin] = (coverageBins[startBO.bin] || 0) + endBO.overlap + startBO.overlap - 1;
+                            }
+                            else {
+                                coverageBins[startBO.bin] = (coverageBins[startBO.bin] || 0) + startBO.overlap;
+                                coverageBins[endBO.bin]   = (coverageBins[endBO.bin]   || 0) + endBO.overlap;
+                            }
+
+                            // increment completely overlapped interior bins by 1
+                            for( var i = startBO.bin+1; i <= endBO.bin-1; i++ ) {
                                 coverageBins[i] = (coverageBins[i] || 0) + 1;
                             }
                         }),
             function () {
                 // make fake features from the coverage
                 for( var i = 0; i < coverageBins.length; i++ ) {
-                    var score = coverageBins[i] || 0;
+                    var score = (coverageBins[i] || 0);
                     var bpOffset = leftBase+binWidth*i;
                     featureCallback( new CoverageFeature({
                         start: bpOffset,
