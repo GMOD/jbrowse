b0a3944bf8eebd3b504c427f32a9485d991e612f
Robert Buels <rmb32@cornell.edu>
2013-02-15 11:20:33 -0500

    add explanatory comments about Store/SeqFeature API methods

--- src/JBrowse/Store/SeqFeature.js
+++ src/JBrowse/Store/SeqFeature.js
@@ -22,10 +22,22 @@ return declare( Store,
         this.config = args || {};
     },
 
+    /**
+     * Get global statistics about the feature store.  Calls its
+     * callback with an object containing whatever statistics are
+     * available about the features in the store.
+     */
     getGlobalStats: function( callback, errorCallback ) {
         callback( this.globalStats || {} );
     },
 
+    /**
+     * Get statistics about the features in a certain sequence region,
+     * specified by `query`, which is an object having at least `ref
+     * (string)`, `start (number)`, and `end (number)` attributes.
+     * Calls its callback with an object containing whatever
+     * statistics are available about the features in that region.
+     */
     getRegionStats: function( query, successCallback, errorCallback ) {
         return this._getRegionStats.apply( this, arguments );
     },
@@ -34,6 +46,13 @@ return declare( Store,
         return this.getGlobalStats( successCallback, errorCallback );
     },
 
+    /**
+     * Get the features in a certain sequence region, specified by
+     * `query`, which is an object having at least `ref (string)`,
+     * `start (number)`, and `end (number)` attributes.  Calls the
+     * featureCallback once for each feature, and calls endCallback
+     * when all features have been processed.
+     */
     getFeatures: function( query, featureCallback, endCallback, errorCallback ) {
         endCallback();
     }
