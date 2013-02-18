533de22c881a91538ad51c7a670a566755f479db
Robert Buels <rmb32@cornell.edu>
2013-02-18 17:24:40 -0500

    implement getRegionStats in JBrowse/Store/SeqFeature base class based on feature scores, in a way that should work for any quantitative data backendj

--- src/JBrowse/Store/SeqFeature.js
+++ src/JBrowse/Store/SeqFeature.js
@@ -31,7 +31,39 @@ return declare( Store,
     },
 
     _getRegionStats: function( query, successCallback, errorCallback ) {
-        return this.getGlobalStats( successCallback, errorCallback );
+        var thisB = this;
+        var s = {
+            scoreMax: -Infinity,
+            scoreMin: Infinity,
+            scoreSum: 0,
+            scoreSumSquares: 0,
+            basesCovered: query.end - query.start
+        };
+        this.getFeatures( query,
+                          function( feature ) {
+                              var score = feature.get('score') || 0;
+                              s.scoreMax = Math.max( score, s.scoreMax );
+                              s.scoreMin = Math.min( score, s.scoreMin );
+                              s.scoreSum += score;
+                              s.scoreSumSquares += score*score;
+                          },
+                          function() {
+                              s.scoreMean = s.scoreSum / s.basesCovered;
+                              s.scoreStdDev = thisB._calcStdFromSums( s.scoreSum, s.scoreSumSquares, s.basesCovered );
+                              //console.log( 'BigWig getRegionStats', query, s );
+                              successCallback(s);
+                          },
+                          errorCallback
+                        );
+    },
+
+    // utility method that calculates standard deviation from sum and sum of squares
+    _calcStdFromSums: function( sum, sumSquares, n ) {
+        var variance = sumSquares - sum*sum/n;
+        if (n > 1) {
+	    variance /= n-1;
+        }
+        return variance < 0 ? 0 : Math.sqrt(variance);
     },
 
     getFeatures: function( query, featureCallback, endCallback, errorCallback ) {
