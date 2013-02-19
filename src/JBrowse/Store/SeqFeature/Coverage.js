9f3fb128f535a1418d84d21cc36a7937ba2fa08f
Robert Buels <rmb32@cornell.edu>
2013-02-19 12:20:29 -0500

    factor FeatureCoverage track coverage calculations into JBrowse/Store/SeqFeature/Coverage, which is a store that encapsulates another store and calculates its coverage.  also make FeatureCoverage tracks default to local autoscaling.

---
+++ src/JBrowse/Store/SeqFeature/Coverage.js
@@ -0,0 +1,94 @@
+/**
+ * Store class that encapsulates another store, and synthesizes
+ * quantitative features that give the depth of coverage for the
+ * features in it.
+ */
+
+define([
+           'dojo/_base/declare',
+           'JBrowse/Store/SeqFeature',
+           'JBrowse/Util'
+       ],
+       function( declare, SeqFeatureStore, Util ) {
+
+// feature class for the features we make for the calculated coverage
+// values
+var CoverageFeature = Util.fastDeclare(
+    {
+        get: function(f) { return this[f]; },
+        tags: function() { return [ 'start', 'end', 'score' ]; },
+        score: 0,
+        constructor: function( args ) {
+            this.start = args.start;
+            this.end = args.end;
+            this.score = args.score;
+        }
+    });
+
+return declare( SeqFeatureStore, {
+
+    constructor: function( args ) {
+        this.store = args.store;
+    },
+
+    getGlobalStats: function() {
+        return {};
+    },
+
+    getFeatures: function( query, featureCallback, finishCallback, errorCallback ) {
+        var leftBase  = query.start;
+        var rightBase = query.end;
+        var scale = query.scale || ( ('basesPerSpan' in query) ? 1/query.basesPerSpan : 10 ); // px/bp
+        var widthBp = rightBase-leftBase;
+        var widthPx = widthBp * scale;
+
+        var binWidth = Math.ceil( 1/scale ); // in bp
+
+        var coverageBins = new Array( Math.ceil( widthBp/binWidth ) );
+        var binOverlap = function( bp, isRightEnd ) {
+            var binCoord  = (bp-leftBase-1) / binWidth;
+            var binNumber = Math.floor( binCoord );
+            var overlap   = isRightEnd ? 1-(binCoord-binNumber) : binCoord - binNumber;
+            return {
+                bin: binNumber,
+                overlap: overlap // between 0 and 1: proportion of this bin that the feature overlaps
+            };
+        };
+
+        this.store.getFeatures(
+            query,
+            function( feature ) {
+                            var startBO = binOverlap( feature.get('start'), false );
+                            var endBO   = binOverlap( feature.get('end'),   true  );
+
+                            // increment start and end partial-overlap bins by proportion of overlap
+                            if( startBO.bin == endBO.bin ) {
+                                coverageBins[startBO.bin] = (coverageBins[startBO.bin] || 0) + endBO.overlap + startBO.overlap - 1;
+                            }
+                            else {
+                                coverageBins[startBO.bin] = (coverageBins[startBO.bin] || 0) + startBO.overlap;
+                                coverageBins[endBO.bin]   = (coverageBins[endBO.bin]   || 0) + endBO.overlap;
+                            }
+
+                            // increment completely overlapped interior bins by 1
+                            for( var i = startBO.bin+1; i <= endBO.bin-1; i++ ) {
+                                coverageBins[i] = (coverageBins[i] || 0) + 1;
+                            }
+            },
+            function () {
+                // make fake features from the coverage
+                for( var i = 0; i < coverageBins.length; i++ ) {
+                    var score = (coverageBins[i] || 0);
+                    var bpOffset = leftBase+binWidth*i;
+                    featureCallback( new CoverageFeature({
+                        start: bpOffset,
+                        end:   bpOffset+binWidth,
+                        score: score
+                     }));
+                }
+                finishCallback();
+            }
+        );
+    }
+});
+});
