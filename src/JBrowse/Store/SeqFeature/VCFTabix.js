c0a960bba64fc940f0197985aec4f7b1fd00ee1a
Robert Buels <rmb32@cornell.edu>
2013-03-08 18:09:20 -0500

    add a bunch more reserved INFO field names from the VCF 4.1 spec

--- src/JBrowse/Store/SeqFeature/VCFTabix.js
+++ src/JBrowse/Store/SeqFeature/VCFTabix.js
@@ -226,29 +226,78 @@ return declare( [SeqFeatureStore,DeferredStatsMixin,DeferredFeaturesMixin,Global
 
     _vcfReservedInfoFields: {
         // from the VCF4.1 spec, http://www.1000genomes.org/wiki/Analysis/Variant%20Call%20Format/vcf-variant-call-format-version-41
-        AA: "ancestral allele",
-        AC: "allele count in genotypes, for each ALT allele, in the same order as listed",
-        AF: "allele frequency for each ALT allele in the same order as listed: use this when estimated from primary data, not called genotypes",
-        AN: "total number of alleles in called genotypes",
-        BQ: "RMS base quality at this position",
-        CIGAR: "cigar string describing how to align an alternate allele to the reference allele",
-        DB: "dbSNP membership",
-        DP: "combined depth across samples, e.g. DP=154",
-        END: "end position of the variant described in this record (esp. for CNVs)",
-        H2: "membership in hapmap2",
-        MQ: "RMS mapping quality, e.g. MQ=52",
-        MQ0: "Number of MAPQ == 0 reads covering this record",
-        NS: "Number of samples with data",
-        SB: "strand bias at this position",
-        SOMATIC: "indicates that the record is a somatic mutation, for cancer genomics",
-        VALIDATED: "validated by follow-up experiment"
+        AA:    { description: "ancestral allele" },
+        AC:    { description: "allele count in genotypes, for each ALT allele, in the same order as listed" },
+        AF:    { description: "allele frequency for each ALT allele in the same order as listed: use this when estimated from primary data, not called genotypes" },
+        AN:    { description: "total number of alleles in called genotypes" },
+        BQ:    { description: "RMS base quality at this position" },
+        CIGAR: { description: "cigar string describing how to align an alternate allele to the reference allele" },
+        DB:    { description: "dbSNP membership" },
+        DP:    { description: "combined depth across samples, e.g. DP=154" },
+        END:   { description: "end position of the variant described in this record (esp. for CNVs)" },
+        H2:    { description: "membership in hapmap2" },
+        MQ:    { description: "RMS mapping quality, e.g. MQ=52" },
+        MQ0:   { description: "Number of MAPQ == 0 reads covering this record" },
+        NS:    { description: "Number of samples with data" },
+        SB:    { description: "strand bias at this position" },
+        SOMATIC: { description: "indicates that the record is a somatic mutation, for cancer genomics" },
+        VALIDATED: { description: "validated by follow-up experiment" },
+
+        //specifically for structural variants
+        "IMPRECISE": { number: 0, type: 'Flag', description: "Imprecise structural variation" },
+        "NOVEL":     { number: 0, type: 'Flag',description: "Indicates a novel structural variation" },
+        "END":       { number: 1, type: 'Integer', description: "End position of the variant described in this record" },
+
+        // For precise variants, END is POS + length of REF allele -
+        // 1, and the for imprecise variants the corresponding best
+        // estimate.
+
+        "SVTYPE": { number: 1, type: 'String',description: "Type of structural variant" },
+
+        // Value should be one of DEL, INS, DUP, INV, CNV, BND. This
+        // key can be derived from the REF/ALT fields but is useful
+        // for filtering.
+
+        "SVLEN": { number:'.',type: 'Integer', description: 'Difference in length between REF and ALT alleles' },
+
+        // One value for each ALT allele. Longer ALT alleles
+        // (e.g. insertions) have positive values, shorter ALT alleles
+        // (e.g. deletions) have negative values.
+
+        "CIPOS":  { number: 2, "type": 'Integer', "description": 'Confidence interval around POS for imprecise variants' },
+        "CIEND":  { number: 2, "type": 'Integer', "description": "Confidence interval around END for imprecise variants" },
+        "HOMLEN": {            "type": "Integer", "description": "Length of base pair identical micro-homology at event breakpoints" },
+        "HOMSEQ": {            "type": "String",  "description": "Sequence of base pair identical micro-homology at event breakpoints" },
+        "BKPTID": {            "type": "String",  "description": "ID of the assembled alternate allele in the assembly file" },
+
+        // For precise variants, the consensus sequence the alternate
+        // allele assembly is derivable from the REF and ALT
+        // fields. However, the alternate allele assembly file may
+        // contain additional information about the characteristics of
+        // the alt allele contigs.
+
+        "MEINFO":  { number:4, "type": "String", "description": "Mobile element info of the form NAME,START,END,POLARITY" },
+        "METRANS": { number:4, "type": "String", "description": "Mobile element transduction info of the form CHR,START,END,POLARITY" },
+        "DGVID":   { number:1, "type": "String", "description": "ID of this element in Database of Genomic Variation"},
+        "DBVARID": { number:1, "type": "String", "description": "ID of this element in DBVAR"},
+        "DBRIPID": { number:1, "type": "String", "description": "ID of this element in DBRIP"},
+        "MATEID":  {           "type": "String", "description": "ID of mate breakends"},
+        "PARID":   { number:1, "type": "String", "description": "ID of partner breakend"},
+        "EVENT":   { number:1, "type": "String", "description": "ID of event associated to breakend"},
+        "CILEN":   { number:2, "type": "Integer","description": "Confidence interval around the length of the inserted material between breakends"},
+        "DP":      { number:1, "type": "Integer","description": "Read Depth of segment containing breakend"},
+        "DPADJ":   {           "type": "Integer","description": "Read Depth of adjacency"},
+        "CN":      { number:1, "type": "Integer","description": "Copy number of segment containing breakend"},
+        "CNADJ":   {           "type": "Integer","description": "Copy number of adjacency"},
+        "CICN":    { number:2, "type": "Integer","description": "Confidence interval around copy number for the segment"},
+        "CICNADJ": {           "type": "Integer","description": "Confidence interval around copy number for the adjacency"}
     },
 
     _vcfReservedGenotypeKeywords: {
         // from the VCF4.1 spec, http://www.1000genomes.org/wiki/Analysis/Variant%20Call%20Format/vcf-variant-call-format-version-41
         GT : "genotype, encoded as allele values separated by either of '/' or '|'. The allele values are 0 for the reference allele (what is in the REF field), 1 for the first allele listed in ALT, 2 for the second allele list in ALT and so on. For diploid calls examples could be 0/1, 1|0, or 1/2, etc. For haploid calls, e.g. on Y, male non-pseudoautosomal X, or mitochondrion, only one allele value should be given; a triploid call might look like 0/0/1. If a call cannot be made for a sample at a given locus, '.' should be specified for each missing allele in the GT field (for example './.' for a diploid genotype and '.' for haploid genotype). The meanings of the separators are as follows (see the PS field below for more details on incorporating phasing information into the genotypes): '/' meaning genotype unphased, '|' meaning genotype phased",
         DP : "read depth at this position for this sample (Integer)",
-        FT : "sample genotype filter indicating if this genotype was “called” (similar in concept to the FILTER field). Again, use PASS to indicate that all filters have been passed, a semi-colon separated list of codes for filters that fail, or ”.” to indicate that filters have not been applied. These values should be described in the meta-information in the same way as FILTERs (String, no white-space or semi-colons permitted)",
+        FT : "sample genotype filter indicating if this genotype was \"called\" (similar in concept to the FILTER field). Again, use PASS to indicate that all filters have been passed, a semi-colon separated list of codes for filters that fail, or \".\" to indicate that filters have not been applied. These values should be described in the meta-information in the same way as FILTERs (String, no white-space or semi-colons permitted)",
         GL : "genotype likelihoods comprised of comma separated floating point log10-scaled likelihoods for all possible genotypes given the set of alleles defined in the REF and ALT fields. In presence of the GT field the same ploidy is expected and the canonical order is used; without GT field, diploidy is assumed. If A is the allele in REF and B,C,... are the alleles as ordered in ALT, the ordering of genotypes for the likelihoods is given by: F(j/k) = (k*(k+1)/2)+j.  In other words, for biallelic sites the ordering is: AA,AB,BB; for triallelic sites the ordering is: AA,AB,BB,AC,BC,CC, etc.  For example: GT:GL 0/1:-323.03,-99.29,-802.53 (Floats)",
         GLE : "genotype likelihoods of heterogeneous ploidy, used in presence of uncertain copy number. For example: GLE=0:-75.22,1:-223.42,0/0:-323.03,1/0:-99.29,1/1:-802.53 (String)",
         PL : "the phred-scaled genotype likelihoods rounded to the closest integer (and otherwise defined precisely as the GL field) (Integers)",
