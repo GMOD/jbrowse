8d48963d33f3d89a496634debd4912e722ddeb98
Robert Buels <rmb32@cornell.edu>
2013-03-13 13:28:14 -0400

    add a JBrowse/View/Track/VariantDetailMixin that makes a feature detail dialog customized for displaying variants, and a HTMLVariants track that uses it.  after polishing up CanvasFeatures, though, I intend to make a Variants track based on it and remove HTMLVariants

---
+++ src/JBrowse/View/Track/VariantDetailMixin.js
@@ -0,0 +1,60 @@
+/**
+ * Mixin to provide a `defaultFeatureDetail` method that is optimized
+ * for displaying variant data from VCF files.
+ */
+
+define([
+           'dojo/_base/declare',
+           'dojo/_base/array',
+           'JBrowse/Util',
+           'JBrowse/View/Track/FeatureDetailMixin'
+       ],
+       function(
+           declare,
+           array,
+           Util,
+           FeatureDetailMixin
+       ) {
+
+return declare( FeatureDetailMixin, {
+
+
+    defaultFeatureDetail: function( /** JBrowse.Track */ track, /** Object */ f, /** HTMLElement */ featDiv, /** HTMLElement */ container ) {
+        container = container || dojo.create('div', { className: 'detail feature-detail feature-detail-'+track.name, innerHTML: '' } );
+
+        this._renderCoreDetails( track, f, featDiv, container );
+
+        this._renderAdditionalTagsDetail( track, f, featDiv, container );
+
+        // genotypes in a separate section
+        this._renderGenotypes( track, f, featDiv, container );
+
+        return container;
+    },
+
+    _isReservedTag: function( t ) {
+        return this.inherited(arguments) || {genotypes:1}[t.toLowerCase()];
+    },
+
+    _renderGenotypes: function( track, f, featDiv, container ) {
+        var genotypes = f.get('genotypes');
+        if( ! genotypes )
+            return;
+
+        var samples = Util.dojof.keys( genotypes ).sort();
+        if( ! samples.length )
+            return;
+
+        var g_html = '<div class="genotypes"><h2 class="sectiontitle">Genotypes</h2>';
+        array.forEach( samples, function( sampleName ) {
+
+            g_html += this._fmtDetailField( sampleName, genotypes[sampleName], 'genotype_'+sampleName.replace(/[^w]/g,'_') );
+
+        }, this);
+        g_html += '</div>';
+
+        container.innerHTML += g_html;
+    }
+
+});
+});
