03bceac1c576ee753e67b2eb9624de848150ebc0
Robert Buels <rmb32@cornell.edu>
2013-04-12 11:29:01 -0400

    add a `fillTemplate` method to JBrowse/Component that knows how to fill a templated string with {refseq} and associated info, and accepts args for additional values to interpolate, references #241

--- src/JBrowse/Component.js
+++ src/JBrowse/Component.js
@@ -4,10 +4,12 @@
 
 define([
            'dojo/_base/declare',
+           'dojo/_base/lang',
            'JBrowse/Util'
        ],
        function(
            declare,
+           lang,
            Util
        ) {
 
@@ -40,9 +42,7 @@ return declare( null, {
         args = args || {};
         return Util.resolveUrl(
             this.getConf('baseUrl',[]),
-            Util.fillTemplate( url,
-                               args
-                             )
+            this.fillTemplate( url, args )
         );
 
     },
@@ -79,6 +79,7 @@ return declare( null, {
     /**
      * Given a dot-separated string configuration path into the config
      * (e.g. "style.bg_color"), get the value of the configuration.
+     *
      * If args are given, evaluate the configuration using them.
      * Otherwise, return a function that returns the value of the
      * configuration when called.
@@ -90,6 +91,43 @@ return declare( null, {
         }
 
         return args ? func.apply( this, args ) : func.call( this );
+    },
+
+    /**
+     * Given a string with templating strings like {refseq}, fill them
+     * in using the given values.
+     *
+     * With no additional values given, knows how to interpolate
+     * {refseq}, {refSeq}, {refSeqNum}, and {refSeqNumNoLeadingZeroes}.
+     *
+     * @param {String} str string to interpolate values into
+     * @param {Object} values optional object with additional values that can be interpolated
+     * @returns new string with interpolations
+     */
+    fillTemplate: function( str, values ) {
+
+        // skip if it's not a string or the string has no interpolations
+        if( typeof str != 'string' || str.indexOf('{') == -1 )
+            return str;
+
+        // fill in a bunch of args for this.refSeq or this.ref
+        var templateFillArgs = {
+            'refseq': (this.refSeq||{}).name || (this.ref||{}).name || this.ref || ''
+        };
+        templateFillArgs.refSeq = templateFillArgs.refseq;
+
+        if( templateFillArgs.refSeq ) {
+            templateFillArgs.refSeqNum = ( /\d+/.exec( templateFillArgs.refSeq ) || [] )[0] || '';
+        }
+        // make refseqNumNoLeadingZeroes
+        if( templateFillArgs.refSeqNum ) {
+            templateFillArgs.refSeqNumNoLeadingZeroes = ( /^0*(\d+)/.exec( templateFillArgs.refSeqNum ) || [] )[1] || '';
+        }
+
+        if( values )
+            lang.mixin( templateFillArgs, values );
+
+        return Util.fillTemplate( str, templateFillArgs );
     }
 });
 });
