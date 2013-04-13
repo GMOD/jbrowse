76e0bbacaed6743cdc75c297583c8c47356dc566
Robert Buels <rmb32@cornell.edu>
2013-04-12 22:14:43 -0400

    make `HTMLVariants` the default track type for local VCF files

--- src/JBrowse/Browser.js
+++ src/JBrowse/Browser.js
@@ -704,7 +704,7 @@ Browser.prototype.getTrackTypes = function() {
                 'JBrowse/Store/SeqFeature/NCList'     : 'JBrowse/View/Track/HTMLFeatures',
                 'JBrowse/Store/SeqFeature/BigWig'     : 'JBrowse/View/Track/Wiggle/XYPlot',
                 'JBrowse/Store/Sequence/StaticChunked': 'JBrowse/View/Track/Sequence',
-                'JBrowse/Store/SeqFeature/VCFTabix'   : 'JBrowse/View/Track/HTMLFeatures'
+                'JBrowse/Store/SeqFeature/VCFTabix'   : 'JBrowse/View/Track/HTMLVariants'
             },
 
             knownTrackTypes: [
