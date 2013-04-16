7bcf39c9931844de63aea5460a7298d456236d0a
jsmirob <jsmirob@gmail.com>
2013-04-16 14:23:28 -0400

    Now generates heatmaps from tracks and three cluster averages

--- src/JBrowse/View/RegionClustering.js
+++ src/JBrowse/View/RegionClustering.js
@@ -5,6 +5,7 @@ define([
         'JBrowse/Store/SeqFeature/regionClustering',
         'JBrowse/View/Overlay',
         'dojo/dom-construct',
+        './RegionClustering/kMeans'
        ],
        function(
                 declare,
@@ -12,32 +13,95 @@ define([
                 all,
                 regionClusteringStore,
                 Overlay,
-                dom
+                dom,
+                Kmeans
                 ) {
 return declare( null, {
 
     constructor: function(args) {
         this.browser = args.browser;
+        this.refSeq = args.browser.view.ref;
         this.numOfBins = args.numOfBins;
+        this.queryLength = args.queryLength || 1000;
         this.storeNames = args.storeNames.display;
-        this.store = new regionClusteringStore({ storeNames: args.storeNames, browser: args.browser });
+        this.regionStore = new regionClusteringStore({ storeNames: args.storeNames.regions, browser: args.browser });
+        this.store = new regionClusteringStore({ storeNames: args.storeNames.display, browser: args.browser });
     },
 
     show: function() {
-        var testDiv = dom.create( 'div', { innerHTML: "You're testing things. Hurray. The heatmap below shows the relative values for the region 1 to 5000. The scale is bounded by the global stats of each track, so colors will have different meanings in each row." } );
-        var heatmapDeferred = this.buildHeatmap( { ref: this.browser.view.ref.name,
-                                                        scale: this.browser.view.pxPerBp,
-                                                        basesPerSpan: 1/this.browser.view.pxPerBp,
-                                                        start: 1, end: 5000 });
         var thisB = this;
-        heatmapDeferred.then( dojo.hitch( thisB, function( heatmap ) {
-            var d = this.getStoreStats();
-            var thisB = this;
-            d.then(function(){
-                var can = thisB.drawHeatmap(heatmap);
-                new Overlay().addTitle('test').addToDisplay(testDiv).addToDisplay(can).show();
-            });
-        }));
+        var regions = []; // regions to query for heatmaps
+        this.regionStore.getFeatures( 
+            { ref: this.refSeq.name,
+              scale: this.browser.view.pxPerBp,
+              basesPerSpan: 1/this.browser.view.pxPerBp,
+              start: this.refSeq.start, end: this.refSeq.end }, 
+            // query the whole genome (alternately, allow the user to set regions?)
+            function(feat){thisB.makeQuery( feat, regions );},
+            dojo.hitch(this, function() {
+                // once the query regions have been built, execute the following.
+                var testDiv = dom.create( 'div', { innerHTML: "You're testing things. Hurray. The overlay shows test clustering data and heatmaps generated from the regions dictated by whatever HTMLFeatures takcs you selected." } );
+                var heatmapDeferred = [];
+                for ( var key in regions ) {
+                    if (regions.hasOwnProperty(key)) {
+                        heatmapDeferred.push(this.buildHeatmap(regions[key]));
+                    }
+                }
+                all(heatmapDeferred).then( dojo.hitch( thisB, function( heatmaps ) {
+                    var d = this.getStoreStats();
+                    d.then(dojo.hitch(this, function(){
+                        var overlay = new Overlay().addTitle('test').addToDisplay(testDiv);
+                        for ( var key in heatmaps ) {
+                            if (heatmaps.hasOwnProperty(key)) {
+                                overlay.addToDisplay(this.drawHeatmap(heatmaps[key]));
+                            }
+                        }
+                        var means = this.testKmeans(heatmaps);
+                        overlay.addToDisplay(dom.create('div',{innerHTML: 'break! clusters come next.'}));
+                        for ( var key in means ) {
+                            if (means.hasOwnProperty(key)) {
+                                overlay.addToDisplay(means[key]);
+                            }
+                        }
+                        overlay.show();
+                    }));
+                }));
+            }));
+    },
+
+    makeQuery: function( feat, regions ) {
+        // given a feature and an array of regions, generates a region to query and 
+        // its directionality, and pushes it to "regions"
+        if (!feat.get('strand')) {
+            console.warn('feature has no "strand" property.');
+            return false; // NOTE: return values should be used to alert users at some point that there was a problem.
+        }
+        var reg = { ref: this.refSeq.name,
+                    scale: this.browser.view.pxPerBp,
+                    basesPerSpan: 1/this.browser.view.pxPerBp,
+                    start: feat.get('end') - this.queryLength/2,
+                    end: feat.get('end') + this.queryLength/2 };
+        switch (feat.get('strand')) {
+            case 1:
+            case '+':
+                { reg.directionality = 'right-to-left';
+                  break; };
+            case -1:
+            case '-':
+                { reg.directionality = 'left-to-right';
+                  break; };
+            }
+        // If the region overlaps with the end of the refSeq, shift it.
+        if ( reg.start < 0 ) {
+            reg.end -= reg.start;
+            reg.start = 0;
+        }
+        if ( reg.end > this.refSeq.end ) {
+            reg.start -= reg.end - this.refSeq.end;
+            reg.end = this.refSeq.end;
+        }
+        regions.push(reg);
+        return true;
     },
 
     buildHeatmap: function( query ) {
@@ -77,6 +141,9 @@ return declare( null, {
                                                     // average over the number of base pairs in each bin.
                                                     heatmap[this.storeNames[name]][i] /= (query.end-query.start)/this.numOfBins;
                                                 }
+                                                if ( query.directionality && query.directionality == 'left-to-right')
+                                                    // reverse the heatmap if required
+                                                    heatmap[this.storeNames[name]].reverse();
                                             }
                                         }
                                         d.resolve(heatmap, true);
@@ -94,6 +161,7 @@ return declare( null, {
         var d = [];
         var thisB = this;
         for (var key in this.store.stores.display ) {
+            // loop through all the stores and fetch their global statistics.
             var x = new Deferred();
             d.push(x);
             this.store.stores.display[key].getGlobalStats(
@@ -108,6 +176,8 @@ return declare( null, {
     drawHeatmap: function( heatmap ) {
         var thisB = this;
         var scoreToColor = function( score, store ) {
+            // given a store and a score, this method will fetch the global statistics for that track,
+            // normalize the score using the track max and min, and convert that to a color code
             var max = thisB.stats[store]['scoreMax'];
             var min = thisB.stats[store]['scoreMin'];
             var normalized = (score-min)/(max-min);
@@ -124,15 +194,58 @@ return declare( null, {
         for (var name in this.storeNames ) {
             if (this.storeNames.hasOwnProperty(name)) {
                 for (var i=0; i<numCols; i++) {
+                    // loop through the numerical elements of the heatmap matrix and convert them to colors.
+                    // use those colors to build a canvas element to be used in the display.
                     var score = heatmap[this.storeNames[name]][i];
                     c.fillStyle = scoreToColor(score, this.storeNames[name]);
-                    console.log(c.fillStyle);
                     c.fillRect(i*20, j*20, 20, 20);
                 }
                 j++;
             }
         }
+        c.strokeRect(0.5,0.5,can.width-0.5,can.height-0.5);
         return can;
+    },
+
+    HMtoArray: function( heatmap ) {
+        // converts a heatmap into an normalized array for use in the k-means algorithm.
+        var hmArray = [];
+        for (var index in this.storeNames ) {
+            if (this.storeNames.hasOwnProperty(index)) {
+                var max = this.stats[this.storeNames[index]]['scoreMax'];
+                var min = this.stats[this.storeNames[index]]['scoreMin'];
+                for (var i=0; i<this.numOfBins; i++) {
+                    // flatten the matrix by concatenating rows
+                    hmArray.push( ( heatmap[this.storeNames[index]][i] - min )/(max-min) );
+                }
+            }
+        }
+        return hmArray;
+    },
+
+    ArrayToHM: function( array, region ) {
+        // converts an array into a heatmap
+        var hm = { region: region };
+        for (var index in this.storeNames ) {
+            if (this.storeNames.hasOwnProperty(index)) {
+                hm[this.storeNames[index]] = [];
+                var max = this.stats[this.storeNames[index]]['scoreMax'];
+                var min = this.stats[this.storeNames[index]]['scoreMin'];
+                for (var i=0; i<this.numOfBins; i++) {
+                    // build heatmap rows by 
+                    hm[this.storeNames[index]][i] = (max-min)*array[this.numOfBins*index+i] + min;
+                }
+            }
+        }
+        return hm;
+    },
+
+    testKmeans: function( data ) {
+        var thisB = this;
+        var data = data.map(function(a){return thisB.HMtoArray(a);});
+        var kmeans = new Kmeans().kMeans({data: data, numClusters: 3});
+        var means = kmeans.means.map(function(a){return thisB.drawHeatmap(thisB.ArrayToHM(a, {}))});;
+        return means;
     }
 
 });
