2227b6dcf692f95ad8acde565558d6e3d80f7859
Robert Buels <rmb32@cornell.edu>
2013-05-02 13:01:11 -0400

    check for presence of VCF genotype data field before trying to parse it

--- src/JBrowse/Store/SeqFeature/VCFTabix/LazyFeature.js
+++ src/JBrowse/Store/SeqFeature/VCFTabix/LazyFeature.js
@@ -74,11 +74,13 @@ var Feature = Util.fastDeclare(
             var g = (fields[i]||'').split(':');
             var gdata = {};
             for( var j = 0; j<format.length; ++j ) {
-                gdata[format[j].id] = {
-                    // don't split on commas if it looks like a string
-                    values: g[j][0] == '"' ? [g[j]] : g[j].split(','),
-                    meta: format[j].meta
-                };
+                if( g[j] && typeof g[j] == 'string' ) {
+                    gdata[format[j].id] = {
+                        // don't split on commas if it looks like a string
+                        values: g[j].charAt(0) == '"' ? [g[j]] : g[j].split(','),
+                        meta: format[j].meta
+                    };
+                }
             }
             genotypes.push( gdata );
         }
