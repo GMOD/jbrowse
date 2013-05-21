cc25bad51e6aeaadc1e8a118643a5b07d78089ff
Robert Buels <rmb32@cornell.edu>
2013-05-20 21:42:10 -0400

    more work on new format-nclist command

--- src/newperl/Bio/JBrowse/Command/format_nclist.pm
+++ src/newperl/Bio/JBrowse/Command/format_nclist.pm
@@ -1,19 +1,20 @@
 package Bio::JBrowse::Command::format_nclist;
+use strict;
+use warnings;
+
 use Bio::JBrowse -command;
 
-use Bio::GFF3::LowLevel::Parser;
-
 use Bio::JBrowse::Store::NCList;
-use Bio::JBrowse::FeatureStream::GFF3;
 
 sub command_names { 'format-nclist' }
 
 sub opt_spec {
     return (
-        [ "format=s",  "format of all input data items" ],
-        [ "out|o=s",   "output location.  will make a new directory here" ],
-        [ "presorted", "input is already sorted by reference sequence and start coordinate" ],
-        [ "force|f", "overwrite any existing output" ]
+        [ "format=s",  "Format of (all) input data.  If not specified, will guess at format of each source." ],
+        [ "out|o=s",   "Output location.  Will make a new directory." ],
+        [ "presorted", "Input is already sorted by reference sequence and start coordinate." ],
+        [ "type=s@", "Format only features of the given type.  For multiple types, may be specified multiple times, or with comma-separated types" ],
+        [ "force|f", "Overwrite any existing output." ],
         );
 }
 
@@ -37,11 +38,27 @@ sub validate_args {
 sub execute {
     my ($self, $opt, $args) = @_;
 
-    my @streams = $self->_open_all( @$args );
+    my $stream = $self->_open_all( @$args );
+
+    if( $opt->{type} ) {
+        require Bio::JBrowse::FeatureStream::Filter;
+        my @types = map {split /,/, $_ } @{ $opt->{type} || [] };
+        $stream = Bio::JBrowse::FeatureStream::Filter->new(
+            $stream,
+            sub {
+                no warnings 'uninitialized';
+                my ($f) = @_;
+                for my $t (@types) {
+                    return 1 if $f->{type} eq $t;
+                }
+                return 0;
+            });
+    }
+
     my $store = Bio::JBrowse::Store::NCList->new({
         path => $opt->{out}
     });
-    $store->${\( $opt->{presorted} ? 'insert_presorted' : 'insert' )}( @streams );
+    $store->${\( $opt->{presorted} ? 'insert_presorted' : 'insert' )}( $stream );
 }
 
 sub _open_all {
@@ -56,25 +73,41 @@ sub _open_all {
         $self->${\"_open_$format"}( $path );
     } @paths;
 
-    return @streams;
-    # return sub {} unless @streams;
+    return sub {} unless @streams;
 
-    # my $curr_stream = shift @streams;
-    # return sub {
-    #     return $curr_stream->() || do {
-    #         my $i;
-    #         until( $i ) {
-    #             $curr_stream = shift @streams
-    #                 or return;
-    #             $i = $curr_stream->();
-    #         }
-    #         $i
-    #     };
-    # };
+    my $curr_stream = shift @streams;
+    return sub {
+        return $curr_stream->() || do {
+            my $i;
+            until( $i || !@streams ) {
+                $curr_stream = shift @streams;
+                $i = $curr_stream->();
+            }
+            $i
+        };
+    };
+}
+
+sub _guess_format {
+    my ( $self, $path ) = @_;
+
+    if( -f $path ) {
+        if( $path =~ /\.([^\.]+)$/ ) {
+            return {qw(
+                          gff   gff3
+                          gff3  gff3
+                      )}->{lc $1};
+        }
+    }
+    return;
 }
 
 sub _open_gff3 {
     my ( $self, $file ) = @_;
+
+    require Bio::JBrowse::FeatureStream::GFF3;
+    require Bio::GFF3::LowLevel::Parser;
+
     open my $f, '<', $file or die "$! reading $file\n";
     my $p = Bio::GFF3::LowLevel::Parser->open( $f );
     return Bio::JBrowse::FeatureStream::GFF3->new( $p );
--- tests/new_perl_tests/format-nclist.t
+++ tests/new_perl_tests/format-nclist.t
@@ -8,17 +8,28 @@ use File::Temp 'tempdir';
 use Test::More;
 use App::Cmd::Tester;
 
+use lib 'tests/perl_tests/lib';
+use FileSlurping qw( slurp slurp_tree ); #< in tests/perl_tests/lib
 
 use Bio::JBrowse;
 
-my $tempdir = tempdir();
+my $tempdir = tempdir( CLEANUP => 1 );
 my $result = test_app( 'Bio::JBrowse' => [qw(
     format-nclist
     --force
+    --type mRNA
     tests/data/au9_scaffold_subset.gff3
     --out ), $tempdir
  ]);
 
+is( $result->error, undef, 'no errors' );
 is( $result->stderr, '', 'nothing on stderr' );
+
+my $out = slurp_tree( $tempdir );
+my $cds_trackdata = $out->{'Group1.33/trackData.json'};
+is( $cds_trackdata->{featureCount}, 28, 'got right feature count' ) or diag explain $cds_trackdata;
+is( scalar @{$cds_trackdata->{intervals}{classes}}, 5, 'got the right number of classes' )
+   or diag explain $cds_trackdata->{intervals}{classes};
+#diag explain $cds_trackdata;
 
 done_testing;
