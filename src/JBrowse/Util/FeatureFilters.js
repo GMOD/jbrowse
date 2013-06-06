4ac6aacb8e7613ba4710c4773be11f9ce9ef5124
Robert Buels <rmb32@cornell.edu>
2013-06-06 16:45:01 -0700

    add JBrowse/Util/FeatureFilters, a standard set of feature filters

---
+++ src/JBrowse/Util/FeatureFilters.js
@@ -0,0 +1,27 @@
+/**
+ * Standard filtering subroutines that operate on feature objects.
+ */
+define([
+       ],
+       function(
+       ) {
+
+return {
+    plusStrand: function(feature)  {
+        var strand = feature.get('strand');
+        if (strand == 1 || strand == '+')  { return true; }
+        else  { return false; }
+    },
+    minusStrand: function(feature)  {
+        var strand = feature.get('strand');
+        if (strand == -1 || strand == '-')  { return true; }
+        else  { return false; }
+    },
+    all: function(feature) {
+        return true;
+    },
+    none: function(feature)  {
+        return false;
+    }
+};
+});
