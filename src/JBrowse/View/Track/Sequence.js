157a4ae81c71f0ea65aadbc25c7a0c22c89ba984
Daniel Kasenberg <daniel.kasenberg@gmail.com>
2013-08-19 16:47:16 -0400

    Began enhancing Sequence track to allow visualization of six-frame translation of sequence

--- src/JBrowse/View/Track/Sequence.js
+++ src/JBrowse/View/Track/Sequence.js
@@ -1,10 +1,12 @@
 define( [
             'dojo/_base/declare',
+            'dojo/dom-construct',
             'JBrowse/View/Track/BlockBased',
             'JBrowse/View/Track/ExportMixin',
+            'JBrowse/CodonTable',
             'JBrowse/Util'
         ],
-        function( declare, BlockBased, ExportMixin, Util ) {
+        function( declare, dom, BlockBased, ExportMixin, CodonTable, Util ) {
 
 return declare( [BlockBased, ExportMixin],
  /**
@@ -44,11 +46,15 @@ return declare( [BlockBased, ExportMixin],
     nbsp: String.fromCharCode(160),
 
     fillBlock:function( args ) {
+
         var blockIndex = args.blockIndex;
         var block = args.block;
         var leftBase = args.leftBase;
         var rightBase = args.rightBase;
         var scale = args.scale;
+
+        var leftExtended = leftBase - 2;
+        var rightExtended = rightBase + 2;
 
         var charSize = this.getCharacterMeasurements();
 
@@ -58,8 +64,8 @@ return declare( [BlockBased, ExportMixin],
                 {
                     ref: this.refSeq.name,
                     seqChunkSize: this.refSeq.seqChunkSize,
-                    start: leftBase,
-                    end: rightBase
+                    start: leftExtended,
+                    end: rightExtended
                 },
                 dojo.hitch( this, '_fillSequenceBlock', block, scale ),
                 function() {}
@@ -100,22 +106,90 @@ return declare( [BlockBased, ExportMixin],
                 seq += this.nbsp;
             }
 
+        var blockStart = start + 2;
+        var blockEnd = end - 2;
+        var blockSeq = seq.substring( 2, seq.length - 2 );
+        var blockLength = blockSeq.length;
+
+        var extStart = start;
+        var extEnd = end;
+        var extStartSeq = seq.substring( 0, seq.length - 2 );
+        var extEndSeq = seq.substring( 2 );
+
+        console.log(blockStart, extStart );
+
+        var frameDiv = [];
+        for( var i = 0; i < 3; i++ ) {
+            var transStart = blockStart + i;
+            var frame = (transStart % 3 + 3) % 3;
+            var translatedDiv = this._renderTranslation( extEndSeq, i, blockStart, blockEnd, blockLength, scale );
+            frameDiv[frame] = translatedDiv;
+        }
+        for( var i = 2; i >= 0; i-- ) {
+            block.domNode.appendChild( frameDiv[i] );
+        }
+
 
         // make a div to contain the sequences
-        var seqNode = document.createElement("div");
-        seqNode.className = "sequence";
-        seqNode.style.width = "100%";
-        block.domNode.appendChild(seqNode);
+        var seqNode = dom.create("div", { className: "sequence", style: { width: "100%"} }, block.domNode);
 
         // add a div for the forward strand
-        seqNode.appendChild( this._renderSeqDiv( start, end, seq, scale ));
+        seqNode.appendChild( this._renderSeqDiv( blockStart, blockEnd, blockSeq, scale ));
 
         // and one for the reverse strand
         if( this.config.showReverseStrand ) {
-            var comp = this._renderSeqDiv( start, end, Util.complement(seq), scale );
+            var comp = this._renderSeqDiv( blockStart, blockEnd, Util.complement(blockSeq), scale );
             comp.className = 'revcom';
             seqNode.appendChild( comp );
         }
+    },
+
+    _renderTranslation: function( seq, offset, blockStart, blockEnd, blockLength, scale, reverse ) {
+        seq = reverse ? Util.revcom( seq ) : seq;
+
+        var extraBases = (seq.length - offset) % 3;
+        var seqSliced = seq.slice( offset, seq.length - extraBases );
+
+        var translated = "";
+        for( var i = 0; i < seqSliced.length; i += 3 ) {
+            var nextCodon = seqSliced.slice(i, i + 3);
+            var aa = CodonTable[nextCodon] || this.nbsp;
+            translated = translated + aa;
+        }
+
+        console.log( seq, offset, translated );
+
+        var charSize = this.getCharacterMeasurements();
+
+        var charWidth = 100/(blockLength / 3);
+
+        var container  = dom.create('div',
+            {
+                className: 'translatedSequence frame'+offset,
+                style:
+                {
+                    left: (charWidth * offset / 3) + "%",
+                    width: (charWidth * translated.length) + "%"
+                }
+            });
+
+        charWidth = 100/ translated.length + "%";
+
+        var drawChars = scale >= charSize.w;
+        var bigTiles = scale > charSize.w + 4; // whether to add .big styles to the base tiles
+
+        for( var i=0; i<translated.length; i++ ) {
+            var aaSpan = document.createElement('span');
+            aaSpan.className = 'aa aa_'+translated.charAt([i]).toLowerCase();
+            aaSpan.style.width = charWidth;
+            if( drawChars ) {
+                if( bigTiles )
+                    aaSpan.className = aaSpan.className + ' big';
+                aaSpan.innerHTML = translated.charAt([i]);
+            }
+            container.appendChild(aaSpan);
+        }
+        return container;
     },
 
     /**
