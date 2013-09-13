bc8ac245d3691de3011879060f0e9421065f1dde
Erik Derohanian <erik.derohanian@gmail.com>
2013-09-13 15:46:30 -0400

    rearanged parser, now checks for start point <= end point

--- src/JBrowse/Store/SeqFeature/Cytoband/Parser.js
+++ src/JBrowse/Store/SeqFeature/Cytoband/Parser.js
@@ -2,42 +2,19 @@ define([
            'dojo/_base/declare',
        ],
        function(
-           declare,
+           declare
        ) {
 
 return declare( null, {
 
     constructor: function( args ) {},
 
-    parseFile: function( lines ) {
+    parseFile: function (lines) {
 
-        var cytobandRegex = // chrom \t chromStart \t chromEnd \t name \t gieStain 
-        /(.+)\t(\d+)\t(\d+)\t(.+)\t(gneg|gpos50|gpos75|gpos25|gpos100|acen|gvar|stalk)/;
-
-        cytoLines = lines.split(/\r\n|\r|\n/);
-
-        var parseCytoBand = function(line){
-            if (match = line.match(cytobandRegex)){ //if it's a match, print it, if not, say so
-            
-               tmpCytoBand = {
-                    "chrom" : match[1],
-                    "chromStart" : match[2],
-                    "chromEnd" : match[3],
-                    "name" : match[4],
-                    "gieStain" : match[5]
-                };
-                
-                console.log(tmpCytoBand);
-                return tmpCytoBand;
-
-            } else { 
-                return undefined;
-            }    
-        }
-
-        cytoHolder = [];
+        var cytoLines = lines.split(/\r?\n/);
+        var cytoHolder = [];
         for (var line in cytoLines){
-            var parsedLine = parseCytoBand(cytoLines[line]);
+            var parsedLine = this._parseCytoBand(cytoLines[line]);
             if (parsedLine){
                 cytoHolder.push(parsedLine);
             } else {
@@ -46,5 +23,36 @@ return declare( null, {
         }
     },
 
-   });
+    _parseCytoBand : function (line) {
+ 
+        var cytobandRegex = // chrom \t chromStart \t chromEnd \t name \t gieStain 
+            /(.+)\t(\d+)\t(\d+)\t(.+)\t(gneg|gpos50|gpos75|gpos25|gpos100|acen|gvar|stalk)/;
+
+        var match = line.match(cytobandRegex);
+        match = match && this._checkMatch(match) ? match : undefined;
+
+        if (match) {    //if it's a match, print it, if not, say so
+            tmpCytoBand = {
+                "chrom" : match[1],
+                "chromStart" : match[2],
+                "chromEnd" : match[3],
+                "name" : match[4],
+                "gieStain" : match[5]
+            };
+            
+            console.log(tmpCytoBand);
+            return tmpCytoBand;
+        } else { 
+            return undefined;
+        }    
+    }
+
+    _checkMatch : function (match) {
+        if (match.chromStart <= match.chromEnd){
+            return true;
+        } else {
+            return false;
+        }
+    }
+});
 });
