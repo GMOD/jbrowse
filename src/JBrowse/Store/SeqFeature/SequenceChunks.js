0881a643e5c71cde215c7a223088c79c7e88498b
Robert Buels <rmb32@cornell.edu>
2013-09-25 16:12:45 -0400

    port chunked sequence store to use Transport system and DeferredGenerator

--- src/JBrowse/Store/SeqFeature/SequenceChunks.js
+++ src/JBrowse/Store/SeqFeature/SequenceChunks.js
@@ -4,23 +4,31 @@
  */
 define( [ 'dojo/_base/declare',
           'dojo/_base/lang',
-          'dojo/request',
           'dojo/promise/all',
           'dojo/Deferred',
           'JBrowse/Store/SeqFeature',
+          'JBrowse/Model/Resource/Text',
+          'JBrowse/Model/Resource/JSON',
           'JBrowse/Util',
+          'JBrowse/Util/DeferredGenerator',
           'JBrowse/Model/SimpleFeature',
+          'JBrowse/Model/ReferenceSequence',
+          'JBrowse/Model/BigSequence',
           'JBrowse/Digest/Crc32'
         ],
         function(
             declare,
             lang,
-            request,
             all,
             Deferred,
             SeqFeatureStore,
+            TextResource,
+            JSONResource,
             Util,
+            DeferredGenerator,
             SimpleFeature,
+            ReferenceSequence,
+            BigSequence,
             Crc32
         ) {
 
@@ -35,79 +43,90 @@ return declare( SeqFeatureStore,
         ]
     },
 
-    getFeatures: function( query, featureCallback, endCallback, errorCallback ) {
-        var thisB = this;
-        errorCallback = errorCallback || function(e) { console.error(e); };
-
-        var refname = query.ref;
-        var chunkSize  = query.seqChunkSize || this.getConf('chunkSize');
-
-        this.getRefSeqMeta(
-            { name: refname, limit: 1 },
-            function(r) { refname = r.name; },
-            function() {
-                var sequrl = thisB.resolveUrl(
-                    thisB.getConf('urlTemplate'),
-                    {
-                        'refseq': refname,
-                        'refseq_dirpath': function() {
-                            var hex = Crc32.crc32( refname )
-                                .toString(16)
-                                .toLowerCase()
-                                .replace('-','n');
-                            // zero-pad the hex string to be 8 chars if necessary
-                            while( hex.length < 8 )
-                                hex = '0'+hex;
-                            var dirpath = [];
-                            for( var i = 0; i < hex.length; i += 3 ) {
-                                dirpath.push( hex.substring( i, i+3 ) );
-                            }
-                            return dirpath.join('/');
-                        }
+    _makeChunkURLBase: function( refname ) {
+        return this.resolveUrl(
+            this.getConf('urlTemplate'),
+            {
+                'refseq': refname,
+                'refseq_dirpath': function() {
+                    var hex = Crc32.crc32( refname )
+                        .toString(16)
+                        .toLowerCase()
+                        .replace('-','n');
+                    // zero-pad the hex string to be 8 chars if necessary
+                    while( hex.length < 8 )
+                        hex = '0'+hex;
+                    var dirpath = [];
+                    for( var i = 0; i < hex.length; i += 3 ) {
+                        dirpath.push( hex.substring( i, i+3 ) );
                     }
-                );
-
-                var firstChunk = Math.floor( Math.max(0,query.start) / chunkSize );
-                var lastChunk  = Math.floor( (query.end - 1)         / chunkSize );
-
-                var error;
-                var fetches = [];
-                for( var chunkNum = firstChunk; chunkNum <= lastChunk; chunkNum++ ) {
-                    (function( chunkNum ) {
-                         var d = new Deferred(); // need to have our own deferred that is resolved to '' on 404
-                         thisB._fetchChunk( sequrl, chunkNum )
-                                 .then( lang.hitch( d, 'resolve' ),
-                                        function( e ) {
-                                            if( e.response.status == 404 )
-                                                d.resolve( '' );
-                                            else
-                                                d.reject( e );
-                                        }
-                                      );
-                         d.then( function( sequenceString ) {
-                                     if( error )
-                                             return;
-                                     featureCallback( thisB._makeFeature( refname, chunkNum, chunkSize, sequenceString ) );
-                                 },
-                                 function( e ) {
-                                     if( !error )
-                                         errorCallback( error = e );
-                                 });
-                         fetches.push( d );
-                     }).call(thisB,chunkNum);
+                    return dirpath.join('/');
                 }
-
-
-                all( fetches ).then( endCallback );
-            },
-            errorCallback
+            }
         );
     },
 
-    _fetchChunk: function( sequrl, chunkNum ) {
-        return request.get( sequrl + chunkNum + ".txt" + ( this.getConf('compress') ? 'z' : '' ),
-                            { handleAs: 'text' }
-                          );
+    getSequenceFragments: function( query ) {
+        return this.getFeatures( query );
+    },
+
+    getFeatures: function( query ) {
+        var thisB = this;
+        var refname;
+        var chunkSize  = query.seqChunkSize || thisB.getConf('chunkSize');
+        return new DeferredGenerator( function( generator ) {
+            return thisB.getReferenceSequences({ name: query.ref, limit: 1 })
+                .forEach( function(r) {
+                              refname = r.get('name');
+                          },
+                          function() {
+                              // if no refname, the refseq was not found
+                              if( ! refname ) {
+                                  generator.resolve();
+                                  return null;
+                              }
+
+                              var urlbase = thisB._makeChunkURLBase( refname );
+
+                              // fetch all the relevant chunk text files
+                              var firstChunk = Math.floor( Math.max(0,query.start) / chunkSize );
+                              var lastChunk  = Math.floor( (query.end - 1)         / chunkSize );
+                              var fetches = [];
+                              for( var chunkNum = firstChunk; chunkNum <= lastChunk; chunkNum++ ) {
+                                  fetches.push(
+                                      thisB._fetchChunkSequence( urlbase, chunkNum )
+                                          .then( lang.hitch(
+                                                     thisB,
+                                                     function( chunkNum, seqString ) {
+                                                         var feature = thisB._makeFeature( refname, chunkNum, chunkSize, seqString );
+                                                         generator.emit( feature );
+                                                     },
+                                                     chunkNum
+                                                 ))
+                                  );
+                              }
+
+                              return all( fetches );
+                          }
+                        );
+        });
+    },
+
+    _fetchChunkSequence: function( urlbase, chunkNum ) {
+        var url = urlbase + chunkNum + ".txt" + ( this.getConf('compress') ? 'z' : '' );
+        var resource = this.openResource( TextResource, url );
+
+        var d = new Deferred(); // need to have our own deferred that is resolved to '' on 404
+        resource.fetch()
+            .then( lang.hitch( d, 'resolve' ),
+                   function( e ) {
+                       if( e.response.status == 404 )
+                           d.resolve( '' );
+                       else
+                           d.reject( e );
+                   }
+                 );
+        return d;
     },
 
     _makeFeature: function( refname, chunkNum, chunkSize, sequenceString ) {
@@ -122,37 +141,67 @@ return declare( SeqFeatureStore,
         });
     },
 
-    _getRefSeqsInfo: function() {
+    _fetchRefSeqsJson: function() {
         return this._refSeqsInfo || function() {
             var thisB = this;
-            return this._refSeqsInfo =
-                request.get( this.resolveUrl( this.getConf('refSeqs') ), { handleAs: 'json' } )
-                       .then( function( r ) {
-                                  var refsByName = {};
-                                  for( var i = 0; i<r.length; i++ ) {
-                                      refsByName[ thisB.browser.regularizeReferenceName( r[i].name ) ] = r[i];
-                                  }
-                                  return refsByName;
-                              });
+            var resource = this.openResource( JSONResource, this.resolveUrl( this.getConf('refSeqs') ) );
+            return this._refSeqsInfo = resource.fetch()
+                .then( function( refseqs ) {
+                           var refsByName = {};
+                           for( var i = 0; i<refseqs.length; i++ ) {
+                               refsByName[ thisB.browser.regularizeReferenceName( refseqs[i].name ) ] = refseqs[i];
+                           }
+                           return refsByName;
+                       });
         }.call(this);
     },
 
-    getRefSeqMeta: function( query, refSeqCallback, finishCallback, errorCallback ) {
+    _makeReferenceSequence: function( data ) {
+        if( ! data )
+            return undefined;
+        if( data instanceof ReferenceSequence )
+            return data;
+
+        data = lang.mixin( { seq_id: data.name }, data );
+        delete data.length;
+        delete data.seqChunkSize;
+        return new ReferenceSequence({
+            data: data,
+            id: data.name,
+            bigSequence: new BigSequence({ store: this, name: data.name, start: data.start, end: data.end })
+        });
+    },
+
+    _getReferenceSequence: function( name ) {
         var thisB = this;
-        this._getRefSeqsInfo().then( function( refSeqs ) {
-            if( query.name ) {
-                refSeqCallback( refSeqs[ thisB.browser.regularizeReferenceName( query.name ) ] );
-                finishCallback();
-            }
-            else {
-                var limit = query.limit || Infinity;
-                for( var n in refSeqs ) {
-                    refSeqCallback( refSeqs[n] );
-                    if( ! --limit )
-                        break;
-                }
-                finishCallback();
-            }
+        name = this.browser.regularizeReferenceName( name );
+        return thisB._fetchRefSeqsJson()
+            .then( function( refseqs ) {
+                       return refseqs[ name ] = thisB._makeReferenceSequence( refseqs[name] );
+                   });
+    },
+
+    getReferenceSequences: function( query ) {
+        var thisB = this;
+        return new DeferredGenerator( function( generator ) {
+            if( query.name || query.ref )
+                thisB._getReferenceSequence( query.name || query.ref )
+                     .then( function(ref) {
+                                generator.emit(ref);
+                                generator.resolve();
+                            },
+                            lang.hitch( generator, 'reject' ));
+            else
+                thisB._fetchRefSeqsJson()
+                     .then( function( refSeqs ) {
+                                var limit = query.limit || Infinity;
+                                for( var n in refSeqs ) {
+                                    generator.emit( refSeqs[n] = thisB._makeReferenceSequence( refSeqs[n] ) );
+                                    if( ! --limit )
+                                        break;
+                                }
+                                generator.resolve();
+                            });
         });
     }
 
--- tests/js_tests/spec/SequenceChunkStore.spec.js
+++ tests/js_tests/spec/SequenceChunkStore.spec.js
@@ -17,11 +17,12 @@ describe( 'sequence chunk store', function() {
 
               it( 'fetches some features', function() {
                       var done, features = [];
-                      s.getFeatures( { ref: 'ctga', start: 100, end: 40000 },
-                                     function(f) { features.push(f); },
-                                     function() { done = true; },
-                                     function(e) { console.error(e); }
-                                   );
+                      s.getFeatures( { ref: 'ctga', start: 100, end: 40000 } )
+                          .each( function(f) { features.push(f); },
+                                 function() { done = true; },
+                                 function(e) { console.error( e.stack || ''+e); }
+                               )
+                          .run();
                       waitsFor(function() { return done; });
                       runs( function() {
                                 expect( features.length ).toEqual( 2 );
@@ -30,123 +31,57 @@ describe( 'sequence chunk store', function() {
                             });
               });
 
-              it( 'fetches ref seq as string 1', function() {
+              function testfetch( name, start, end, expectSeq ) {
                       var seq;
-                      s.getReferenceSequence(
-                          { ref: 'ctga', start: 0, end: 5 },
-                          function(s) { seq = s; },
-                          function(e) { console.error(e); }
-                      );
-                      waitsFor(function() { return seq; });
-                      runs( function() {
-                                expect( seq ).toEqual( "cattg" );
-                            });
-              });
+                      var d = s.getReferenceSequences({ ref: name } )
+                          .forEach( function(s) { seq = s; },
+                                    function( ) { return seq.getSequence(start,end); },
+                                    function(e) { console.error(e); }
+                                  )
+                          .then( function( sequenceFragment ) {
+                                     seq = sequenceFragment;
+                                 });
 
-              it( 'fetches ref seq as string 2', function() {
-                      var seq;
-                      s.getReferenceSequence(
-                          { ref: 'ctga', start: 1, end: 5 },
-                          function(s) { seq = s; },
-                          function(e) { console.error(e); }
-                      );
-                      waitsFor(function() { return seq; });
+                      waitsFor(function() { return d.isFulfilled(); });
                       runs( function() {
-                                expect( seq ).toEqual( "attg" );
+                                expect( seq ).toEqual( expectSeq );
                             });
+              }
+
+              it( 'fetches ref seq as string 1', function() {
+                      testfetch( 'ctga', 0, 5, 'cattg' );
+              });
+              it( 'fetches ref seq as string 2', function() {
+                      testfetch( 'ctga', 1, 5, 'attg' );
               });
 
               it( 'fetches ref seq as string 3', function() {
-                      var seq;
-                      s.getReferenceSequence(
-                          { ref: 'ctga', start: 49999, end: 50001 },
-                          function(s) { seq = s; },
-                          function(e) { console.error(e); }
-                      );
-                      waitsFor(function() { return seq; });
-                      runs( function() {
-                                expect( seq ).toEqual( "ac" );
-                            });
+                      testfetch( 'ctga', 49999, 50001, 'ac' );
               });
 
               it( 'fetches ref seq as string 4', function() {
-                      var seq;
-                      s.getReferenceSequence(
-                          { ref: 'ctga', start: 100, end: 105 },
-                          function(s) { seq = s; },
-                          function(e) { console.error(e); }
-                      );
-                      waitsFor(function() { return seq; });
-                      runs( function() {
-                                expect( seq ).toEqual( "agcgg" );
-                            });
+                      testfetch( 'ctga', 100, 105, 'agcgg' );
               });
 
               it( 'fetches ref seq as string 5', function() {
-                      var seq;
-                      s.getReferenceSequence(
-                          { ref: 'ctga', start: 19996, end: 20005 },
-                          function(s) { seq = s; },
-                          function(e) { console.error(e); }
-                      );
-                      waitsFor(function() { return seq; });
-                      runs( function() {
-                                expect( seq ).toEqual( "ttaccgcgt" );
-                            });
+                      testfetch( 'ctga', 19996, 20005, 'ttaccgcgt' );
               });
 
               it( 'fetches ref seq as string 6, with space padding at the beginning', function() {
-                      var seq;
-                      s.getReferenceSequence(
-                          { ref: 'ctga', start: -3, end: 5 },
-                          function(s) { seq = s; },
-                          function(e) { console.error(e); }
-                      );
-                      waitsFor(function() { return seq; });
-                      runs( function() {
-                                expect( seq ).toEqual( "   cattg" );
-                            });
+                      testfetch( 'ctga', -3, 5, "   cattg");
               });
 
               it( 'fetches ref seq as string 7, with space padding at the end', function() {
-                      var seq;
-                      s.getReferenceSequence(
-                          { ref: 'ctga', start: 49999, end: 50003 },
-                          function(s) { seq = s; },
-                          function(e) { console.error(e); }
-                      );
-                      waitsFor(function() { return seq; });
-                      runs( function() {
-                                expect( seq ).toEqual( "ac  " );
-                            });
+                      testfetch( 'ctga', 49999, 50003, "ac  ");
               });
 
               it( 'fetches ref seq as string 8, with space padding at the beginning', function() {
-                      var seq;
-                      s.getReferenceSequence(
-                          { ref: 'ctga', start: -5, end: 1 },
-                          function(s) { seq = s; },
-                          function(e) { console.error(e); }
-                      );
-                      waitsFor(function() { return seq; });
-                      runs( function() {
-                                expect( seq ).toEqual( "     c" );
-                            });
+                      testfetch( 'ctga', -5, 1, '     c' );
               });
 
               it( 'fetches ref seq as string 9, with space padding at the beginning', function() {
-                      var seq;
-                      s.getReferenceSequence(
-                          { ref: 'ctga', start: -28, end: 1, seqChunkSize: 20000 },
-                          function(s) { seq = s; },
-                          function(e) { console.error(e); }
-                      );
-                      waitsFor(function() { return seq; });
-                      runs( function() {
-                                expect( seq ).toEqual( "                            c" );
-                            });
+                      testfetch( 'ctga', -28, 1, "                            c" );
               });
-
 }
 );
 });
