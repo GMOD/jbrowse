822f296da225a819508e072fbc9b979905d0476c
Robert Buels <rmb32@cornell.edu>
2013-10-01 15:43:08 -0400

    improve error styling in credential widgets

--- src/JBrowse/View/Auth/Keyring/CredentialWidget.js
+++ src/JBrowse/View/Auth/Keyring/CredentialWidget.js
@@ -121,11 +121,11 @@ return declare( [_WidgetBase, _TemplatedMixin, _Contained, _CssStateMixin],  {
 
     _onCredentialErrorChange: function() {
         var credentialError = this.get('credentialError');
-        if( credentialError && !( credentialError instanceof Errors.UserCancel )) {
+        if( credentialError && !( credentialError instanceof Errors.UserCancel ) ) {
             cssClass.add( this.containerNode, 'credentialError' );
             this.errorNode.innerHTML = ''+credentialError;
             this.errorNode.title = ''+credentialError;
-            new ErrorDialog({ description: ''+credentialError, diagnosticMessage: credentialError.stack || ''+credentialError }).show();
+            //new ErrorDialog({ description: ''+credentialError, diagnosticMessage: credentialError.stack || ''+credentialError }).show();
         }
         else {
             this.errorNode.innerHTML = ' ';
