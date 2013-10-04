GMOD/jbrowse

commit e068cca8faee253f273647adb14c4ca861c14e60

remove wiggle scale normalization's use of with(), I think it might have been confusing the closure compiler.  fixes #347

src/JBrowse/View/Track/Wiggle/_Scale.js
@@ -6,10 +6,14 @@
  */
 
 define([
+           'dojo/_base/lang',
            'JBrowse/Util',
            'JBrowse/Digest/Crc32'
        ],
-       function( Util, Digest ) {
+       function( lang,
+                 Util,
+                 Digest
+               ) {
 return Util.fastDeclare({
 
     // Returns a boolean value saying whether a stats object is needed
@@ -32,7 +36,7 @@ return Util.fastDeclare({
             throw 'No stats object provided, cannot calculate scale';
 
         if( needStats && stats.scoreMin == stats.scoreMax ) {
-            stats = dojo.clone( stats );
+            stats = lang.mixin( {}, stats );
             if( stats.scoreMin < 0 )
                 stats.scoreMax = 0;
             else
@@ -111,39 +115,37 @@ return Util.fastDeclare({
           }
         })();
 
-        dojo.mixin( this, {
+        lang.mixin( this, {
             offset: offset,
             min: min,
             max: max,
             range: max - min,
             origin: origin,
+            scoreMean: stats.scoreMean,
+            scoreStdDev: stats.scoreStdDev,
             _statsFingerprint: Digest.objectFingerprint( stats )
         });
 
         // make this.normalize a func that converts wiggle values to a
         // range between 0 and 1, depending on what kind of scale we
         // are using
-        var thisB = this;
-        this.normalize = (function() {
+        this.normalize = (function(config) {
             switch( config.scale ) {
             case 'z_score':
                 return function( value ) {
-                    with(thisB)
-                        return (value+offset-stats.scoreMean) / stats.scoreStdDev-min / range;
+                    return (value+this.offset-this.scoreMean) / this.scoreStdDev-this.min / this.range;
                 };
             case 'log':
                 return function( value ) {
-                    with(thisB)
-                        return ( thisB.log(value+offset) - min )/range;
+                    return ( this.log(value+this.offset) - this.min )/this.range;
                 };
             case 'linear':
             default:
                 return function( value ) {
-                    with(thisB)
-                        return ( value + offset - min ) / range;
+                    return ( value + this.offset - this.min ) / this.range;
                 };
             }
-        })();
+        })(config);
     },
 
     log: function( value ) {

src/JBrowse/View/Track/Wiggle/Density.js
@@ -43,8 +43,8 @@ return declare( WiggleBase,
         var featureColor = typeof this.config.style.color == 'function' ? this.config.style.color :
             (function() { // default color function uses conf variables
                 var disableClipMarkers = thisB.config.disable_clip_markers;
-                var normOrigin = normalize( dataScale.origin );
-                
+                var normOrigin = dataScale.normalize( dataScale.origin );
+
                 return function( p , n) {
                     var feature = p['feat'];
                     return ( disableClipMarkers || n <= 1 && n >= 0 )
@@ -65,8 +65,8 @@ return declare( WiggleBase,
             if (p) {
                 var score = p['score'];
                 var f = p['feat'];
-                
-                var n = normalize( score );
+
+                var n = dataScale.normalize( score );
                 context.fillStyle = ''+featureColor( p, n );
                 context.fillRect( i, 0, 1, canvasHeight );
                 if( n > 1 ) { // pos clipped

src/JBrowse/View/Track/Wiggle/XYPlot.js
@@ -78,11 +78,12 @@ var XYPlot = declare( [WiggleBase, YScaleMixin],
         var context = canvas.getContext('2d');
         var canvasHeight = canvas.height;
         var toY = dojo.hitch( this, function( val ) {
-           return canvasHeight * ( 1-dataScale.normalize.call(this, val) );
+           return canvasHeight * ( 1-dataScale.normalize(val) );
         });
         var originY = toY( dataScale.origin );
 
         var disableClipMarkers = this.config.disable_clip_markers;
+        console.log( originY, canvasHeight, JSON.stringify(dataScale) );
 
         dojo.forEach( pixels, function(p,i) {
             if (!p)
@@ -182,7 +183,7 @@ var XYPlot = declare( [WiggleBase, YScaleMixin],
         var context = canvas.getContext('2d');
         var canvasHeight = canvas.height;
         var toY = dojo.hitch( this, function( val ) {
-           return canvasHeight * (1-dataScale.normalize.call(this, val));
+           return canvasHeight * (1-dataScale.normalize(val));
         });
 
         // draw the variance_band if requested

commit 2b84a5183684d8499131d99a2127a32cebef13ae

fix another bug introduced by e068cca8faee253f273647adb14c4ca861c14e60, fixes #357

src/JBrowse/View/Track/Wiggle/_Scale.js
@@ -121,10 +121,12 @@ return Util.fastDeclare({
             max: max,
             range: max - min,
             origin: origin,
-            scoreMean: stats.scoreMean,
-            scoreStdDev: stats.scoreStdDev,
             _statsFingerprint: Digest.objectFingerprint( stats )
         });
+        if( needStats ) {
+            this.scoreMean = stats.scoreMean;
+            this.scoreStdDev = stats.scoreStdDev;
+        }
 
         // make this.normalize a func that converts wiggle values to a
         // range between 0 and 1, depending on what kind of scale we