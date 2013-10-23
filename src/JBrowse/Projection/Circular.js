d19a1da8707f4cc7da40b871941f2b632e2019e8
Robert Buels <rmb32@cornell.edu>
2013-10-23 19:17:52 -0400

    bugfixes to circular projection

--- src/JBrowse/Projection/Circular.js
+++ src/JBrowse/Projection/Circular.js
@@ -14,20 +14,19 @@ define([
 return declare( 'JBrowse.Projection.Circular', [ LinearProjection,_CanonicalZoomLevelsMixin ], {
 
    constructor: function(args) {
-       if( ! this.bOrigin ) this.bOrigin = 0;
        if( ! this.bLength ) throw new Error('bLength arg required');
    },
 
-   _modB: function( b, length, origin ) {
-       if( b >= origin )
-           return ( b - origin ) % length + origin;
+   _modB: function( b, length ) {
+       if( b >= 0 )
+           return  b % length;
        else
-           return length + origin - ( origin - b ) % length;
+           return length + b % length;
    },
 
    projectPoint: function( a ) {
        var b = this.inherited(arguments);
-       return this._modB( b, this.bLength, this.bOrigin );
+       return this._modB( b, this.bLength );
    },
 
    getBlocksForRange: function( a1, a2 ) {
@@ -38,16 +37,16 @@ return declare( 'JBrowse.Projection.Circular', [ LinearProjection,_CanonicalZoom
        }
 
        var b1 = this.projectPoint( a1 );
-       var modBOffset = this._modB( this.bOffset, this.bLength, this.bOrigin );
+       var modBOffset = this._modB( this.bOffset, this.bLength );
        var blocks = [];
        for( var aStart = Math.max( this.aStart, a1 - ( this.scale > 0 ? b1 : b1-this.bLength )/this.scale );
             aStart < a2 && aStart < this.aEnd;
             aStart = aEnd+1
           ) {
-              var bOffset = modBOffset + ( this.scale > 0 ? -1 : 1 )*this.bLength*blocks.length;
+              var bOffset = ( this.scale > 0 ? 0 : this.bLength ) - aStart*this.scale;
               var aEnd = Math.min(
                   this.aEnd,
-                  ( ( this.scale > 0 ? this.bLength : 0 ) - bOffset ) / this.scale
+                  ((this.scale > 0 ? this.bLength : 0 )-bOffset)/this.scale
               );
               blocks.push(
                   new LinearProjection(
