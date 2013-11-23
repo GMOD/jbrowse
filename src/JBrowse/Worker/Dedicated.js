50950db2ae84ee0b0596a0e5fdc4f0021c75e712
Robert Buels <rmb32@cornell.edu>
2013-11-23 11:17:29 -0500

    work on web worker job management

--- src/JBrowse/Worker/Dedicated.js
+++ src/JBrowse/Worker/Dedicated.js
@@ -1,17 +1,57 @@
+/**
+ * Main object for managing jobs inside a Worker.  Lives in a worker process.
+ */
 define([
-           'dojo/_base/declare'
+           'dojo/_base/declare',
+           'dojo/_base/lang',
+           'dojo/Deferred',
+           'dojo/when',
+
+           'JBrowse/Util/Serialization'
        ],
        function(
-           declare
+           declare,
+           lang,
+           Deferred,
+           when,
+
+           Serialization
        ) {
 
 return declare( null, {
 constructor: function(args) {
     this._self = args.self;
     this._self.postMessage('ready');
+
+    this._self.onmessage = lang.hitch( this, '_handleMessage' );
+},
+
+_handleMessage: function( event ) {
+    var data = event.data;
+    if( data && data.requestNumber && data.operation && this[data.operation] ) {
+        var thisB = this;
+        Serialization.inflate( data.args || [] )
+            .then( function( args ) {
+                       return when( thisB[data.operation].apply( thisB, args ) )
+                           .then( function( result ) {
+                               thisB._self.postMessage(
+                                   { requestNumber: data.requestNumber,
+                                     result: Serialization.deflate( result )
+                                   });
+                             });
+                   },
+                   function(e) {
+                       console.error( e.stack || ''+e, e );
+                   });
+    }
+    else {
+        console.warn( "unknown request received by worker", event );
+    }
+},
+
+apply: function( object, method, arguments ) {
+    return object[method].apply( object, arguments );
 }
-
-
 
 });
 });
--- src/JBrowse/Worker/Handle.js
+++ src/JBrowse/Worker/Handle.js
@@ -1,25 +1,73 @@
+/**
+ * Wrapper object for a Worker.  Lives in the main process.
+ */
 define([
            'dojo/_base/declare',
-           'dojo/_base/lang'
+           'dojo/_base/lang',
+           'dojo/Deferred',
+
+           'JBrowse/Util/Serialization'
        ],
        function(
            declare,
-           lang
+           lang,
+           Deferred,
 
+           Serialization
        ) {
+
+var serialNumber = 0;
+
 return declare( null, {
   constructor: function( args ) {
       this._worker = args.worker;
       this._worker.onmessage = lang.hitch( this, '_handleMessage' );
+      this._worker.onerror   = lang.hitch( this, '_handleError' );
+      this._requests = {};
+  },
+
+  _handleError: function( errorEvent ) {
+      console.error( errorEvent );
   },
 
   _handleMessage: function( event ) {
-      console.log( 'worker said', event.data, event );
+      var data = event.data;
+      var requestRecord;
+      if( data && data.requestNumber && (requestRecord = this._requests[ data.requestNumber ] )) {
+          delete this._requests[ data.requestNumber ];
+          if( data.error ) {
+              requestRecord.deferred.reject( data.error );
+          }
+          else if( data.result ) {
+              Serialization.inflate( data.result )
+                  .then( requestRecord.deferred.resolve, requestRecord.deferred.reject );
+          }
+      }
+      else {
+          console.warn( "unknown message from worker", event );
+      }
   },
 
   getWorker: function() {
       return this._worker;
-  }
+  },
 
+  apply: function( obj, methodName, args ) {
+      if( typeof methodName != 'string' )
+          throw new Error('must pass a string method name');
+
+      var requestNumber = ++serialNumber;
+
+      this._worker.postMessage( Serialization.deflate(
+          { requestNumber: requestNumber,
+            operation: 'apply',
+            args: [ obj, methodName, args ]
+          }));
+      var deferred = new Deferred();
+      this._requests[ requestNumber ] = {
+          deferred: deferred
+      };
+      return deferred;
+  }
 });
 });
