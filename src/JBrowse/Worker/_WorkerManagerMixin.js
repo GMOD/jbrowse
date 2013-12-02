adfafed6490b6a10bb3aa3a889da082276f09e06
Robert Buels <rmb32@cornell.edu>
2013-12-02 14:36:52 -0500

    refactor _WorkerManagerMixin a bit to allow eventual expansion to support a "fake" worker that would work on old IE

--- src/JBrowse/Worker/_WorkerManagerMixin.js
+++ src/JBrowse/Worker/_WorkerManagerMixin.js
@@ -22,44 +22,48 @@ return declare( null, {
 
   getWorker: function( taskGroup, taskIdentifier, workerClass ) {
       return this._worker || ( this._worker = function() {
-          var thisB = this;
-          var d = new Deferred();
-          var worker = new Worker( require.toUrl('JBrowse/Worker/boot_dedicated.js') );
-          var timeout;//  =setTimeout( function() {
-          //     d.reject( new Error( 'worker was not ready within '+thisB.getConf('workerStartTimeout')+' seconds' ) );
-          // }, thisB.getConf('workerStartTimeout')*1000 );
-          worker.onerror = function(e) { console.error(e); d.reject(e); };
-          worker.onmessage = function( event ) {
-              if( event.data == 'starting' ) {
-                  // do nothing
-              }
-              else if( event.data == 'ready' ) {
-                  if( timeout ) { clearTimeout( timeout ); timeout = undefined; }
-                  d.resolve( new WorkerHandle({ worker: worker, authManager: thisB }) );
-              }
-              else {
-                  d.reject( new Error( event ) );
-              }
-          };
-          worker.postMessage( JSON.stringify(
-                                  { preload: [
-                                        '../../dojo/dojo.js'
-                                        , '../init.js'
-                                    ],
-                                    //delay: 3000,
-                                    require: { baseUrl: '../..' },
-                                    config: this.exportMergedConfig(),
-                                    workerClass: workerClass || 'JBrowse/Worker/Dedicated'
-                                  }));
+          return this._makeRealWorker( workerClass );
+      }.call(this));
+  },
 
-          return d.then(
-              undefined,
-              function(e) {
-                  if( timeout ) { clearTimeout( timeout ); timeout = undefined; }
-                  worker.terminate();
-                  throw e;
-              });
-      }.call(this) );
+  _makeRealWorker: function( workerClass ) {
+      var d = new Deferred();
+      var thisB = this;
+      var worker = new Worker( require.toUrl('JBrowse/Worker/boot_dedicated.js') );
+      var timeout;//  =setTimeout( function() {
+      //     d.reject( new Error( 'worker was not ready within '+thisB.getConf('workerStartTimeout')+' seconds' ) );
+      // }, thisB.getConf('workerStartTimeout')*1000 );
+      worker.onerror = function(e) { console.error(e); d.reject(e); };
+      worker.onmessage = function( event ) {
+          if( event.data == 'starting' ) {
+              // do nothing
+          }
+          else if( event.data == 'ready' ) {
+              if( timeout ) { clearTimeout( timeout ); timeout = undefined; }
+              d.resolve( new WorkerHandle({ worker: worker, authManager: thisB }) );
+          }
+          else {
+              d.reject( new Error( event ) );
+          }
+      };
+      worker.postMessage( JSON.stringify(
+                              { preload: [
+                                    '../../dojo/dojo.js'
+                                    , '../init.js'
+                                ],
+                                //delay: 3000,
+                                require: { baseUrl: '../..' },
+                                config: this.exportMergedConfig(),
+                                workerClass: workerClass || 'JBrowse/Worker/Dedicated'
+                              }));
+
+      return d.then(
+          undefined,
+          function(e) {
+              if( timeout ) { clearTimeout( timeout ); timeout = undefined; }
+              worker.terminate();
+              throw e;
+          });
   }
 
 });
