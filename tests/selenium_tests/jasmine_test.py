03f8dc44ff20bb1a58612cf3a2470494c12525df
Erik Derohanian <erik.derohanian@gmail.com>
2013-12-12 18:56:17 -0500

    Add test to jasmine tests. TODO: Something useful when they fail, instead of causing an error

---
+++ tests/selenium_tests/jasmine_test.py
@@ -0,0 +1,21 @@
+import unittest
+from jbrowse_selenium import JBrowseTest
+
+class JasmineTest( JBrowseTest ):
+
+    data_dir = 'tests/js_tests/index.html'
+    base_url = 'http://localhost/'
+    def setUp( self ):
+        super( JasmineTest, self ).setUp()
+
+    def test_jasmine( self ):
+        
+        self.assert_element(".duration")
+        self.assert_no_element(".alert")
+
+        self.assert_no_js_errors()
+        
+
+
+class Jasmine_Test( JasmineTest, unittest.TestCase ):
+    pass
