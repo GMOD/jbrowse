bbf29074b7fb3d4fb102ef4d8a40e5147c633f60
Robert Buels <rmb32@cornell.edu>
2013-12-12 22:33:21 -0500

    rewrite gbrowse conf adaptor to support more than 2 levels of nesting

--- src/JBrowse/ConfigAdaptor/conf.js
+++ src/JBrowse/ConfigAdaptor/conf.js
@@ -1,113 +1,56 @@
-// Code for parsing GBrowse config files
-// Output : a JS object
-
-// Based (now *extremely* loosely) on
-// https://github.com/shockie/node-iniparser -> released under the MIT license
-
-
 define([
            'dojo/_base/declare',
-           'JBrowse/Util',
+           'dojo/_base/lang',
+           'dojo/_base/array',
+
            'JBrowse/ConfigAdaptor/JB_json_v1'
-       ], function(
+       ],
+       function(
            declare,
-           Util,
+           lang,
+           array,
+
            JB_json
        ) {
-return declare('JBrowse.ConfigAdaptor.gbrowse', JB_json,
-    {
-        parse_conf: function(data){
-            this.regex = {
-                section:    /^\s*\[\s*([^\]\/]*)\s*\]\s*$/,
-                subsection: /^\s*\[\s*([^\]]*)\/([^\]]*)\s*\]\s*$/,
-                param:      /^(\w[\w\.\-\_\:\s]+)=\s*(.*?)\s*$/,
-                comment:    /^\s*(#|\/\/).*$/,
-                emptyLine:  /^\s*$/,
-                newLine:    /\r\n|\r|\n/,
-                code:       /^\s*function\s*\(/
-            };
-            this.value = {};
-            this.lines = data.split(this.regex.newLine);
-            this.section = null;
-            this.subsection = null;
-            var n = this.lines.length;
+return declare( [JB_json], {
 
+parse_conf: function( text, load_args ) {
+    var section, key, value;
+    var data = {};
 
-            //Process each line
-            for (var i = 0; i < n; i++) {
-                if (this.regex.comment.test(this.lines[i])) {                      //      #this is a comment
-                    // do nothing, since it's a comment.                           //      // this is also a comment
-                } else if (this.regex.param.test(this.lines[i])) {                 //      param = *
-                    i = this.param(i);
-                } else if (this.regex.subsection.test(this.lines[i])) {            //      [section/subsection]
-                    this.subSection(i);
-                } else if (this.regex.section.test(this.lines[i])){                //      [section]
-                    this.mainSection(i);
-                }
-            };
+    array.forEach( text.split("\n"), function( line ) {
+        line = line.replace(/#.+/,'');
+        var match;
 
-            return this.value; //Returns the JS object
-        },
-
-        //called for:  something = *
-        param: function(i){
-            var line = this.lines; var rx = this.regex;
-            var lineNum = i+1;
-
-            var match = this.lines[i].match(this.regex.param);  // Check for a value
-            match[2].trim();                                    // on the first
-            if (!isNaN(match[2].trim())) {                      // line, then check
-                match[2] = parseFloat(match[2]);                // the rest of the
-            }                                                   // block
-
-            var tmpStrVal = match[2] ? match[2] : '';
-            var divider = this.regex.code.test(line[lineNum]) ? "\n" : ' ';
-
-            // while (line is defined, and not a parameter, section, or subsection)
-            while (typeof line[lineNum] !== 'undefined' && (! ( rx.param.test(line[lineNum]) ||
-                    rx.section.test(line[lineNum]) || rx.subsection.test(line[lineNum])))){
-                //if (line is not a comment or empty)
-                if (! ( rx.comment.test(line[lineNum]) || rx.emptyLine.test(line[lineNum]) )) {
-                    if (tmpStrVal === ''){
-                        tmpStrVal += line[lineNum].trim();
-                    } else {
-                        tmpStrVal = tmpStrVal + divider + line[lineNum].trim();
-                    }
-                }
-                lineNum++;
-            }
-            var match = this.lines[i].match(this.regex.param);
-            if (this.subsection && this.section) {
-                this.value[this.section][this.subsection][match[1].trim()] = tmpStrVal;
-            } else if (this.section){
-                this.value[this.section][match[1].trim()] = tmpStrVal;
-            } else {
-                this.value[match[1].trim()] = tmpStrVal;
-            }
-            return lineNum - 1; //The line it should continue at.
-        },
-
-        //called for:  [foobar]
-        mainSection: function(i) {
-            var match = this.lines[i].match(this.regex.section);
-            this.section = match[1].trim();
-            if( this.section.toLowerCase() == 'general' )
-                this.section = null;
-            else
-                if( ! this.value[this.section] )
-                    this.value[this.section] = {};
-            this.subsection = null;
-        },
-
-        //called for:  [foo/bar]
-         subSection: function(i) {
-            var match = this.lines[i].match(this.regex.subsection);
-            this.section = match[1].trim();
-            this.subsection = match[2].trim();
-             if( ! this.value[this.section] )
-                 this.value[this.section] = {};
-             if( ! this.value[this.section][this.subsection] )
-                 this.value[this.section][this.subsection] = {};
+        // new section
+        if(( match = line.match( /^\s*\[([^\]]+)/ ))) { // new section
+            section = match[1].trim().split(/\s*[\/\.]\s*/);
+            if( section.length == 1 && section[0].toLowerCase() == 'general' )
+                section = [];
         }
-    });
+        // new value
+        else if(( match = line.match( value == undefined ? /^([^=]+)=(.*)/ : /^(\S[^=]+)=(.*)/ ))) {
+            key = match[1].trim();
+            value = match[2].trim();
+            if( /^[\+\-]?[\d\.,]+([eE][\-\+]?\d+)?$/.test(value) ) // parseFloat if it looks numeric
+                value = parseFloat( value.replace(/,/g,'') );
+
+            lang.setObject( section.concat(key).join('.'), value, data );
+        }
+        // add to existing value
+        else if( value !== undefined && (match = line.match( /^\s+(\S.+)/ ))) {
+            value += value.length ? ' '+match[1].trim() : match[1].trim();
+
+            lang.setObject( section.concat(key).join('.'), value, data );
+        }
+        // done with last value
+        else {
+            key = value = undefined;
+        }
+    },this);
+
+    return data;
+}
+
+});
 });
--- tests/js_tests/spec/GBrowseParser.spec.js
+++ tests/js_tests/spec/GBrowseParser.spec.js
@@ -7,7 +7,7 @@ require(['JBrowse/ConfigAdaptor/conf'], function ( gbrowseConfigAdaptor ) {
         // to test different kinds of input
         // config file => ./spec/GBrowseParseTestBasic.conf
 
-        it('should return a javascript object', function() {
+        it('should parse', function() {
             gbrowse.load({
                              config: { url: 'spec/GBrowseParseTestBasic.conf' }
                          })
@@ -15,44 +15,41 @@ require(['JBrowse/ConfigAdaptor/conf'], function ( gbrowseConfigAdaptor ) {
                              gbConfig = c;
                          },
                          function( error ) {
-                             console.error(error);
+                             console.error( error.stack || ''+error );
                          });
 
             waitsFor( function() { return gbConfig; } );
             runs(function() {
                 expect( typeof gbConfig).toEqual('object');
-            });
-        });
+                expect(gbConfig['TRACK DEFAULTS']).toBeTruthy();
+                expect(gbConfig['TRACK DEFAULTS']['glyph'           ]).toBe('generic');
+                expect(gbConfig['TRACK DEFAULTS']['height'          ]).toBe(8);
+                expect(gbConfig['TRACK DEFAULTS']['bgcolor'         ]).toBe('cyan');
+                expect(gbConfig['TRACK DEFAULTS']['fgcolor'         ]).toBe('black');
+                expect(gbConfig['TRACK DEFAULTS']['label density'   ]).toBe(25);
+                expect(gbConfig['TRACK DEFAULTS']['bump density'    ]).toBe(25);
 
-        it ("should return correct values", function(){
+                expect(gbConfig['Markers:region']).toBeTruthy();
+                expect(gbConfig['Markers:region']['feature'         ]).toBe('match:ITAG_sgn_markers');
+                expect(gbConfig['Markers:region']['ignore_sub_part' ]).toBe('match_part');
+                expect(gbConfig['Markers:region']['key'             ]).toBe('Markers');
+                expect(gbConfig['Markers:region']['fgcolor'         ]).toBe('black');
+                expect(gbConfig['Markers:region']['bgcolor'         ]).toBe('yellow');
+                expect(gbConfig['Markers:region']['glyph'           ]).toBe('generic');
+                expect(gbConfig['Markers:region']['label density'   ]).toBe(100);
+                expect(gbConfig['Markers:region']['bump density'    ]).toBe(100);
+                expect(gbConfig['Markers:region']['link'            ]).toBe('/search/quick?term=$name');
+                expect(gbConfig['Markers:region']['citation'        ]).toBe('GenomeThreader alignments of SGN marker sequences.');
 
-            expect(gbConfig['TRACK DEFAULTS']).toBeTruthy();
-            expect(gbConfig['TRACK DEFAULTS']['glyph'           ]).toBe('generic');
-            expect(gbConfig['TRACK DEFAULTS']['height'          ]).toBe(8);
-            expect(gbConfig['TRACK DEFAULTS']['bgcolor'         ]).toBe('cyan');
-            expect(gbConfig['TRACK DEFAULTS']['fgcolor'         ]).toBe('black');
-            expect(gbConfig['TRACK DEFAULTS']['label density'   ]).toBe(25);
-            expect(gbConfig['TRACK DEFAULTS']['bump density'    ]).toBe(25);
+                expect(gbConfig['DM_BAC']).toBeTruthy();
+                expect(gbConfig['DM_BAC']['feature'  ]).toBe('\"BAC_clone:DBolser_Dundee_BES_SSAHA_dm_bes_sg\" \"BAC_clone:DBolser_Dundee_BES_SSAHA_dm_bes_sb\" \"BAC_clone:DBolser_Dundee_BES_SSAHA_dm_bes_lg\" \"BAC_clone:DBolser_Dundee_BES_SSAHA_dm_bes_lb\" \"BAC_clone:DBolser_Dundee_BES_SSAHA_dm_bes_hg\" \"BAC_clone:DBolser_Dundee_BES_SSAHA_dm_bes_hb\"');
+                expect(gbConfig['DM_BAC']['citation' ]).toBe('BAC ends from the \'POTATO-B-01-100-110KB\' library aligned using SSAHA2. The clone ends pair correctly (solid connector) or not (dashed connector), and are color coded by insert-size.  Provided by Dan Bolser, <a href="http://www.compbio.dundee.ac.uk/">University of Dundee</a>.');
 
-            expect(gbConfig['Markers:region']).toBeTruthy();
-            expect(gbConfig['Markers:region']['feature'         ]).toBe('match:ITAG_sgn_markers');
-            expect(gbConfig['Markers:region']['ignore_sub_part' ]).toBe('match_part');
-            expect(gbConfig['Markers:region']['key'             ]).toBe('Markers');
-            expect(gbConfig['Markers:region']['fgcolor'         ]).toBe('black');
-            expect(gbConfig['Markers:region']['bgcolor'         ]).toBe('yellow');
-            expect(gbConfig['Markers:region']['glyph'           ]).toBe('generic');
-            expect(gbConfig['Markers:region']['label density'   ]).toBe(100);
-            expect(gbConfig['Markers:region']['bump density'    ]).toBe(100);
-            expect(gbConfig['Markers:region']['link'            ]).toBe('/search/quick?term=$name');
-            expect(gbConfig['Markers:region']['citation'        ]).toBe('GenomeThreader alignments of SGN marker sequences.');
-
-            expect(gbConfig['DM_BAC']).toBeTruthy();
-            expect(gbConfig['DM_BAC']['feature'  ]).toBe('\"BAC_clone:DBolser_Dundee_BES_SSAHA_dm_bes_sg\" \"BAC_clone:DBolser_Dundee_BES_SSAHA_dm_bes_sb\" \"BAC_clone:DBolser_Dundee_BES_SSAHA_dm_bes_lg\" \"BAC_clone:DBolser_Dundee_BES_SSAHA_dm_bes_lb\" \"BAC_clone:DBolser_Dundee_BES_SSAHA_dm_bes_hg\" \"BAC_clone:DBolser_Dundee_BES_SSAHA_dm_bes_hb\"');
-            expect(gbConfig['DM_BAC']['citation' ]).toBe('BAC ends from the \'POTATO-B-01-100-110KB\' library aligned using SSAHA2. The clone ends pair correctly (solid connector) or not (dashed connector), and are color coded by insert-size.  Provided by Dan Bolser, <a href="http://www.compbio.dundee.ac.uk/">University of Dundee</a>.');
-
-            expect(gbConfig['sub']['section']['giant word']).toBe('Antidisestablishmentarianism');
-            expect(gbConfig['sub']['section']['multiline']).toBe('herp derp');
-            expect(gbConfig['sub']['section']['fancyMultiline']).toBe('Le herp derp');
+                expect(gbConfig['sub']['section']['giant word']).toBe('Antidisestablishmentarianism');
+                expect(gbConfig['sub']['section']['multiline']).toBe('herp derp');
+                expect(gbConfig['sub']['section']['fancyMultiline']).toBe('Le herp derp');
+                expect(gbConfig.threshold).toBe(-1234221e-02);
         });
     });
+});
 });
