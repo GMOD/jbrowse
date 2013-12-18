b8b72c3f88c3b08c80a9dd831ee0fadca20c8fc7
Robert Buels <rmb32@cornell.edu>
2013-12-18 14:54:32 -0500

    make REST store not inherit from Hash, and make it use ?startswith for wildcard queries and equals for exact queries

--- src/JBrowse/Store/Names/REST.js
+++ src/JBrowse/Store/Names/REST.js
@@ -2,47 +2,67 @@ define( [
             'dojo/_base/declare',
             'dojo/_base/array',
             'dojo/store/util/QueryResults',
-            'JBrowse/Store/Hash',
-            'JBrowse/Model/Location',
-            'dojo/request/xhr'
+            'dojo/request/xhr',
+            'dojo/io-query',
+
+            'JBrowse/Store/Hash'
         ],
         function(
             declare,
             array,
             QueryResults,
-            HashStore,
-            Location,
-            xhr
+            xhr,
+            ioQuery,
+
+            HashStore
         ) {
 
-return declare( HashStore,
+return declare( null,
 {
     constructor: function( args ) {
-        var data;
+        this.url = args.url;
     },
 
     query: function( query, options ) {
         var thisB = this;
-        return xhr((thisB.browser.config.names.url+"?starts="+query.name), {
-            handleAs: "json",
-        }).then(function(data){
+
+        var op = 'equals';
+        var name = ''+query.name;
+        if( /\*$/.test( name ) ) {
+            name = name.replace(/\*$/,'');
+            op = 'startswith';
+        }
+        var myquery = {};
+        myquery[op] = name;
+
+        return xhr( thisB.url+"?"+ioQuery.objectToQuery( myquery ),
+                    { handleAs: "json" }
+        ).then(function(data){
             for (var i = 0; i < data.length; i++){
                 var dat = data[i];
-                dat.label = dat.name + (dat.location ? 
-                    '<span class="locString">'+dat.location.ref+":"+dat.location.start+".."+dat.location.end+"("+dat.name+")</span>"
-                    : "");
+                dat.label = dat.name + (
+                    dat.location ? ( '<span class="locString">'
+                                     +dat.location.ref+":"+dat.location.start+".."+dat.location.end
+                                     +" ("+dat.name+")"
+                                     + "</span>"
+                                    )
+                                 : ""
+                );
             }
             return QueryResults( data );
         }, function(err){
             // Handle the error condition
-        }, function(evt){
-            // Handle a progress event from the request if the
-            // browser supports XHR2
+            return QueryResults( [] );
         });
     },
 
     get: function( id ) {
         return this.query(id, undefined);
+    },
+
+    getIdentity: function( object ) {
+        return object.id;
     }
+
 });
 });
