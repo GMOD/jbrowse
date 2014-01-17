1b18af35a55824f4e48e756d89477b3224ff864b
Kevin-Mohamed <kevmoha@hotmail.com>
2014-01-17 09:46:35 -0500

    removed old renderBox

--- src/JBrowse/View/FeatureGlyph/Diamond.js
+++ src/JBrowse/View/FeatureGlyph/Diamond.js
@@ -198,61 +198,7 @@ return declare([ FeatureGlyph, FeatureLabelMixin ], {
         this.renderArrowhead( context, fRect );
     },
 
-    // top and height are in px
-    renderBox: function( context, viewInfo, feature, top, overallHeight, parentFeature, style ) {
-        var left  = viewInfo.block.bpToX( feature.get('start') );
-        
-        var width = viewInfo.block.bpToX( feature.get('end') ) - left;
-        //left = Math.round( left );
-        //width = Math.round( width );
-
-        style = style || lang.hitch( this, 'getStyle' );
-
-        var height = this._getFeatureHeight( viewInfo, feature );
-        if( ! height )
-            return;
-        if( height != overallHeight )
-            top += Math.round( (overallHeight - height)/2 );
-
-        // background
-        var bgcolor = style( feature, 'color' );
-        if( bgcolor ) {
-            context.fillStyle = bgcolor;
-            context.fillRect( left, top, Math.max(1,width), height );
-        }
-        else {
-            context.clearRect( left, top, Math.max(1,width), height );
-        }
-        
-        //rotate
-        var rAngle = style( feature, 'rotation');
-        if (rAngle) {
-			context.rotateRect(Math.PI*rAngle/180);
-			context.fillRect( left, top, Math.max(1,width), height );
-		}
-
-        // foreground border
-        var borderColor, lineWidth;
-        if( (borderColor = style( feature, 'borderColor' )) && ( lineWidth = style( feature, 'borderWidth')) ) {
-            if( width > 3 ) {
-                context.lineWidth = lineWidth;
-                context.strokeStyle = borderColor;
-
-                // need to stroke a smaller rectangle to remain within
-                // the bounds of the feature's overall height and
-                // width, because of the way stroking is done in
-                // canvas.  thus the +0.5 and -1 business.
-                context.strokeRect( left+lineWidth/2, top+lineWidth/2, width-lineWidth, height-lineWidth );
-            }
-            else {
-                context.globalAlpha = lineWidth*2/width;
-                context.fillStyle = borderColor;
-                context.fillRect( left, top, Math.max(1,width), height );
-                context.globalAlpha = 1;
-            }
-        }
-    },
-    
+    // top and height are in px    
     renderBox: function( context, viewInfo, feature, top, overallHeight, parentFeature, style ) {
 		
         var left  = viewInfo.block.bpToX( feature.get('start') );
