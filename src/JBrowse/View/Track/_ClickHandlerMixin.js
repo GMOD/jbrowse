27c3508b6dafcebde9a8551ecf6d9e555d1061d1
Robert Buels <rmb32@cornell.edu>
2014-01-30 17:51:19 -0800

    bugfix to Glyph track event handler args

--- src/JBrowse/View/Track/_ClickHandlerMixin.js
+++ src/JBrowse/View/Track/_ClickHandlerMixin.js
@@ -133,7 +133,7 @@ return declare( null, {
                     track._openDialog( spec, evt, ctx );
             }
             else if( typeof spec.action == 'function' ) {
-                spec.action.apply( ctx, ctx.callbackArgs ? [evt].concat(ctx.callbackArgs) : arguments );
+                spec.action.apply( ctx, ctx.callbackArgs || arguments );
             }
             else {
                 return;
