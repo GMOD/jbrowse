40d5b39d884f06f2da4b830348743e860ba8895e
rdhayes <rdhayes@lbl.gov>
2014-02-03 17:23:16 -0800

    removed the second regex looking for the closing brace for a functional callback declaration

--- src/JBrowse/ConfigAdaptor/AdaptorUtil.js
+++ src/JBrowse/ConfigAdaptor/AdaptorUtil.js
@@ -12,7 +12,7 @@ define( [ 'dojox/lang/functional/object',
                 else if( typeof conf[x] == 'string' ) {
                     // compile
                     var spec = conf[x];
-                    if( /^\s*function\s*\(/.test(spec) && /\}[\s;]*$/.test(spec) ) {
+                    if( /^\s*function\s*\(/.test(spec) ) {
                         conf[x] = this.evalHook(spec);
                     }
                 }
