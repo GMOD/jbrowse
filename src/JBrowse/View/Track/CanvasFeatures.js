4e6930a0dc85ae4ebc8fcb9032b39a1aaedfa6e2
Colin <colin.diesh@gmail.com>
2015-07-27 13:27:18 -0500

    Fix loading the histogram from flatfile-to-json generated tracks. See #612

--- src/JBrowse/View/Track/CanvasFeatures.js
+++ src/JBrowse/View/Track/CanvasFeatures.js
@@ -362,7 +362,7 @@ return declare(
             basesPerBin: basesPerBin
         };
 
-        if( !this.config.histograms&&this.store.getRegionFeatureDensities ) {
+        if( !this.config.histograms.store&&this.store.getRegionFeatureDensities ) {
             this.store.getRegionFeatureDensities(
                 query,
                 lang.hitch( this, '_drawHistograms', args )
