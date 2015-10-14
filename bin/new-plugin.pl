2396b48a868a22d1d097651982e17515178b0f22
Colin <colin.diesh@gmail.com>
2015-10-14 16:37:48 -0500

    Generate a dojo build profile from the bin/new-plugin.pl

--- bin/new-plugin.pl
+++ bin/new-plugin.pl
@@ -62,6 +62,78 @@ EOF
 print STDERR "wrote $main_js\n";
 }
 
+# make a dojo build profile
+{
+    my $profile_js = catfile( $plugin_dir, 'js', $plugin_name.'.profile.js' );
+    open my $profile, '>', $profile_js or die "$! writing $profile_js\n";
+    print $profile <<EOF;
+function copyOnly(mid) {
+    return mid in {
+        // There are no modules right now that are copy-only. If you have some, though, just add
+        // them here like this:
+        // 'app/module': 1
+    };
+}
+
+var profile = {
+    action: 'release',
+    cssOptimize: 'comments',
+    mini: true,
+
+    basePath: '../../../src',
+    packages: [
+        {name: '$plugin_name', location: '../plugins/$plugin_name/js' }
+    ],
+
+    layerOptimize: 'closure',
+    stripConsole: 'normal',
+    selectorEngine: 'acme',
+
+    layers: {
+        '$plugin_name/main': {
+            include: [
+                '$plugin_name',
+            ],
+            exclude: [ 'JBrowse' ]
+        }
+    },
+
+    staticHasFeatures: {
+        'dojo-trace-api':0,
+        'dojo-log-api':0,
+        'dojo-publish-privates':0,
+        'dojo-sync-loader':0,
+        'dojo-xhr-factory':0,
+        'dojo-test-sniff':0
+    },
+
+    resourceTags: {
+        // Files that contain test code.
+        test: function (filename, mid) {
+            return false;
+        },
+
+        // Files that should be copied as-is without being modified by the build system.
+        copyOnly: function (filename, mid) {
+            return copyOnly(mid);
+        },
+
+        // Files that are AMD modules.
+        amd: function (filename, mid) {
+            return !copyOnly(mid) && /\.js\$/.test(filename);
+        },
+
+        // Files that should not be copied when the “mini” compiler flag is set to true.
+        miniExclude: function (filename, mid) {
+            return ! ( /^$plugin_name/.test(mid) );
+        }
+    }
+};
+EOF
+print STDERR "wrote $profile_js\n";
+
+}
+
 { # put a blank main.css in
     my $main_css = catfile( $plugin_dir, 'css', 'main.css' );
     open my $f, '>', $main_css or die "$! writing $main_css\n";
