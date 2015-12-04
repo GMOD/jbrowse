530736b932d75daddb7b5fa7c732ac8a35de3e73
Colin <colin.diesh@gmail.com>
2015-12-04 16:10:00 -0600

    Allow copy and paste in electron app

--- browser/menu.js
+++ browser/menu.js
@@ -40,3 +40,44 @@ menu.push(
         ]
     }
 );
+
+menu.push(
+  {
+    label: 'Edit',
+    submenu: [
+      {
+        label: 'Undo',
+        accelerator: 'Command+Z',
+        selector: 'undo:'
+      },
+      {
+        label: 'Redo',
+        accelerator: 'Shift+Command+Z',
+        selector: 'redo:'
+      },
+      {
+        type: 'separator'
+      },
+      {
+        label: 'Cut',
+        accelerator: 'Command+X',
+        selector: 'cut:'
+      },
+      {
+        label: 'Copy',
+        accelerator: 'Command+C',
+        selector: 'copy:'
+      },
+      {
+        label: 'Paste',
+        accelerator: 'Command+V',
+        selector: 'paste:'
+      },
+      {
+        label: 'Select All',
+        accelerator: 'Command+A',
+        selector: 'selectAll:'
+      },
+    ]
+  }
+);
