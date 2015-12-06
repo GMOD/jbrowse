d05af36d8506b8618bad97023330ecdcc077bb1a
Colin <colin.diesh@gmail.com>
2015-12-06 00:36:43 -0600

    Fix require style on HideTrackLabels

--- plugins/HideTrackLabels/js/main.js
+++ plugins/HideTrackLabels/js/main.js
@@ -1,7 +1,7 @@
 /*
 HideTrackLabels JBrowse plugin CSS
 */
-/* 
+/*
     Created on : Jul 15, 2015, 7:19:50 PM
     Author     : EY
 */
@@ -12,6 +12,12 @@ define([
             'dojo/Deferred',
             'dojo/dom-construct',
             'dijit/form/Button',
+            'dojo/fx',
+            'dojo/dom',
+            'dojo/dom-style',
+            'dojo/on',
+            'dojo/query',
+            'dojo/dom-geometry',
             'JBrowse/Plugin'
        ],
        function(
@@ -20,15 +26,20 @@ define([
            Deferred,
            domConstruct,
            dijitButton,
+           coreFx,
+           dom,
+           style,
+           on,
+           query,
+           domGeom,
            JBrowsePlugin
        ) {
 return declare( JBrowsePlugin,
-{    
+{
     constructor: function( args ) {
         console.log("plugin HideTracksButton constructor");
 
         var baseUrl = this._defaultConfig().baseUrl;
-
         var thisB = this;
 
         // create the hide/show button after genome view initialization
@@ -38,7 +49,6 @@ return declare( JBrowsePlugin,
 
             thisB.browser.hideTitlesButton = new dijitButton(
             {
-                //label: 'H',
                 title: "Hide/Show Track Titles",
                 id: "hidetitles-btn",
                 width: "24px",
@@ -48,76 +58,66 @@ return declare( JBrowsePlugin,
                 })
             }, dojo.create('button',{},navBox));   //thisB.browser.navBox));
         });
-        
+
         /* show or hide track labels
          * showTrackLabels(param)
          * @param {string} function "show", "hide", "toggle", or "hide-if"
          * "hide-if" rehides if already hidden.
          * @returns {undefined}
          */
-
         this.browser.showTrackLabels = function(fn) {
-            
+
             // does the hide/show button exists yet?
             if (dojo.byId('hidetitles-btn')==null) return;
-            
-            require(["dojo/fx", "dojo/dom", "dojo/dom-style", "dojo/on", "dojo/query","dojo/dom-geometry","dojo/NodeList-dom","dojo/domReady!"],
-            function(coreFx, dom, style, on, query, domGeom){
 
-                var direction = 1;
 
-                if (fn=="show") {
+            var direction = 1;
+
+            if (fn=="show") {
+                dojo.removeAttr(dom.byId("hidetitles-btn"),"hidden-titles");
+                direction = 1;
+            }
+            if (fn=="hide") {
+                dojo.attr(dom.byId("hidetitles-btn"),"hidden-titles","");
+                direction = -1;
+            }
+            if (fn=="hide-if") {
+                if (dojo.hasAttr(dom.byId("hidetitles-btn"),"hidden-titles")) direction = -1;
+                else return;
+            }
+
+            if (fn=="toggle"){
+                if (dojo.hasAttr(dom.byId("hidetitles-btn"),"hidden-titles")) {     // if hidden, show
                     dojo.removeAttr(dom.byId("hidetitles-btn"),"hidden-titles");
                     direction = 1;
                 }
-                if (fn=="hide") {
-                    dojo.attr(dom.byId("hidetitles-btn"),"hidden-titles","");
+                else {
+                    dojo.attr(dom.byId("hidetitles-btn"),"hidden-titles","");       // if shown, hide
                     direction = -1;
                 }
-                if (fn=="hide-if") {
-                    if (dojo.hasAttr(dom.byId("hidetitles-btn"),"hidden-titles")) direction = -1;
-                    else return;
-                }
+            }
+            // protect Hide button from clicks durng animation
+            dojo.attr(dom.byId("hidetitles-btn"),"disabled","");
+            setTimeout(function(){
+                dojo.removeAttr(dom.byId("hidetitles-btn"),"disabled");
+            },1000);
 
-                if (fn=="toggle"){
-                    if (dojo.hasAttr(dom.byId("hidetitles-btn"),"hidden-titles")) {     // if hidden, show
-                        dojo.removeAttr(dom.byId("hidetitles-btn"),"hidden-titles");
-                        direction = 1;
-                    }
-                    else {
-                        dojo.attr(dom.byId("hidetitles-btn"),"hidden-titles","");       // if shown, hide
-                        direction = -1;
-                    }
-                }
-                // protect Hide button from clicks durng animation
-                dojo.attr(dom.byId("hidetitles-btn"),"disabled","");
-                setTimeout(function(){ 
-                    dojo.removeAttr(dom.byId("hidetitles-btn"),"disabled");
-                },1000);
-
-                // slide em
-                query(".track-label").forEach(function(node, index, arr){
-                    var w = domGeom.getMarginBox(node).w;
-                    coreFx.slideTo({
-                      node: node,
-                      top: domGeom.getMarginBox(node).t.toString(),
-                      left: (domGeom.getMarginBox(node).l + (w*direction) ).toString(),
-                      unit: "px"
-                    }).play();
-                });
+            // slide em
+            query(".track-label").forEach(function(node, index, arr){
+                var w = domGeom.getMarginBox(node).w;
+                coreFx.slideTo({
+                  node: node,
+                  top: domGeom.getMarginBox(node).t.toString(),
+                  left: (domGeom.getMarginBox(node).l + (w*direction) ).toString(),
+                  unit: "px"
+                }).play();
             });
         };
-        
         // trap the redraw event for handling resize, scroll and zoom events
         dojo.subscribe("/jbrowse/v1/n/tracks/redraw", function(data){
             // hide track labels if necessary
-            //setTimeout(function(){ 
-                thisB.browser.showTrackLabels("hide-if");
-            //}, 1000); // delay hiding for 1 seconda - not working -ey  
-
-        });        
-        
-
+            thisB.browser.showTrackLabels("hide-if");
+        });
     }
 
 });
