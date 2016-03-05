9c8882ecbd1ad0ac0431acbec8a206cd5dcd385d
Ian Holmes <ihh@berkeley.edu>
2016-03-04 18:07:47 -0800

    Added [waiting on stdin] message to add-track-json.js

--- node/add-track-json.js
+++ node/add-track-json.js
@@ -28,97 +28,103 @@ fs.readFile (trackListPath, function (err, trackListData) {
     }
     var trackListJson = err ? {} : JSON.parse(trackListData)
     trackListJson.tracks = trackListJson.tracks || []
+
+    var timeout = setTimeout (function() {
+	if (newTrackPath == '/dev/stdin')
+	    console.log ("[waiting for new track on stdin]")
+    }, 500)
+    fs.readFile (newTrackPath, function (err, newTrackData) {
+	clearTimeout (timeout)
+	if (err) throw err
     
-    var newTrackData = fs.readFileSync (newTrackPath)
-    var newTrackJson = JSON.parse(newTrackData)
+	var newTrackJson = JSON.parse(newTrackData)
 
-    // if it's a single definition, coerce to an array
-    if (Object.prototype.toString.call(newTrackJson) != '[object Array]') {
-	newTrackJson = [ newTrackJson ]
-    }
-
-    // validate the new track JSON structures
-    newTrackJson.forEach (function (track) {
-	if (!track.label) {
-	    console.log ("Invalid track JSON: missing a label element")
-	    process.exit (1)
+	// if it's a single definition, coerce to an array
+	if (Object.prototype.toString.call(newTrackJson) != '[object Array]') {
+	    newTrackJson = [ newTrackJson ]
 	}
-    })
-    
-    // insert/replace the tracks
-    var addedTracks = [], replacedTracks = []
-    newTrackJson.forEach (function (newTrack) {
-	var newTracks = []
-	trackListJson.tracks.forEach (function (oldTrack) {
-	    if (oldTrack.label == newTrack.label) {
-		newTracks.push (newTrack)
-		replacedTracks.push (newTrack)
-		newTrack = {}
-	    } else {
-		newTracks.push (oldTrack)
+
+	// validate the new track JSON structures
+	newTrackJson.forEach (function (track) {
+	    if (!track.label) {
+		console.log ("Invalid track JSON: missing a label element")
+		process.exit (1)
 	    }
 	})
-	if (newTrack.label) {
-	    newTracks.push (newTrack)
-	    addedTracks.push (newTrack)
+	
+	// insert/replace the tracks
+	var addedTracks = [], replacedTracks = []
+	newTrackJson.forEach (function (newTrack) {
+	    var newTracks = []
+	    trackListJson.tracks.forEach (function (oldTrack) {
+		if (oldTrack.label == newTrack.label) {
+		    newTracks.push (newTrack)
+		    replacedTracks.push (newTrack)
+		    newTrack = {}
+		} else {
+		    newTracks.push (oldTrack)
+		}
+	    })
+	    if (newTrack.label) {
+		newTracks.push (newTrack)
+		addedTracks.push (newTrack)
+	    }
+	    trackListJson.tracks = newTracks
+	})
+
+	// write the new track list
+	var trackListOutputData = JSON.stringify (trackListJson, null, 2)
+	if (opt.options.stdout) {
+	    process.stdout.write (trackListOutputData + "\n")
+	} else {
+	    fs.writeFileSync (trackListPath, trackListOutputData)
 	}
-	trackListJson.tracks = newTracks
-    })
 
-    // write the new track list
-    var trackListOutputData = JSON.stringify (trackListJson, null, 2)
-    if (opt.options.stdout) {
-	process.stdout.write (trackListOutputData + "\n")
-    } else {
-	fs.writeFileSync (trackListPath, trackListOutputData)
-    }
+	// publish notifications
+	var publishUrl = opt.options['notify']
+	if (publishUrl) {
+	    var client = new faye.Client (publishUrl)
+	    var secret = opt.options['secret']
+	    if (secret)
+		client.addExtension({
+		    outgoing: function(message, callback) {
+			message.ext = message.ext || {};
+			message.ext.password = secret;
+			callback(message);
+		    }
+		});
 
-    // publish notifications
-    var publishUrl = opt.options['notify']
-    if (publishUrl) {
-	var client = new faye.Client (publishUrl)
-	var secret = opt.options['secret']
-	if (secret)
-	    client.addExtension({
-		outgoing: function(message, callback) {
-		    message.ext = message.ext || {};
-		    message.ext.password = secret;
-		    callback(message);
-		}
-	    });
+	    if (logging)
+		client.addExtension({
+		    outgoing: function(message, callback) {
+			console.log ('client outgoing', message);
+			callback(message);
+		    }
+		});
 
-	if (logging)
-	    client.addExtension({
-		outgoing: function(message, callback) {
-		    console.log ('client outgoing', message);
-		    callback(message);
-		}
-	    });
-
-	deferred.map (addedTracks, function (track) {
-	    return client.publish ("/tracks/new", track)
-		.then (function() {
-		    console.log ("Announced new track " + track.label)
-		}, function() {
-		    console.log ("Failed to announce new track " + track.label)
-		    process.exit()
-		})
-	}) (function() {
-	    deferred.map (replacedTracks, function (track) {
-		return client.publish ("/tracks/replace", track)
+	    deferred.map (addedTracks, function (track) {
+		return client.publish ("/tracks/new", track)
 		    .then (function() {
-			console.log ("Announced replacement track " + track.label)
+			console.log ("Announced new track " + track.label)
 		    }, function() {
-			console.log ("Failed to announce replacement track " + track.label)
+			console.log ("Failed to announce new track " + track.label)
 			process.exit()
 		    })
 	    }) (function() {
-		process.exit()
+		deferred.map (replacedTracks, function (track) {
+		    return client.publish ("/tracks/replace", track)
+			.then (function() {
+			    console.log ("Announced replacement track " + track.label)
+			}, function() {
+			    console.log ("Failed to announce replacement track " + track.label)
+			    process.exit()
+			})
+		}) (function() {
+		    process.exit()
+		})
 	    })
-	})
-
-    } else {
-	process.exit()
-    }
-    
+	} else {
+	    process.exit()
+	}
+    })
 })
