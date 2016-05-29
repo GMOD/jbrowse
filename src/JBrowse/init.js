16dcbc675929bfc919c4d3dfd79194ce66f54258
Colin <colin.diesh@gmail.com>
2016-05-29 12:09:47 -0500

    Fixup tests

--- src/JBrowse/init.js
+++ src/JBrowse/init.js
@@ -10,8 +10,8 @@ require( {
                { name: 'lazyload', location: 'node_modules/lazyload', main: 'lazyload' },
                { name: 'xstyle', location: 'node_modules/xstyle' },
                { name: 'put-selector', location: 'node_modules/put-selector' },
-               { name: 'jDataView', location: 'node_modules/jDataView/src', main: 'jdataview' },
-               { name: 'FileSaver', location: 'node_modules/FileSaver' },
+               { name: 'jDataView', location: 'node_modules/jdataview/dist/node/', main: 'jdataview' },
+               { name: 'FileSaver', location: 'node_modules/filesaver.js', main: 'FileSaver' },
                { name: 'JBrowse', location: 'src/JBrowse' }
            ]
          }
