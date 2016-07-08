7c572326627cb08593b87038583eb7edfd339633
Colin <colin.diesh@gmail.com>
2016-07-08 15:29:58 -0500

    Add basic matches display

--- src/JBrowse/Store/SeqFeature/_MismatchesMixin.js
+++ src/JBrowse/Store/SeqFeature/_MismatchesMixin.js
@@ -178,6 +178,7 @@ return declare( null, {
                                                 1
                                               )
                                   : 'X';
+                  curr.altbase = token;
                   nextRecord();
               }
           }
