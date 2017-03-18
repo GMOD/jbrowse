caad4639bd1aa1d15196a2cf366ed9641d4f787a
Colin <colin.diesh@gmail.com>
2017-03-17 22:32:47 -0400

    Expect bai to be .bam.bai for file dialog

--- tests/js_tests/spec/AddFiles.spec.js
+++ tests/js_tests/spec/AddFiles.spec.js
@@ -24,20 +24,6 @@ describe( 'FileDialog BAM driver', function() {
                       expect( confs.foo.bam.blob.name ).toEqual( 'zee.bam' );
                   });
 
-              it( 'matches a BAI file like zee.bai -> zee.bam', function( ) {
-                      var confs = { foo: { urlTemplate: '/zee.bam' } };
-                      expect( (new BAMDriver()).tryResource( confs, { type: 'bai', file: { name: 'zee.bai'} } ) )
-                          .toBeTruthy();
-                      expect( confs.foo.bai.blob.name ).toEqual( 'zee.bai' );
-                  });
-
-              it( 'matches a BAM file like zee.bam -> zee.bai', function( ) {
-                      var confs = { foo: { baiUrlTemplate: '/zee.bai' } };
-                      expect( (new BAMDriver()).tryResource( confs, { type: 'bam', file: { name: 'zee.bam'} } ) )
-                          .toBeTruthy();
-                      expect( confs.foo.bam.blob.name ).toEqual( 'zee.bam' );
-                  });
-
 
           });
 
