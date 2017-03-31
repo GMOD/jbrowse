eaec8fb08ffd0022d188c960f6e7f339c310d4f1
Colin <colin.diesh@gmail.com>
2017-03-31 16:15:02 -0400

    Update postinstall script

--- postinstall.sh
+++ postinstall.sh
@@ -1,3 +1,12 @@
-for i in dojo dijit dojox util xstyle put-selector FileSaver jszlib json-schema jDataView lazyload dbind dgrid; do
+#!/usr/bin/env bash
+
+for i in dojo dijit dojox xstyle put-selector jszlib json-schema lazyload dbind dgrid; do
     cp -R node_modules/$i src/$i;
 done;
+
+cp -R node_modules/dojo-util src/util
+cp -R node_modules/filesaver.js src/FileSaver
+cp -R node_modules/jdataview src/jDataView
+
+
+
