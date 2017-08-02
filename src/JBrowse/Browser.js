6af3d6881eb61d31e67869508f11e8003e7d6540
Colin <colin.diesh@gmail.com>
2017-08-02 09:55:02 -0400

    Create jbrowse error screen when resource fails to load

--- src/JBrowse/Browser.js
+++ src/JBrowse/Browser.js
@@ -316,6 +316,11 @@ initPlugins: function() {
             return new Deferred();
         });
 
+        require.on("error", function(error) {
+            console.error(error);
+            deferred.reject('A resource failed to load '+error.src + ':' + error.info[0]);
+        });
+
         // fire the "all plugins done" deferred when all of the plugins are done loading
         (new DeferredList( pluginDeferreds ))
             .then( function() { deferred.resolve({success: true}); });
