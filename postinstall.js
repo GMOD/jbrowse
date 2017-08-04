29a023070de325e8a7fd03c9f14fbde942ecb346
enuggetry <github@meyao.net>
2017-08-03 19:52:36 -0700

    support npm install jbrowse as a module

---
+++ postinstall.js
@@ -0,0 +1,36 @@
+var appPath = require("app-root-path").path;
+var modPath = ""+require("shelljs").pwd();
+var fs = require("fs-extra");
+
+console.log(">>> postinstall");
+console.log("appPath",appPath,"modPath",modPath);
+
+copyMod('dojo','dojo');
+copyMod('dijit','dijit');
+copyMod('dojox','dojox');
+copyMod('jszlib','jszlib');
+copyMod('json-schema','json-schema');
+copyMod('lazyload','lazyload');
+copyMod('dgrid','dgrid');
+copyMod('jDataView','jDataView');
+copyMod('dojo-util','util');
+copyMod('filesaver.js','FileSaver');
+copyMod('dojo-dstore','dstore');
+
+
+function copyMod(from,to) {
+    var src = appPath+"/node_modules/"+from;
+    var trg = modPath+"/src/"+to;
+
+    // delete the old dir from src dir and make a current copy
+    fs.remove(trg)
+    .then(() => {
+        return fs.copy(src, trg);
+    })
+    .then(() => {
+        console.log("copied",trg);
+    })
+    .catch(err => {
+      console.error(err);
+    });
+}
