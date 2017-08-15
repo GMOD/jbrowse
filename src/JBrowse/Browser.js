f676ad34fff676b1b33505f043628d301cb31e17
rdhayes <rdhayes@lbl.gov>
2017-08-15 11:00:58 -0700

    address Travis build failure on mulit-line string

--- src/JBrowse/Browser.js
+++ src/JBrowse/Browser.js
@@ -1686,10 +1686,10 @@ _reportGoogleUsageStats: function( stats ) {
         });
     }
 
-    var analyticsScript = "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){ \
-            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o), \
-            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m) \
-            })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');";
+    var analyticsScript = "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){ ";
+    analyticsScript += "(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o), ";
+    analyticsScript += "m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m) ";
+    analyticsScript += "})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');";
 
     // set up users
     var trackerNum = 1;
