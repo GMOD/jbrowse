55a44f230abe9e39b2d1088b2b0ddc17d9f2146b
Colin <colin.diesh@gmail.com>
2017-12-20 17:44:14 -0500

    Add groupfeatures

--- src/JBrowse/Store/SeqFeature/BigBed.js
+++ src/JBrowse/Store/SeqFeature/BigBed.js
@@ -34,28 +34,33 @@ return declare(BigWig,
         }
 
         v.readWigData( chrName, min, max, dojo.hitch( this, function( features ) {
-            var genes = {};
-            array.forEach( features || [], function(feature) {
-                var id = feature.get('id');
-                if(!genes[id]) {
-                    genes[id] = new SimpleFeature({
-                        id: feature.get('id'),
-                        data: {
-                            start: feature.get('start'),
-                            end: feature.get('end'),
-                            type: 'gene',
-                            name: feature.get('name'),
+            if(this.config.groupFeatures) {
+                var genes = {};
+                array.forEach( features || [], function(feature) {
+                    var id = feature.get('id');
+                    if(!genes[id]) {
+                        genes[id] = new SimpleFeature({
                             id: feature.get('id'),
-                            subfeatures: []
-                        }
-                    });
-                }
-                feature.data.name = null;
-                genes[id].data.subfeatures.push(feature);
-            });
-            Object.keys(genes).forEach(function(name) {
-                featureCallback(genes[name]);
-            });
+                            data: {
+                                start: feature.get('start'),
+                                end: feature.get('end'),
+                                type: 'gene',
+                                name: feature.get('name'),
+                                id: feature.get('id'),
+                                subfeatures: []
+                            }
+                        });
+                    }
+                    feature.data.name = null;
+                    genes[id].data.subfeatures.push(feature);
+                });
+                Object.keys(genes).forEach(function(name) {
+                    featureCallback(genes[name]);
+                });
+            }
+            else {
+                array.forEach( features || [], featureCallback );
+            }
             endCallback();
         }), errorCallback );
     },
