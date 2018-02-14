226442fcfcbc8f20b0eca6200d7537cd59e46e12
Robert Buels <rbuels@gmail.com>
2018-02-14 15:29:20 -0800

    make release notes GH-flavored-markdown hack support GH usernames with hyphens in them

--- build/format_release_notes.pl
+++ build/format_release_notes.pl
@@ -8,5 +8,5 @@ local $/;
 my $html = markdown( scalar <> );
 $html =~ s!issue \#(\d+)!<a href="https://github.com/gmod/jbrowse/issues/$1">issue #$1</a>!g;
 $html =~ s!pull ( request)?\#(\d+)!<a href="https://github.com/gmod/jbrowse/pull/$2">issue #$2</a>!g;
-$html =~ s!(\W)\@(\w+)!$1<a href="https://github.com/$2">\@$2</a>!g;
+$html =~ s!(\W)\@([-\w]+)!$1<a href="https://github.com/$2">\@$2</a>!g;
 print $html;
