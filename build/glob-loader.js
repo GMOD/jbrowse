7401265c3ccb32b8857ebd003201fa40605847f3
Robert Buels <rbuels@gmail.com>
2018-03-02 12:35:37 -0800

    improve console output of glob-loader

--- build/glob-loader.js
+++ build/glob-loader.js
@@ -15,8 +15,6 @@ JBrowse/main
 `
 .trim().split(/\s+/).forEach(mid => blacklist[`src/${mid}.js`] = 1)
 
-console.log(blacklist)
-
 const format = 'AMD';
 
 // tiny Webpack Loader that replaces "//! webpackRequireGlob" expressions with a big bunch of requires
@@ -27,11 +25,14 @@ module.exports = function(content,map,meta) {
             globExpression = globExpression.trim().replace(/^['"]|["']$/g,'')
             let files = glob.sync('src/'+globExpression)
             return files.map( (filename,i) => {
-                let blacklisted = blacklist[filename] ? '//BLACKLISTED ' : '';
+                let blacklisted = '';
+                if (blacklist[filename]) {
+                    blacklisted = '//BLACKLISTED '
+                    console.log(`build/glob-loader: SKIPPING blacklisted module ${filename}`)
+                }
                 let code =
                     format === 'CommonJS' ? `${blacklisted}var __webpackRequireGlob${i} = require('${filename.replace('src/JBrowse/','./').replace('.js','')}') // ${filename}` :
                                             `${blacklisted} '${filename.replace('src/','').replace('.js','')}',`
-                console.log(code)
                 return code
             }).join('\n')
         }
