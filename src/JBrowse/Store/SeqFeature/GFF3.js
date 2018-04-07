d05c10154f34c10a55c9f96dfec681504d615933
Robert Buels <rbuels@gmail.com>
2018-04-06 18:17:29 -0700

    add toplevelfeatures/featuretransforms support to GFF3 store

--- src/JBrowse/Store/SeqFeature/GFF3.js
+++ src/JBrowse/Store/SeqFeature/GFF3.js
@@ -160,12 +160,14 @@ return declare([ SeqFeatureStore, DeferredFeatures, DeferredStats, GlobalStatsEs
                 break;
 
             if( checkEnd( f ) ) {
-                featureCallback( f );
+                this.applyFeatureTransforms([f]).forEach(featureCallback)
             }
         }
 
         finishCallback();
     },
+
+    supportsFeatureTransforms: true,
 
     _formatFeature: function( data ) {
         var f = new SimpleFeature({
