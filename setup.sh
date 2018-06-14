d05df9b34b6bca33de27d1bc1f315125af802413
Nathan Dunn <ndunn@me.com>
2018-06-13 17:38:42 -0700

    Revert "removed sample data installation"
    This reverts commit 2e9dd0db99bec0b785421d109c32730e14ebcc39.

--- setup.sh
+++ setup.sh
@@ -108,4 +108,68 @@ fi;
 ) >>setup.log 2>&1;
 done_message "" "As a first troubleshooting step, make sure development libraries and header files for GD, Zlib, and libpng are installed and try again.";
 
-echo "Download sample data from https://genomearchitect.readthedocs.io/en/latest/Apollo2Build.html#adding-sample-data"
+echo
+echo -n "Formatting Volvox example data ...";
+(   set -e;
+    set -x;
+
+    # format volvox
+    rm -rf sample_data/json/volvox;
+    bin/prepare-refseqs.pl --fasta docs/tutorial/data_files/volvox.fa --out sample_data/json/volvox;
+    if [ $SUPPRESS_BIODB_TO_JSON -eq 1 ]; then
+        echo "Not running biodb-to-json.pl for Volvox";
+    else
+        bin/biodb-to-json.pl -v --conf docs/tutorial/conf_files/volvox.json --out sample_data/json/volvox;
+    fi
+    cat docs/tutorial/data_files/volvox_microarray.bw.conf >> sample_data/json/volvox/tracks.conf
+    cat docs/tutorial/data_files/volvox_sine.bw.conf >> sample_data/json/volvox/tracks.conf
+    cat docs/tutorial/data_files/volvox-sorted.bam.conf >> sample_data/json/volvox/tracks.conf
+    cat docs/tutorial/data_files/volvox-sorted.bam.coverage.conf >> sample_data/json/volvox/tracks.conf
+    cat docs/tutorial/data_files/volvox-paired.bam.conf >> sample_data/json/volvox/tracks.conf
+    cat docs/tutorial/data_files/volvox.vcf.conf >> sample_data/json/volvox/tracks.conf
+    cat docs/tutorial/data_files/volvox_fromconfig.conf >> sample_data/json/volvox/tracks.conf
+    cat docs/tutorial/data_files/volvox.gff3.conf >> sample_data/json/volvox/tracks.conf
+    cat docs/tutorial/data_files/volvox.gtf.conf >> sample_data/json/volvox/tracks.conf
+    cat docs/tutorial/data_files/volvox.sort.gff3.gz.conf >> sample_data/json/volvox/tracks.conf
+    cat docs/tutorial/data_files/volvox.bw.gff3.gz.conf >> sample_data/json/volvox/tracks.conf
+    cat docs/tutorial/data_files/volvox.sort.bed.gz.conf >> sample_data/json/volvox/tracks.conf
+    cat docs/tutorial/data_files/bookmarks.conf >> sample_data/json/volvox/tracks.conf
+    bin/add-json.pl '{ "dataset_id": "volvox", "include": [ "../../raw/volvox/functions.conf" ] }' sample_data/json/volvox/trackList.json
+    bin/add-json.pl '{ "dataset_id": "volvox", "plugins": [ "NeatHTMLFeatures","NeatCanvasFeatures","HideTrackLabels" ] }' sample_data/json/volvox/trackList.json
+    bin/generate-names.pl --safeMode -v --out sample_data/json/volvox;
+
+    # also recreate some symlinks used by tests and such
+    if [ -d sample_data/json/modencode ]; then
+            mkdir -p sample_data/json/modencode/tracks;
+            ln -sf ../../volvox/tracks/volvox_microarray.wig sample_data/json/modencode/tracks/volvox_microarray.wig;
+    fi;
+    mkdir -p sample_data/raw;
+    if [ ! -e sample_data/raw/volvox ]; then
+        ln -s ../../docs/tutorial/data_files sample_data/raw/volvox;
+    fi;
+    ln -sf ../../docs/tutorial/conf_files/volvox.json sample_data/raw/;
+
+    touch sample_data/json/volvox/successfully_run;
+
+) >>setup.log 2>&1
+done_message "To see the volvox example data, browse to http://your.jbrowse.root/index.html?data=sample_data/json/volvox.";
+
+echo
+echo -n "Formatting Yeast example data ...";
+(   set -e;
+    set -x;
+
+    # format volvox
+    rm -rf sample_data/json/yeast/;
+    bin/prepare-refseqs.pl --fasta sample_data/raw/yeast_scaffolds/chr1.fa.gz --fasta sample_data/raw/yeast_scaffolds/chr2.fa.gzip  --out sample_data/json/yeast/;
+    gunzip -c sample_data/raw/yeast_scaffolds/chr1.fa.gz sample_data/raw/yeast_scaffolds/chr2.fa.gzip > sample_data/raw/yeast_chr1+2/yeast.fa;
+    if [ $SUPPRESS_BIODB_TO_JSON -eq 1 ]; then
+        echo "Not running biodb-to-json.pl for Yeast";
+    else
+        bin/biodb-to-json.pl --conf sample_data/raw/yeast.json --out sample_data/json/yeast/;
+    fi
+    bin/add-json.pl '{ "dataset_id": "yeast" }' sample_data/json/yeast/trackList.json
+    bin/add-json.pl '{ "dataset_id": "yeast",  "plugins": [ "NeatHTMLFeatures","NeatCanvasFeatures","HideTrackLabels" ] }' sample_data/json/yeast/trackList.json
+    bin/generate-names.pl --dir sample_data/json/yeast/;
+) >>setup.log 2>&1
+done_message "To see the yeast example data, browse to http://your.jbrowse.root/index.html?data=sample_data/json/yeast.";
