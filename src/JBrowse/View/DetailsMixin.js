3b3c32a42cc73f3cd4ef5c1cc1528ecfcfd062d5
Robert Buels <rbuels@gmail.com>
2018-07-10 13:07:50 -0700

    fix default feature details code to not crash when presented with arrays of objects

--- src/JBrowse/View/DetailsMixin.js
+++ src/JBrowse/View/DetailsMixin.js
@@ -131,9 +131,15 @@ return declare( null, {
         else if( valType == 'undefined' || val === null )
             return 0;
         else if( lang.isArray( val ) ) {
-            var vals = array.map( val, function(v) {
-                       return this.renderDetailValue( parent, title, v, f, class_ );
-                   }, this );
+            parent.style.width = '90%'
+            var vals = val.map( v => {
+                const itemContainer = domConstruct.create('div', {
+                    className: 'value_container '+class_,
+                    style: { width: '100%' },
+                }, parent );
+                this.renderDetailValue( itemContainer, title, v, f, class_ );
+                return itemContainer
+            })
             if( vals.length > 1 )
                 domClass.add( parent, 'multi_value' );
             if( vals.length > 10 )
