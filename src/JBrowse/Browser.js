575162cdedc12fe339553e1e6b091c3ed483c431
Colin <colin.diesh@gmail.com>
2018-07-13 14:41:39 -0400

    Add a disableSearch option

--- src/JBrowse/Browser.js
+++ src/JBrowse/Browser.js
@@ -920,21 +920,22 @@ initView: function() {
                 }
             }));
 
-            // add a global menu item for resizing all visible quantitative tracks
-            this.addGlobalMenuItem( 'view', new dijitMenuItem({
-                label: 'Search',
-                id: 'menubar_search',
-                title: 'Search for features',
-                onClick: () => {
-                    var conf = dojo.mixin( dojo.clone( this.config.names || {} ),
-                                           this.config.autocomplete || {} );
+            if (!this.config.disableSearch) {
+                this.addGlobalMenuItem( 'view', new dijitMenuItem({
+                    label: 'Search features',
+                    id: 'menubar_search',
+                    title: 'Search for features',
+                    onClick: () => {
+                        var conf = dojo.mixin( dojo.clone( this.config.names || {} ),
+                                               this.config.autocomplete || {} );
 
-                    var type = conf.dialog || 'JBrowse/View/Dialog/Search';
-                    dojo.global.require ([type], CLASS => {
-                        new CLASS(dojo.mixin({ browser: this }, conf)).show();
-                    });
-                }
-            }));
+                        var type = conf.dialog || 'JBrowse/View/Dialog/Search';
+                        dojo.global.require ([type], CLASS => {
+                            new CLASS(dojo.mixin({ browser: this }, conf)).show();
+                        });
+                    }
+                }));
+            }
 
             this.renderGlobalMenu( 'view', {text: 'View'}, menuBar );
 
