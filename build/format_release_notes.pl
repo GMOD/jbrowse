1263aa8252ce7d56617241205e9690a0074ca06a
Colin <colin.diesh@gmail.com>
2018-08-16 17:40:58 -0400

    [update docs] Add 1.15.2 release blogpost

--- build/format_release_notes.pl
+++ build/format_release_notes.pl
@@ -9,9 +9,8 @@ while(<>) {
     $release .= $_;
 }
 
-my $html = markdown( $release );
-$html =~ s!issue \#(\d+)!<a href="https://github.com/gmod/jbrowse/issues/$1">issue #$1</a>!g;
-$html =~ s!pull ( request)?\#(\d+)!<a href="https://github.com/gmod/jbrowse/pull/$2">issue #$2</a>!g;
-$html =~ s!(\(|\ )\@([-\w]+)!$1<a href="https://github.com/$2">\@$2</a>!g;
+$release =~ s!issue \#(\d+)!<a href="https://github.com/gmod/jbrowse/issues/$1">issue #$1</a>!g;
+$release =~ s!pull ( request)?\#(\d+)!<a href="https://github.com/gmod/jbrowse/pull/$2">issue #$2</a>!g;
+$release =~ s!(\(|\ )\@([-\w]+)!$1<a href="https://github.com/$2">\@$2</a>!g;
 
-print $html;
+print $release;
