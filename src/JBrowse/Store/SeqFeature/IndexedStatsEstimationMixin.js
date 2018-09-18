30e1456781a1ee9687bfbe9d1b428365c6bd8a30
Colin <colin.diesh@gmail.com>
2018-09-18 12:01:47 -0400

    Use async estimateGlobalStats

--- src/JBrowse/Store/SeqFeature/IndexedStatsEstimationMixin.js
+++ src/JBrowse/Store/SeqFeature/IndexedStatsEstimationMixin.js
@@ -1,9 +1,3 @@
-/**
- * Mixin that adds _estimateGlobalStats method to a store, which
- * samples a section of the features in the store and uses those to
- * esimate the statistics of the whole data set.
- */
-
 define([
            'dojo/_base/declare',
            'dojo/_base/array',
@@ -29,6 +23,10 @@ return declare( GlobalStats, {
             const chr = this.browser.regularizeReferenceName(refseq.name)
             const chrId = this.bam.chrToIndex && this.bam.chrToIndex[chr]
             featCount = await this.bam.index.featureCount(chrId, true)
+        } else if (this.bam) {
+            const chr = this.browser.regularizeReferenceName(refseq.name)
+            const chrId = this.bam.chrToIndex && this.bam.chrToIndex[chr]
+			featCount = await this.bam.index.lineCount(chrId, true)
         }
         if (featCount == -1) {
             return this.inherited('_estimateGlobalStats', arguments)
