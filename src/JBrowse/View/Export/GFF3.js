d311603f888369dbd5b59d6efddd34af0fb5ce34
Colin <colin.diesh@gmail.com>
2019-02-15 12:35:32 -0500

    Just put json in the gff3 value field

--- src/JBrowse/View/Export/GFF3.js
+++ src/JBrowse/View/Export/GFF3.js
@@ -205,6 +205,9 @@ return declare( ExportBase,
                                 ? array.map( val, s => gff.util.escape( s ) ).join(',')
                                 :
 
+                            val instanceof Object
+                                ? JSON.stringify(val)
+                                :
                             val.values
                                 ? val instanceof Array
                                         ? array.map( val, s => gff.util.escape( s ) ).join(',')
