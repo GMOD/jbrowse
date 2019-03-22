ea313d86ba1930278ab91bad7ad97502d87d6b1b
Colin <colin.diesh@gmail.com>
2019-03-22 17:25:02 -0400

    [update docs] Fix demo links

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -27,7 +27,7 @@ const FeatureCallout = props => (
       <li>Scales easily to multi-gigabase genomes and deep-coverage sequencing.</li>
       <li>Quickly open and view data files on your computer without uploading them to any server.</li>
       <li>Supports GFF3, BED, FASTA, Wiggle, BigWig, BAM, CRAM, VCF (with either .tbi or .idx index), REST, and more.  BAM, BigBed, BigWig, and VCF data are displayed directly from chunks of the compressed binary files, no conversion needed.</li>
-      <li>Includes an optional “faceted” track selector (<a href="http://jbrowse.org/code/latest-release/index.html?data=sample_data/json/modencode">see demo</a>) suitable for large installations with thousands of tracks.</li>
+      <li>Includes an optional “faceted” track selector (<a href="/code/latest-release/index.html?data=sample_data/json/modencode">see demo</a>) suitable for large installations with thousands of tracks.</li>
       <li>Very light server resource requirements. In fact, JBrowse has no back-end server code, it just reads chunks of files directly over HTTP using <a href="https://developer.mozilla.org/en-US/docs/Web/HTTP/Range_requests">byte-range requests</a>. You can serve huge datasets from a single low-cost cloud instance.</li>
       <li>JBrowse Desktop runs as a stand-alone app on Windows, Mac OS, and Linux.</li>
       <li>Highly extensible plugin architecture, with a <a href="https://gmod.github.io/jbrowse-registry">large registry of plugins</a>.</li>
@@ -59,7 +59,7 @@ const ExtraInfo = props => (
     <p>If you use JBrowse in a project that you publish, please cite the most recent JBrowse paper listed on the <a href="references.html">References</a> page</p>
 
     <h2>License</h2>
-    <p>JBrowse is released under the GNU LGPL or the Artistic License, see the JBrowse <a href="http://jbrowse.org/code/latest-release/LICENSE">LICENSE</a> file.</p>
+    <p>JBrowse is released under the GNU LGPL or the Artistic License, see the JBrowse <a href="/code/latest-release/LICENSE">LICENSE</a> file.</p>
   </div>
 )
 
@@ -85,7 +85,7 @@ class Index extends React.Component {
         <div className="flexContainer" style={{display: 'flex'}}>
           <div>
             <HeaderInfo />
-            <iframe width="100%" height="600" src="https://jbrowse.org/code/latest-release/?loc=ctgA%3A9892..32101&tracks=DNA%2CTranscript%2Cvolvox_microarray_bw_density%2Cvolvox_microarray_bw_xyplot%2Cvolvox-sorted-vcf%2Cvolvox-sorted_bam_coverage%2Cvolvox-sorted_bam&data=sample_data%2Fjson%2Fvolvox&tracklist=0"></iframe>
+            <iframe width="100%" height="600" src="/code/latest-release/?loc=ctgA%3A9892..32101&tracks=DNA%2CTranscript%2Cvolvox_microarray_bw_density%2Cvolvox_microarray_bw_xyplot%2Cvolvox-sorted-vcf%2Cvolvox-sorted_bam_coverage%2Cvolvox-sorted_bam&data=sample_data%2Fjson%2Fvolvox&tracklist=0"></iframe>
             <div className="mainContainer">
               <FeatureCallout />
               <BrowserCompat />
