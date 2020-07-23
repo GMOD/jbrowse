f6faee3b3dee8b7208003dcf20967228ae4cce81
Colin <colin.diesh@gmail.com>
2020-07-23 11:09:33 -0400

    Run yarn upgrade

--- build/dojo-webpack-plugin-loader/dojo/dojo.js
+++ build/dojo-webpack-plugin-loader/dojo/dojo.js
@@ -1,4 +1,4 @@
-module.exports = function(userConfig, defaultConfig, global, window) { this.loaderVersion = "1.16.2"; (function(
+module.exports = function(userConfig, defaultConfig, global, window) { this.loaderVersion = "1.16.3"; (function(
 	userConfig,
 	defaultConfig
 ){
